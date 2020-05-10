77773df00aee3aa0dde6d198f250d6b20c20c71d
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-09 23:26:52 -0700

    try first Gripp3r-1 Python program

--- Core-Robots/Gripp3r/Gripp3r-1.py
+++ Core-Robots/Gripp3r/Gripp3r-1.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import MediumMotor, OUTPUT_A
+from ev3dev2.sound import Sound
+
+from time import sleep
+
+
+MEDIUM_MOTOR = MediumMotor(OUTPUT_A)
+SPEAKER = Sound()
+
+
+MEDIUM_MOTOR.on_for_seconds(speed=-50, seconds=1, brake=True, block=True)
+SPEAKER.play_file(wav_file='', volume=100, play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+MEDIUM_MOTOR.on_for_seconds(speed=50, seconds=1, brake=True, block=True)
+sleep(1)
+SPEAKER.play_file(wav_file='', volume=100, play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+MEDIUM_MOTOR.on_for_seconds(speed=-50, seconds=1, brake=True, block=True)
