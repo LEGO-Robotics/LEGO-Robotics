7712541fe3c19020a0068eccaa832012bb0fc27c
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-09 21:35:09 -0700

    refactor Track3r-4 program

--- Core-Robots/Track3r/Track3r-4.swift
+++ Core-Robots/Track3r/Track3r-4.swift
@@ -1,20 +1,136 @@
+let leftWheelPort: OutputPort = .c
+let rightWheelPort: OutputPort = .b
+
+func startForward(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: power)
+}
+
+func forward(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: power,
+        brakeAtEnd: brake)
+}
+
+func startBackward(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: -power)
+}
+
+func backward(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: -power,
+        brakeAtEnd: brake)
+}
+
+func startLeft(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: -power)
+}
+
+func left(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: -power,
+        brakeAtEnd: brake)
+}
+
+func startRight(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: power)
+}
+
+func right(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: power,
+        brakeAtEnd: brake)
+}
+
+func startForwardLeft(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: 0)
+}
+
+func forwardLeft(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: 0,
+        brakeAtEnd: brake)
+}
+
+func startForwardRight(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: 0, rightPower: power)
+}
+
+func forwardRight(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: 0, rightPower: power,
+        brakeAtEnd: brake)
+}
+
+func startBackwardLeft(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: 0)
+}
+
+func backwardLeft(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: 0,
+        brakeAtEnd: brake)
+}
+
+func startBackwardRight(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: 0, rightPower: -power)
+}
+
+func backwardRight(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: 0, rightPower: -power,
+        brakeAtEnd: brake)
+}
+
 ev3.motorOn(forSeconds: 1, on: .a, withPower: -20)
 while true {
     ev3.motorOff(on: .a, brakeAtEnd: true)
     if ev3.measureIRProximity(on: .four) < 25 {
         ev3.displayImage(named: .pinchRight)
-        ev3.move(forDegrees: 1200, leftPort: .b, rightPort: .c, leftPower: -100, rightPower: 100)
+        right(nRotations: 6, power: 75, brake: true)
         ev3.displayImage(named: .hurt)
         ev3.motorOn(forSeconds: 0.8, on: .a, withPower: 100)
         ev3.playSound(file: .laser, atVolume: 100, withStyle: .waitForCompletion)
         ev3.motorOn(forSeconds: 1, on: .a, withPower: -20)
     } else {
         ev3.displayImage(named: .boom)
-        ev3.move(forSeconds: 0.8, leftPort: .b, rightPort: .c, leftPower: 100, rightPower: -100)
+        startForward(power: 75)
         ev3.motorOn(forSeconds: 0.1, on: .a, withPower: 100)
         ev3.waitFor(seconds: 0.1)
         ev3.displayImage(named: .ev3Icon)
-        ev3.move(forSeconds: 0.2, leftPort: .b, rightPort: .c, leftPower: 100, rightPower: -100)
+        startForwardRight(power: 75)
+        ev3.motorOff(on: .a, brakeAtEnd: false)
         ev3.motorOn(forSeconds: 0.2, on: .a, withPower: -10)
     }
 }
