3b9fa7299800a9ceda2aa3389fe37a60da8a39ca
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-09 21:28:39 -0700

    add Swift drivingUtil funcs

--- drivingUtil.swift
+++ drivingUtil.swift
@@ -1,12 +1,24 @@
 let leftWheelPort: OutputPort = .c
 let rightWheelPort: OutputPort = .b
 
+func startForward(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: power)
+}
+
 func forward(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
     ev3.move(
         forRotations: nRotations,
         leftPort: leftWheelPort, rightPort: rightWheelPort,
         leftPower: power, rightPower: power,
         brakeAtEnd: brake)
+}
+
+func startBackward(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: -power)
 }
 
 func backward(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
@@ -17,12 +29,24 @@ func backward(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
         brakeAtEnd: brake)
 }
 
+func startLeft(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: -power)
+}
+
 func left(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
     ev3.move(
         forRotations: nRotations,
         leftPort: leftWheelPort, rightPort: rightWheelPort,
         leftPower: power, rightPower: -power,
         brakeAtEnd: brake)
+}
+
+func startRight(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: power)
 }
 
 func right(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
@@ -33,12 +57,24 @@ func right(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
         brakeAtEnd: brake)
 }
 
+func startForwardLeft(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: 0)
+}
+
 func forwardLeft(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
     ev3.move(
         forRotations: nRotations,
         leftPort: leftWheelPort, rightPort: rightWheelPort,
         leftPower: power, rightPower: 0,
         brakeAtEnd: brake)
+}
+
+func startForwardRight(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: 0, rightPower: power)
 }
 
 func forwardRight(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
@@ -49,12 +85,24 @@ func forwardRight(nRotations: Float = 1, power: Float = 100, brake: Bool = false
         brakeAtEnd: brake)
 }
 
+func startBackwardLeft(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: 0)
+}
+
 func backwardLeft(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
     ev3.move(
         forRotations: nRotations,
         leftPort: leftWheelPort, rightPort: rightWheelPort,
         leftPower: -power, rightPower: 0,
         brakeAtEnd: brake)
+}
+
+func startBackwardRight(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: 0, rightPower: -power)
 }
 
 func backwardRight(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
