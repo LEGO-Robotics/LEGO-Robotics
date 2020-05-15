844c713af41f0efeec1f9a2dbd62bbd3f90d3bc3
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-05-15 10:59:54 -0700

    refactor util.drive_util

--- util/drive_util.py
+++ util/drive_util.py
@@ -7,7 +7,7 @@ class IRBeaconDriver:
     def __init__(
             self,
             left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C, motor_class=LargeMotor,
-            ir_sensor_port: str = INPUT_4,   # convention: https://sites.google.com/site/ev3devpython/learn_ev3_python/using-sensors
+            ir_sensor_port: str = INPUT_4,   # ref: sites.google.com/site/ev3devpython/learn_ev3_python/using-sensors
             ir_beacon_channel: int = 1):
         self.tank_driver = \
             MoveTank(
@@ -24,22 +24,26 @@ class IRBeaconDriver:
         self.ir_beacon_channel = ir_beacon_channel
         
     def drive(self, speed: float = 100):
-        if self.ir_sensor.top_left(self.ir_beacon_channel) and self.ir_sensor.top_right(self.ir_beacon_channel):
+        if self.ir_sensor.top_left(self.ir_beacon_channel) and \
+                self.ir_sensor.top_right(self.ir_beacon_channel):
             self.tank_driver.on(
                 left_speed=speed,
                 right_speed=speed)
     
-        elif self.ir_sensor.bottom_left(self.ir_beacon_channel) and self.ir_sensor.bottom_right(self.ir_beacon_channel):
+        elif self.ir_sensor.bottom_left(self.ir_beacon_channel) and \
+                self.ir_sensor.bottom_right(self.ir_beacon_channel):
             self.tank_driver.on(
                 left_speed=-speed,
                 right_speed=-speed)
     
-        elif self.ir_sensor.top_left(self.ir_beacon_channel) and self.ir_sensor.bottom_left(self.ir_beacon_channel):
+        elif self.ir_sensor.top_left(self.ir_beacon_channel) and \
+                self.ir_sensor.bottom_left(self.ir_beacon_channel):
             self.steer_driver.on(
                 steering=-100,
                 speed=speed)
     
-        elif self.ir_sensor.top_right(self.ir_beacon_channel) and self.ir_sensor.bottom_right(self.ir_beacon_channel):
+        elif self.ir_sensor.top_right(self.ir_beacon_channel) and \
+                self.ir_sensor.bottom_right(self.ir_beacon_channel):
             self.steer_driver.on(
                 steering=100,
                 speed=speed)
