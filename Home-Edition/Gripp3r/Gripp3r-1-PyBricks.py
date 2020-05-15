f1e9605340de64e23282959ab399219dcf2c54ec
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-14 23:03:04 -0700

    add draft Gripp3r-1-PyBricks program

--- Home-Edition/Gripp3r/Gripp3r-1-PyBricks.py
+++ Home-Edition/Gripp3r/Gripp3r-1-PyBricks.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env pybricks-micropython
+
+
+from pybricks import ev3brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port, Stop
+
+from time import sleep
+
+
+MEDIUM_MOTOR = Motor(port=Port.A)
+
+
+MEDIUM_MOTOR.run_time(
+    -50,
+    1,
+    Stop.BRAKE,
+    wait=True)
+
+MEDIUM_MOTOR.run_time(
+    50,
+    1,
+    Stop.BRAKE,
+    wait=True)
+
+sleep(1)
+
+MEDIUM_MOTOR.run_time(
+    -50,
+    1,
+    Stop.BRAKE,
+    wait=True)
