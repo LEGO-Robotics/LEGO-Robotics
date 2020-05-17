35601481e4ef3761c29ee4e162d58fc074e0d049
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-16 18:46:59 -0700

    add R3ptar program

--- Home-Edition/Core-Robots/R3ptar/R3ptar-1.py
+++ Home-Edition/Core-Robots/R3ptar/R3ptar-1.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B
+
+
+LARGE_MOTOR = LargeMotor(OUTPUT_B)
+MEDIUM_MOTOR = MediumMotor(OUTPUT_A)
+
+
+LARGE_MOTOR.on(
+    speed=100,
+    brake=False,
+    block=False)
+
+for i in range(3):   # MUST NOT have spaces between commands within a func/loop in MICROPYTHON
+    MEDIUM_MOTOR.on_for_seconds(
+        speed=10,
+        seconds=1,
+        brake=False,
+        block=True)
+    MEDIUM_MOTOR.on_for_seconds(
+        speed=-10,
+        seconds=1,
+        brake=False,
+        block=True)
