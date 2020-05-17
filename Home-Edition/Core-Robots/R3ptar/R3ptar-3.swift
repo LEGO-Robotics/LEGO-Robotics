a2d1e08b462341d2ce7731d77ed488d5489fee7f
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-16 18:06:53 -0700

    add R3ptar-3 swift program

---
+++ Home-Edition/Core-Robots/R3ptar/R3ptar-3.swift
@@ -0,0 +1,20 @@
+ev3.motorOn(forSeconds: 1, on: .d, withPower: -30)
+while true {
+    if ev3.measureIRProximity(on: .four) < 30 {
+        ev3.brickLightOn(withColor: .red, inMode: .on)
+        ev3.motorOff(on: .a, brakeAtEnd: true)
+        ev3.motorOff(on: .b)
+        ev3.playSound(file: .errorAlarm, atVolume: 100, withStyle: .playOnce)
+        ev3.motorOn(forSeconds: 1, on: .d, withPower: 100)
+        ev3.motorOn(on: .a, withPower: 100)
+        ev3.motorOn(on: .b, withPower: -100)
+        ev3.motorOn(forSeconds: 1, on: .d, withPower: -100)
+        ev3.waitFor(seconds: 2)
+        ev3.motorOn(forSeconds: 1, on: .a, withPower: -100)
+        ev3.waitFor(seconds: 1)
+    } else {
+        ev3.brickLightOn(withColor: .orange, inMode: .pulsating)
+        ev3.motorOn(on: .b, withPower: 100)
+        ev3.motorOn(forSeconds: 0.2, on: .a, withPower: random(min: -30, max: 30), brakeAtEnd: false)
+    }
+}
