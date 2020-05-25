26b7446d21b59ca317878334cd8a0fe7d4f494df
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-24 20:29:34 -0700

    add Spik3r-5 program

---
+++ Home-Edition/Core-Robots/Spik3r/Spik3r-5.swift
@@ -0,0 +1,27 @@
+ev3.motorOn(forSeconds: 1, on: .a, withPower: 50)
+ev3.motorOn(forSeconds: 0.3, on: .a, withPower: -50)
+ev3.motorOn(forSeconds: 1, on: .d, withPower: 40)
+ev3.motorOn(on: .b, withPower: -50)
+ev3.playSound(file: .blip, atVolume: 100, withStyle: .waitForCompletion)
+ev3.waitForIRProximity(on: .four, lessThanOrEqualTo: 30)
+ev3.waitForIRSeek(on: .four, greaterThanOrEqualTo: 5)
+ev3.motorOn(on: .b, withPower: -20)
+ev3.waitForIRSeek(on: .four, lessThanOrEqualTo: 3)
+ev3.motorOff(on: .b)
+ev3.playSound(file: .blip, atVolume: 100, withStyle: .waitForCompletion)
+for i in 1 ... 3 {
+    ev3.motorOn(forDegrees: 10, on: .b, withPower: -100)
+    ev3.motorOn(forDegrees: 220, on: .d, withPower: -75)
+    ev3.waitFor(seconds: 0.1)
+    ev3.motorOn(forSeconds: 1, on: .d, withPower: -100)
+    ev3.motorOn(forSeconds: 1, on: .d, withPower: 40)
+}
+ev3.motorOn(on: .b, withPower: 100)
+ev3.playSound(file: .blip, atVolume: 100, withStyle: .playOnce)
+for i in 1 ... 5 {
+    ev3.motorOn(forSeconds: 0.2, on: .a, withPower: 75)
+    ev3.motorOn(forSeconds: 0.2, on: .a, withPower: -75)
+}
+ev3.playSound(file: .blip, atVolume: 100, withStyle: .waitForCompletion)
+ev3.motorOn(forRotations: 2, on: .b, withPower: -100)
+ev3.motorOn(forRotations: 2, on: .b, withPower: 100)
