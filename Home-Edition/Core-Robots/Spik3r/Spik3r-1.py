f86f2a2e12fa82bd884c8370b9e7db484d7c4124
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-25 10:46:14 -0700

    add Spik3r-1 program

--- Home-Edition/Core-Robots/Spik3r/Spik3r-1.py
+++ Home-Edition/Core-Robots/Spik3r/Spik3r-1.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, OUTPUT_D
+from ev3dev2.sound import Sound
+
+
+LARGE_MOTOR = LargeMotor(OUTPUT_D)
+
+SPEAKER = Sound()
+
+
+LARGE_MOTOR.on_for_seconds(
+    speed=40,
+    seconds=1,
+    brake=True,
+    block=True)
+
+LARGE_MOTOR.on_for_degrees(
+    speed=-75,
+    degrees=220,
+    brake=True,
+    block=True)
+
+SPEAKER.play_file(
+    wav_file='/home/robot/sounds/Blip 3.wav',
+    volume=100,
+    play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+
+LARGE_MOTOR.on_for_seconds(
+    speed=-100,
+    seconds=1,
+    block=True,
+    brake=True)
+
+LARGE_MOTOR.on_for_seconds(
+    speed=40,
+    seconds=1,
+    block=True,
+    brake=True)
