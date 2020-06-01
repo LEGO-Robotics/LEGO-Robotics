406b595450e01d42e11c438200067d2116c860f5
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-31 20:29:18 -0700

    test displaying images in Track3r-1 (doesn't work yet with MicroPython 2.1.0)

--- Home-Edition/Core-Robots/Track3r/Track3r-1.py
+++ Home-Edition/Core-Robots/Track3r/Track3r-1.py
@@ -1,1 +1,18 @@
-#!/usr/bin/env micropython
+#!/usr/bin/env python3
+# (MicroPython does not yet support Display)
+
+
+from ev3dev2.motor import LargeMotor
+from ev3dev2.display import Display
+
+
+SCREEN = Display()
+
+
+SCREEN.image_filename(
+    filename='/home/robot/image/Pinch left.bmp',
+    clear_screen=True,
+    x1=0, y1=0,
+    # x2=177, y2=127   # ref: https://sites.google.com/site/ev3python/learn_ev3_python/screen
+       # commented out because of ValueError: images do not match
+)
