0a94b1e52c76809c98525f4203e0efbb36f62595
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-31 20:52:19 -0700

    add Track3r-2 Swift program

---
+++ Home-Edition/Core-Robots/Track3r/Track3r-2.swift
@@ -0,0 +1,212 @@
+let leftWheelPort: OutputPort = .b
+let rightWheelPort: OutputPort = .c
+
+let leftRightBC = (leftWheelPort == .b)
+
+func startForward(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: power)
+}
+
+func forward(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: power, rightPower: power,
+        brakeAtEnd: brake)
+}
+
+func startBackward(power: Float = 100) {
+    ev3.move(
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: -power)
+}
+
+func backward(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    ev3.move(
+        forRotations: nRotations,
+        leftPort: leftWheelPort, rightPort: rightWheelPort,
+        leftPower: -power, rightPower: -power,
+        brakeAtEnd: brake)
+}
+
+func startLeft(power: Float = 100) {
+    if leftRightBC {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: power)
+    } else {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: -power)
+    }
+}
+
+func left(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    if leftRightBC {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: power,
+            brakeAtEnd: brake)
+    } else {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: -power,
+            brakeAtEnd: brake)
+    }
+}
+
+func startRight(power: Float = 100) {
+    if leftRightBC {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: -power)
+    } else {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: power)
+    }
+}
+
+func right(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    if leftRightBC {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: -power,
+            brakeAtEnd: brake)
+    } else {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: power,
+            brakeAtEnd: brake)
+    }
+}
+
+func startForwardLeft(power: Float = 100) {
+    if leftRightBC {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: power)
+    } else {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: 0)
+    }
+}
+
+func forwardLeft(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    if leftRightBC {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: power,
+            brakeAtEnd: brake)
+    } else {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: 0,
+            brakeAtEnd: brake)
+    }
+}
+
+func startForwardRight(power: Float = 100) {
+    if leftRightBC {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: 0)
+    } else {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: power)
+    }
+}
+
+func forwardRight(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    if leftRightBC {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: power, rightPower: 0,
+            brakeAtEnd: brake)
+    } else {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: power,
+            brakeAtEnd: brake)
+    }
+}
+
+func startBackwardLeft(power: Float = 100) {
+    if leftRightBC {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: -power)
+    } else {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: 0)
+    }
+}
+
+func backwardLeft(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    if leftRightBC {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: -power,
+            brakeAtEnd: brake)
+    } else {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: 0,
+            brakeAtEnd: brake)
+    }
+}
+
+func startBackwardRight(power: Float = 100) {
+    if leftRightBC {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: 0)
+    } else {
+        ev3.move(
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: -power)
+    }
+    
+}
+
+func backwardRight(nRotations: Float = 1, power: Float = 100, brake: Bool = false) {
+    if leftRightBC {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: -power, rightPower: 0,
+            brakeAtEnd: brake)
+    } else {
+        ev3.move(
+            forRotations: nRotations,
+            leftPort: leftWheelPort, rightPort: rightWheelPort,
+            leftPower: 0, rightPower: -power,
+            brakeAtEnd: brake)
+    }
+}
+
+
+ev3.displayImage(named: .pinchRight)
+left(nRotations: 1, power: 25, brake: true)
+ev3.motorOn(forRotations: 3, on: .a, withPower: 100)
+ev3.displayImage(named: .warning)
+right(nRotations: 2, power: 25, brake: true)
+ev3.motorOn(forRotations: 6, on: .a, withPower: 100)
+ev3.displayImage(named: .ev3Icon)
+left(nRotations: 1, power: 25, brake: true)
+ev3.playSound(file: .fanfare, atVolume: 100, withStyle: .waitForCompletion)
