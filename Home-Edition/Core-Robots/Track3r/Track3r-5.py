09bb5d8f3a80276d953ffec284d6436f775011b6
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-31 18:15:47 -0700

    add Track3r-5.py :o program :)))))

--- Home-Edition/Core-Robots/Track3r/Track3r-5.py
+++ Home-Edition/Core-Robots/Track3r/Track3r-5.py
@@ -1,1 +1,89 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor import INPUT_4
+from ev3dev2.sensor.lego import InfraredSensor
+from ev3dev2.sound import Sound
+
+
+MEDIUM_MOTOR = MediumMotor(OUTPUT_A)
+TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B,
+                       right_motor_port=OUTPUT_C,
+                       motor_class=LargeMotor)
+
+IR_SENSOR = InfraredSensor(INPUT_4)
+
+SPEAKER = Sound()
+
+
+def drive_by_ir_beacon(channel: int = 1, speed: float = 100):
+    if IR_SENSOR.top_left(channel) and IR_SENSOR.top_right(channel):
+        # go forward
+        TANK_DRIVER.on(
+            left_speed=speed,
+            right_speed=speed)
+
+    elif IR_SENSOR.bottom_left(channel) and IR_SENSOR.bottom_right(channel):
+        # go backward
+        TANK_DRIVER.on(
+            left_speed=-speed,
+            right_speed=-speed)
+
+    elif IR_SENSOR.top_left(channel) and IR_SENSOR.bottom_right(channel):
+        # turn around left
+        TANK_DRIVER.on(
+            left_speed=-speed,
+            right_speed=speed)
+
+    elif IR_SENSOR.top_right(channel) and IR_SENSOR.bottom_left(channel):
+        # turn around right
+        TANK_DRIVER.on(
+            left_speed=speed,
+            right_speed=-speed)
+
+    elif IR_SENSOR.top_left(channel):
+        # turn left
+        TANK_DRIVER.on(
+            left_speed=0,
+            right_speed=speed)
+
+    elif IR_SENSOR.top_right(channel):
+        # turn right
+        TANK_DRIVER.on(
+            left_speed=speed,
+            right_speed=0)
+
+    elif IR_SENSOR.bottom_left(channel):
+        # left backward
+        TANK_DRIVER.on(
+            left_speed=0,
+            right_speed=-speed)
+
+    elif IR_SENSOR.bottom_right(channel):
+        # right backward
+        TANK_DRIVER.on(
+            left_speed=-speed,
+            right_speed=0)
+
+    else:
+        TANK_DRIVER.off(brake=False)
+
+
+def hit_objects_by_ir_beacon():
+    if IR_SENSOR.beacon(channel=1):
+        MEDIUM_MOTOR.on(
+            speed=100,
+            block=False,
+            brake=False)
+
+    else:
+        MEDIUM_MOTOR.off(brake=False)
+
+
+while True:
+    drive_by_ir_beacon(
+        channel=1,
+        speed=100)
+
+    hit_objects_by_ir_beacon()
