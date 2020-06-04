5cadfcd57dbeb6f9672c9071bc6d0b55d77fe491
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-04 00:51:17 -0700

    update Animal-Rescue program

--- Other-Robots/Swift-Playgrounds-Animal-Rescue/2-Explore-Idea-2/4-Make-Your-Own-with-IR-Sensor.swift
+++ Other-Robots/Swift-Playgrounds-Animal-Rescue/2-Explore-Idea-2/4-Make-Your-Own-with-IR-Sensor.swift
@@ -3,9 +3,7 @@ while true {
     // 100% is approximately 70cm/27in
     if ev3.measureIRProximity(on: .four) > 20.0 * 100 / 70 {
         ev3.motorOn(on: .b, withPower: -100)
-        ev3.motorOn(on: .c, withPower: -100)
     } else {
         ev3.motorOn(on: .b, withPower: -50)
-        ev3.motorOn(on: .c, withPower: -50)
     }
 }
