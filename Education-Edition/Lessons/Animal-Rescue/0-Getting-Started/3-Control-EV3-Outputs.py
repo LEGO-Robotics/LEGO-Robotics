3923a688ef2bca868b92daec44ae2866305a7446
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 16:46:50 -0700

    add Animal-Rescue Getting-Started Control-EV3-Outputs program

--- Education-Edition/Lessons/Animal-Rescue/0-Getting-Started/3-Control-EV3-Outputs.py
+++ Education-Edition/Lessons/Animal-Rescue/0-Getting-Started/3-Control-EV3-Outputs.py
@@ -1,1 +1,30 @@
-#!/usr/bin/env micropython
+#!/usr/bin/env python3
+# (Display not yet working in MicroPython)
+
+
+from ev3dev2.display import Display
+from ev3dev2.led import Leds
+
+from time import sleep
+
+
+SCREEN = Display()
+LEDS = Leds()
+
+
+SCREEN.image_filename(
+    filename='/home/robot/image/Hurt.bmp',
+    clear_screen=True)
+
+sleep(0.5)
+
+SCREEN.image_filename(
+    filename='/home/robot/image/Neutral.bmp',
+    clear_screen=True)
+
+LEDS.animate_flash(
+    color='RED',
+    groups=('LEFT', 'RIGHT'),
+    sleeptime=0.5,
+    duration=5,
+    block=True)
