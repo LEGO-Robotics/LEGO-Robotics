f674229835befc7b2cd552ce22a66f17e65542b8
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-11 21:23:51 -0700

    add :)) color crazy turtle .py program

--- Education-Edition/Lessons/Animal-Rescue/5-What-Is-Next/Solve-Another-Problem.py
+++ Education-Edition/Lessons/Animal-Rescue/5-What-Is-Next/Solve-Another-Problem.py
@@ -4,7 +4,7 @@
 # program your Turtle to perform different actions based on the color it detects
 
 
-from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.motor import MediumMotor, MoveTank, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor
 from ev3dev2.sound import Sound
@@ -13,6 +13,7 @@ from ev3dev2.sound import Sound
 LEG_MOTOR_RIGHT = LargeMotor(OUTPUT_C)
 LEG_MOTOR_LEFT = LargeMotor(OUTPUT_B)
 MEDIUM_MOTOR = MediumMotor(OUTPUT_A)
+TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B, right_motor_port=OUTPUT_C)
 
 TOUCH_SENSOR = TouchSensor(INPUT_1)
 COLOR_SENSOR = ColorSensor(INPUT_2)
@@ -29,7 +30,7 @@ while True:
             block=True,
             brake=True)
 
-    elif COLOR_SENSOR.color == ColorSensor.COLOR_BLACK:
+    elif COLOR_SENSOR.color == ColorSensor.COLOR_YELLOW:
         LEG_MOTOR_RIGHT.on_for_rotations(
             speed=100,
             rotations=3,
@@ -41,10 +42,29 @@ while True:
             volume=100,
             play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
 
-    elif COLOR_SENSOR.color == ColorSensor.COLOR_BROWN:
+    elif COLOR_SENSOR.color == ColorSensor.COLOR_GREEN:
         if IR_SENSOR.proximity <= 10:
             LEG_MOTOR_LEFT.on_for_seconds(
                 speed=-91,
                 seconds=1,
+                brake=True,
+                block=True)
+
+    elif COLOR_SENSOR.color == ColorSensor.COLOR_BLUE:
+        LEG_MOTOR_LEFT.on_for_seconds(
+            speed=-91,
+            seconds=10,
+            brake=True,
+            block=True)
+              
+        if IR_SENSOR.proximity <= 10:
+            TANK_DRIVER.on_for_rotations(
+                rotations=4,
+                right_speed=100,
+                left_speed=100)
+
+            LEG_MOTOR_LEFT.on_for_seconds(
+                speed=-91,
+                seconds=10,
                 brake=True,
                 block=True)
