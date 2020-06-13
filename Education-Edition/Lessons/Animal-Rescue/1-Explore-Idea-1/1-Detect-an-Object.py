b7a042fbf091187f6d213eefc022b6771155c9e1
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 18:10:06 -0700

    add animal rescue detect an object program

--- Education-Edition/Lessons/Animal-Rescue/1-Explore-Idea-1/1-Detect-an-Object.py
+++ Education-Edition/Lessons/Animal-Rescue/1-Explore-Idea-1/1-Detect-an-Object.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.sensor.lego import InfraredSensor
+from ev3dev2.sensor import INPUT_4
+from ev3dev2.led import Leds
+
+
+IR_SENSOR = InfraredSensor(INPUT_4)
+
+LEDS = Leds()
+
+
+while IR_SENSOR.proximity >= 30:
+    pass
+
+LEDS.animate_flash(
+    color='RED',
+    sleeptime=0.5,
+    duration=2.0,
+    block=True)
