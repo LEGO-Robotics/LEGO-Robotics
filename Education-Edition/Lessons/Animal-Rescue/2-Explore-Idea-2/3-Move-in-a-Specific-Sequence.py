a587f9bf0a19885d715a75053e0ebde82449c3f6
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 20:34:45 -0700

    add animal rescue move in a sequence program :B

--- Education-Edition/Lessons/Animal-Rescue/2-Explore-Idea-2/3-Move-in-a-Specific-Sequence.py
+++ Education-Edition/Lessons/Animal-Rescue/2-Explore-Idea-2/3-Move-in-a-Specific-Sequence.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, OUTPUT_B
+
+from time import sleep
+
+
+LARGE_MOTOR = LargeMotor(OUTPUT_B)
+
+
+def step_leg():
+    LARGE_MOTOR.on_for_degrees(
+        speed=100,
+        degrees=90,
+        brake=True,
+        block=True)
+
+    sleep(1)
+
+    LARGE_MOTOR.on_for_degrees(
+        speed=50,
+        degrees=360,
+        block=True,
+        brake=True)
+
+    
+for i in range(3):
+    step_leg()
