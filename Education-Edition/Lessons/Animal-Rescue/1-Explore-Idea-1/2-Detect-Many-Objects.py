67b6c0cbe086e625356f3ab73680fe8308e1a30a
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 18:31:18 -0700

    add animal rescue detect many objects program :B

--- Education-Edition/Lessons/Animal-Rescue/1-Explore-Idea-1/2-Detect-Many-Objects.py
+++ Education-Edition/Lessons/Animal-Rescue/1-Explore-Idea-1/2-Detect-Many-Objects.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.sensor.lego import InfraredSensor
+from ev3dev2.sensor import INPUT_4
+from ev3dev2.led import Leds
+
+
+IR_SENSOR = InfraredSensor(INPUT_4)
+
+LEDS = Leds()
+
+
+def detect_object(
+        distance: float = 30,
+        flashing_color: str = 'RED',
+        flashing_duration: float = 5):
+    if IR_SENSOR.proximity <= distance:
+        LEDS.animate_flash(
+            color=flashing_color,
+            sleeptime=0.5,
+            duration=flashing_duration,
+            block=True)
+
+    else:
+        LEDS.all_off()
+
+ 
+while True:
+    detect_object(
+        distance=10,
+        flashing_color='AMBER',
+        flashing_duration=4.2)
