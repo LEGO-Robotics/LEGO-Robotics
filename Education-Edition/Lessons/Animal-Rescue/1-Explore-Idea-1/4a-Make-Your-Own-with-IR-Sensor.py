fe14e1d9b92d098851678091398b64471fa3b78f
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 19:45:31 -0700

    add animal rescue program

--- Education-Edition/Lessons/Animal-Rescue/1-Explore-Idea-1/4a-Make-Your-Own-with-IR-Sensor.py
+++ Education-Edition/Lessons/Animal-Rescue/1-Explore-Idea-1/4a-Make-Your-Own-with-IR-Sensor.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.sensor.lego import InfraredSensor
+from ev3dev2.sensor import INPUT_4
+from ev3dev2.sound import Sound
+
+
+IR_SENSOR = InfraredSensor(INPUT_4)
+SPEAKER = Sound()
+
+
+def detect_object(
+        distance: float = 30,
+        sound_to_play: str = 'Hi'):
+    if IR_SENSOR.proximity <= distance:
+        SPEAKER.play_file(
+            wav_file='/home/robot/sound/{}.wav'.format(sound_to_play),
+            volume=100,
+            play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+ 
+while True:
+    detect_object(
+        distance=30,
+        sound_to_play='Magic wand')
