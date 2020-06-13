0b07d3019d5e1b0e2a9c2b6b7887112c1ffa26dd
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 17:19:15 -0700

    add  animal rescue 2.py program

--- Education-Edition/Lessons/Animal-Rescue/0-Getting-Started/2-Program-with-Parameters.py
+++ Education-Edition/Lessons/Animal-Rescue/0-Getting-Started/2-Program-with-Parameters.py
@@ -13,6 +13,36 @@ SPEAKER.play_file(
     play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
 
 SPEAKER.play_file(
+    wav_file='/home/robot/sound/LEGO.wav',
+    volume=100,
+    play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+SPEAKER.play_file(
+    wav_file='/home/robot/sound/Green.wav',
+    volume=100,
+    play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+SPEAKER.play_file(
+    wav_file='/home/robot/sound/EV3.wav',
+    volume=100,
+    play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+SPEAKER.play_file(
+    wav_file='/home/robot/sound/Good.wav',
+    volume=100,
+    play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+SPEAKER.play_file(
+    wav_file='/home/robot/sound/Boo.wav',
+    volume=100,
+    play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+SPEAKER.play_file(
     wav_file='/home/robot/sound/Activate.wav',
-    volume=50,
+    volume=100,
+    play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+SPEAKER.play_file(
+    wav_file='/home/robot/sound/Blip 1.wav',
+    volume=100,
     play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
