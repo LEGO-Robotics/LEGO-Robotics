9cba345d9577b5d061db289fd456ed2d94d33eb5
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 20:12:34 -0700

    add animal rescue move randomly progam :B

--- Education-Edition/Lessons/Animal-Rescue/2-Explore-Idea-2/2-Move-Randomly.py
+++ Education-Edition/Lessons/Animal-Rescue/2-Explore-Idea-2/2-Move-Randomly.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, OUTPUT_B
+
+from random import randint
+
+
+LARGE_MOTOR = LargeMotor(OUTPUT_B)
+
+
+LARGE_MOTOR.on_for_seconds(
+    speed=randint(0, 100),
+    seconds=randint(1, 5),
+    block=True,
+    brake=True)
+
+while True:
+    LARGE_MOTOR.on_for_seconds(
+        speed=randint(-100, 100),
+        seconds=randint(1, 5),
+        block=True,
+        brake=True)
