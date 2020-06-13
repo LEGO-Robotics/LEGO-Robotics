2dd76125e39dbb9d6c77bba27d02cf0d7ddddcfe
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 19:59:20 -0700

    add animal rescue make a leg move program :B

--- Education-Edition/Lessons/Animal-Rescue/2-Explore-Idea-2/1-Make-the-Leg-Move.py
+++ Education-Edition/Lessons/Animal-Rescue/2-Explore-Idea-2/1-Make-the-Leg-Move.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, OUTPUT_B
+
+
+LARGE_MOTOR = LargeMotor(OUTPUT_B)
+
+
+LARGE_MOTOR.on_for_seconds(
+    speed=50.0,
+    seconds=4.0,
+    block=True,
+    brake=True)
+
+LARGE_MOTOR.on_for_seconds(
+    speed=100.0,
+    seconds=4.0,
+    block=True,
+    brake=True)
+
+LARGE_MOTOR.on_for_seconds(
+    speed=-100.0,
+    seconds=4.0,
+    block=True,
+    brake=True)
+
+LARGE_MOTOR.on_for_rotations(
+    speed=100.0,
+    rotations=5.0,
+    block=True,
+    brake=True)
