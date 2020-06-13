dfdad6d3574aadc5cb1c2c47489798d096d33c7e
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 17:31:52 -0700

    add animal Rescue 4 program :)

--- Education-Edition/Lessons/Animal-Rescue/0-Getting-Started/4-Connect-a-Large-Motor.py
+++ Education-Edition/Lessons/Animal-Rescue/0-Getting-Started/4-Connect-a-Large-Motor.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, OUTPUT_B
+
+
+LARGE_MOTOR = LargeMotor(OUTPUT_B)
+
+
+LARGE_MOTOR.on_for_seconds(
+    speed=100.0,
+    seconds=3.0,
+    block=True,
+    brake=True)
+
+LARGE_MOTOR.on_for_seconds(
+    speed=-100.0,
+    seconds=3.0,
+    block=True,
+    brake=True)
