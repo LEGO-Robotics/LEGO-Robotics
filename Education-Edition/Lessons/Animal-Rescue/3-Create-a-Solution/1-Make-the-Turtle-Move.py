2adaa82b49b42389e2a852d6254a2d49c8c570b7
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-12 21:30:39 -0700

    add Animal-Rescue Make-the-Turtle-Move program

--- Education-Edition/Lessons/Animal-Rescue/3-Create-a-Solution/1-Make-the-Turtle-Move.py
+++ Education-Edition/Lessons/Animal-Rescue/3-Create-a-Solution/1-Make-the-Turtle-Move.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, MoveTank
+
+
+TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B, right_motor_port=OUTPUT_C)
+
+
+def zigzag(for_seconds: int):
+    for i in range(for_seconds // 2):
+        TANK_DRIVER.on_for_seconds(
+            left_speed=-100,
+            right_speed=0,
+            seconds=1,
+            brake=True,
+            block=True)
+
+        TANK_DRIVER.on_for_seconds(
+            left_speed=0,
+            right_speed=-100,
+            seconds=1,
+            brake=True,
+            block=True)
+
+
+zigzag(for_seconds=30)
