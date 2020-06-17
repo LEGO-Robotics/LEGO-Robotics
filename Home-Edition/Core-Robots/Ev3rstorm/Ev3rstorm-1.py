1ca87840ecffd819965bff0433a1af14e8659380
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-16 21:10:49 -0700

    add ev3rstorm 1 program :P :P

--- Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-1.py
+++ Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-1.py
@@ -2,4 +2,61 @@
 # (Display not yet working in MicroPython as of June 2020)
 
 
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, MoveSteering, MoveTank
 from ev3dev2.display import Display
+
+
+TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B,
+                       right_motor_port=OUTPUT_C,
+                       motor_class=LargeMotor)
+
+STEER_DRIVER =  MoveSteering(left_motor_port=OUTPUT_B,
+                             right_motor_port=OUTPUT_C,
+                             motor_class=LargeMotor)
+
+SCREEN = Display()
+
+
+SCREEN.image_filename(
+    filename='/home/robot/image/Neutral.bmp',
+    clear_screen=True)
+
+TANK_DRIVER.on_for_rotations(
+    left_speed=75,
+    right_speed=75,
+    rotations=5,
+    brake=True,
+    block=True)
+
+SCREEN.image_filename(
+    filename='/home/robot/image/Middle left.bmp',
+    clear_screen=True)
+
+STEER_DRIVER.on_for_rotations(
+    steering=50,
+    speed=75,
+    rotations=5,
+    brake=True,
+    block=True)
+
+SCREEN.image_filename(
+    filename='/home/robot/image/Neutral.bmp',
+    clear_screen=True)
+
+TANK_DRIVER.on_for_rotations(
+    left_speed=75,
+    right_speed=75,
+    rotations=5,
+    brake=True,
+    block=True)
+
+SCREEN.image_filename(
+    filename='/home/robot/image/Middle right.bmp',
+    clear_screen=True)
+
+STEER_DRIVER.on_for_rotations(
+    steering=-50,
+    speed=75,
+    rotations=5,
+    brake=True,
+    block=True)
