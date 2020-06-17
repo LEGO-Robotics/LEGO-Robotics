ae63d2d790da46753a8f126d5aa04fd73a9ad832
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-16 20:19:59 -0700

    add Ev3rstorm 1 program :P

---
+++ Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-1.swift
@@ -0,0 +1,15 @@
+ev3.displayImage(named: .neutral)
+
+ev3.move(forRotations: 5, leftPort: .b, rightPort: .c, leftPower: 75, rightPower: 75)
+
+ev3.displayImage(named: .decline)
+
+ev3.motorOn(forRotations: 5, on: .b, withPower: 75)
+
+ev3.displayImage(named: .neutral)
+
+ev3.move(forRotations: 5, leftPort: .b, rightPort: .c, leftPower: 75, rightPower: 75)
+
+ev3.displayImage(named: .pinchRight)
+
+ev3.motorOn(forRotations: 5, on: .c, withPower: 75)
