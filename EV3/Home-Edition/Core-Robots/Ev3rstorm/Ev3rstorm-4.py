248af8c0217b84e8c0ac4da1c672ecc4cbec498d
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-18 22:10:37 -0700

    add partial Ev3rstorm 4 program

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-4.py
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-4.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env micropython
+
+
+from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
+from ev3dev2.sensor.lego import ColorSensor, TouchSensor, InfraredSensor
+from ev3dev2.display import Display
+from ev3dev2.sound import Sound
+
+
+TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B,
+                       right_motor_port=OUTPUT_C,
+                       motor_class=LargeMotor)
+
+MEDIUM_MOTOR = MediumMotor(OUTPUT_A)
+
+COLOR_SENSOR = ColorSensor(INPUT_2)
+TOUCH_SENSOR = TouchSensor(INPUT_1)
+IR_SENSOR = InfraredSensor(INPUT_4)
+
+SCREEN = Display()
+SPEAKER = Sound()
+
+
+while True:
+    ...
