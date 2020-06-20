029b15ab1b1cb5380d218a31229da6a1469a4ae4
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-19 21:27:25 -0700

    add ev3rstorm 3 program :P

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-4.EV3Dev2.py
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-4.EV3Dev2.py
@@ -4,8 +4,10 @@
 from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
 from ev3dev2.sensor.lego import ColorSensor, TouchSensor, InfraredSensor
-from ev3dev2.display import Display
 from ev3dev2.sound import Sound
+from ev3dev2.led import Leds
+
+from time import time
 
 
 TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B,
@@ -18,9 +20,94 @@ COLOR_SENSOR = ColorSensor(INPUT_2)
 TOUCH_SENSOR = TouchSensor(INPUT_1)
 IR_SENSOR = InfraredSensor(INPUT_4)
 
-SCREEN = Display()
 SPEAKER = Sound()
+LEDS = Leds()
 
 
 while True:
-    ...
+    if IR_SENSOR.proximity < 25: 
+        TANK_DRIVER.off(brake=True)
+
+        LEDS.animate_police_lights(
+            color1='ORANGE',
+            color2='RED',
+            group1='LEFT',
+            group2='RIGHT',
+            sleeptime=0.5,
+            duration=5,
+            block=True)
+        
+        SPEAKER.play_file(
+            wav_file='/home/robot/sound/Object.wav',
+            volume=100,
+            play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+        SPEAKER.play_file(                                  
+            wav_file='/home/robot/sound/Detected.wav',
+            volume=100,
+            play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+        SPEAKER.play_file(
+            wav_file='/home/robot/sound/Error alarm.wav',
+            volume=100,
+            play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+        MEDIUM_MOTOR.on(
+            speed=100,
+            block=False,
+            brake=False)
+
+        TANK_DRIVER.on_for_rotations(
+            left_speed=100,
+            right_speed=80,
+            rotations=1,
+            brake=True,
+            block=True)
+
+        MEDIUM_MOTOR.on(
+            speed=-100,
+            block=False,
+            brake=False)
+
+        TANK_DRIVER.on_for_rotations(
+            left_speed=-100,
+            right_speed=-80,
+            rotations=1,
+            brake=True,
+            block=True)
+
+        MEDIUM_MOTOR.off(brake=True)
+
+        TANK_DRIVER.on_for_rotations(
+            left_speed=100,
+            right_speed=-100,
+            rotations=2,
+            brake=True,
+            block=True)
+
+        TANK_DRIVER.on_for_rotations(
+            left_speed=0,
+            right_speed=100,
+            rotations=1,
+            brake=True,
+            block=True)
+
+    else:
+        LEDS.animate_police_lights(
+            color1='YELLOW',
+            color2='GREEN',
+            group1='LEFT',
+            group2='RIGHT',
+            sleeptime=0.5,
+            duration=5,
+            block=True)
+           
+        if time() % 3 < 1.5:
+            TANK_DRIVER.on(
+                left_speed=50,
+                right_speed=100)
+
+        else:
+            TANK_DRIVER.on(
+                left_speed=100,
+                right_speed=50)
