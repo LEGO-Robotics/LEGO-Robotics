07367361efe07c5f772f045e0aab6f200f9ff26a
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-21 16:41:27 -0700

    add ev3rstorm 4 pybricks program :P

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-4.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-4.PyBricks.py
@@ -1,1 +1,74 @@
 #!/usr/bin/env pybricks-micropython
+
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, InfraredSensor
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Color, Direction, Port
+
+from time import time
+
+
+BRICK = EV3Brick()
+
+MEDIUM_MOTOR = Motor(port=Port.A, 
+                     positive_direction=Direction.CLOCKWISE)
+
+LEFT_MOTOR = Motor(port=Port.B,
+                   positive_direction=Direction.CLOCKWISE)
+RIGHT_MOTOR = Motor(port=Port.C,
+                    positive_direction=Direction.CLOCKWISE)
+WHEEL_DIAMETER = 26
+AXLE_TRACK = 102
+DRIVER = DriveBase(left_motor=LEFT_MOTOR,
+                   right_motor=RIGHT_MOTOR,
+                   wheel_diameter=WHEEL_DIAMETER,
+                   axle_track=AXLE_TRACK)
+DRIVER.settings(
+    straight_speed=300,
+    straight_acceleration=300,
+    turn_rate=90,
+    turn_acceleration=90)
+
+IR_SENSOR = InfraredSensor(port=Port.S4)
+
+
+while True:
+    if IR_SENSOR.distance() < 25:
+        DRIVER.stop()
+
+        BRICK.light.on(color=Color.RED)
+
+        BRICK.speaker.play_file(file=SoundFile.OBJECT)
+
+        BRICK.speaker.play_file(file=SoundFile.DETECTED)
+        
+        BRICK.speaker.play_file(file=SoundFile.ERROR_ALARM)
+
+        MEDIUM_MOTOR.run(speed=1000)
+
+        DRIVER.straight(distance=100)
+
+        MEDIUM_MOTOR.run(speed=-1000)
+
+        DRIVER.straight(distance=-100)
+
+        MEDIUM_MOTOR.hold()
+
+        DRIVER.turn(angle=135)
+
+        DRIVER.turn(angle=-30)
+
+    else:
+        BRICK.light.on(color=Color.GREEN)
+
+        if time() % 3 < 1.5:
+            DRIVER.drive(
+                speed=100,
+                turn_rate=-30)
+
+        else:
+            DRIVER.drive(
+                speed=100,
+                turn_rate=30)
