38cc32aaaaecaba21cc6c46aed362acf953d424e
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-21 13:45:26 -0700

    update Ev3rstorm-Super-Advanced.PyBricks program

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-Super-Advanced.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-Super-Advanced.PyBricks.py
@@ -7,10 +7,101 @@ from pybricks.media.ev3dev import ImageFile, SoundFile
 from pybricks.robotics import DriveBase
 from pybricks.parameters import Button, Color, Direction, Port, Stop
 
-import os
-import sys
-sys.path.append(os.path.expanduser('~'))   # TODO: AttributeError: 'module' object has no attribute 'path'
-from util.drive_util_pybricks import IRBeaconDriverMixin
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))   # TODO: AttributeError: 'module' object has no attribute 'path'
+# from util.drive_util_pybricks import IRBeaconDriverMixin
+
+
+class IRBeaconDriverMixin:
+    """
+    This reusable mixin provides the capability of driving a robot with a Driving Base by the IR beacon
+    """
+    def __init__(
+            self,
+            wheel_diameter: float, axle_track: float,   # both in milimeters
+            left_motor_port: Port = Port.B, right_motor_port: Port = Port.C,
+            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
+        self.driver = DriveBase(left_motor=Motor(port=left_motor_port,
+                                                 positive_direction=Direction.CLOCKWISE),
+                                right_motor=Motor(port=right_motor_port,
+                                                  positive_direction=Direction.CLOCKWISE),
+                                wheel_diameter=wheel_diameter,
+                                axle_track=axle_track)
+
+        self.ir_sensor = InfraredSensor(ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+    
+    
+    def drive_by_ir_beacon(
+            self,
+            speed: float = 100,     # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+        ):
+        ir_beacon_button_pressed = set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
+
+        # forward
+        if ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=0)
+
+        # backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN, Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=0)
+
+        # turn left on the spot
+        elif ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=0,
+                turn_rate=-turn_rate)
+
+        # turn right on the spot
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP, Button.LEFT_DOWN}:
+            self.driver.drive(
+                speed=0,
+                turn_rate=turn_rate)
+
+        # turn left forward
+        elif ir_beacon_button_pressed == {Button.LEFT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=-turn_rate)
+
+        # turn right forward
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP}:
+            self.driver.drive(
+                speed=speed,
+                turn_rate=turn_rate)
+
+        # turn left backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=turn_rate)
+
+        # turn right backward
+        elif ir_beacon_button_pressed == {Button.RIGHT_DOWN}:
+            self.driver.drive(
+                speed=-speed,
+                turn_rate=-turn_rate)
+
+        # otherwise stop
+        else:
+            self.driver.stop()
+
+
+    def keep_driving_by_ir_beacon(
+            self,
+            speed: float = 100,     # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+        ):
+        while True:
+            self.drive_by_ir_beacon(
+                speed=speed,
+                turn_rate=turn_rate)
 
 
 class Ev3rstorm(EV3Brick, IRBeaconDriverMixin):
@@ -102,7 +193,12 @@ class Ev3rstorm(EV3Brick, IRBeaconDriverMixin):
 
         while True:
             self.drive_by_ir_beacon()
-            self.detect_object_by_ir_sensor()
+
+            # DON'T use IR Sensor in 2 different modes in the same program / loop
+            # - https://github.com/pybricks/support/issues/62
+            # - https://github.com/ev3dev/ev3dev/issues/1401
+            # self.detect_object_by_ir_sensor()
+            
             self.blast_bazooka_if_touched()
 
 
