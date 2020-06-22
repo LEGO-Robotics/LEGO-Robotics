edea88d6208312b71f7fd100f9d96455dc05d6e1
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-21 17:58:04 -0700

    add Ev3rstorm 5 Swift program

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-6.swift
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-6.swift
@@ -1,1 +1,15 @@
-// No Swift API for Infrared Beacon buttons yet
+while true {
+    ev3.brickLightOn(withColor: .orange, inMode: .pulsating)
+
+    var headingDifference = ev3.measureIRSeek(on: .four) - (-3)
+
+    if abs(headingDifference) < 5 {
+        ev3.stopMove(leftPort: .b, rightPort: .c, withBrake: true)
+        ev3.brickLightOn(withColor: .red, inMode: .pulsating)
+        ev3.motorOn(forRotations: 3, on: .a, withPower: 100, brakeAtEnd: true)
+        ev3.playSound(file: .fanfare, atVolume: 100, withStyle: .waitForCompletion)
+    } else {
+        // TODO: fix to make it work
+        ev3.move(leftPort: .b, rightPort: .c, leftPower: 1, rightPower: 1)
+    }
+}
