531d98107d2061679e3b1a603d8a2d7ce496ba8a
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-06-25 22:03:53 -0700

    fix Ev3rstorm-Super.PyBricks program

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-Super.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-Super.PyBricks.py
@@ -188,7 +188,12 @@ class Ev3rstorm(EV3Brick, IRBeaconDriverMixin):
 
         while True:
             self.drive_by_ir_beacon()
-            self.detect_object_by_ir_sensor()
+
+            # DON'T use IR Sensor in 2 different modes in the same program / loop
+            # - https://github.com/pybricks/support/issues/62
+            # - https://github.com/ev3dev/ev3dev/issues/1401
+            # self.detect_object_by_ir_sensor()
+            
             self.blast_bazooka_if_touched()
 
 
