2b03509032b2e89a7b4cc9a42e59ba8c32745dbc
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-06-27 11:28:58 -0700

    add ev3rstorm 5 program :P :) :B :O

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-5.EV3Dev1.py
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-5.EV3Dev1.py
@@ -1,1 +1,120 @@
-#!/usr/bin/env micropython
+#!/usr/bin/env python3
+
+
+from ev3dev.ev3 import (
+    Motor, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, ColorSensor, InfraredSensor, RemoteControl, INPUT_1, INPUT_3, INPUT_4, 
+    Leds, Screen, Sound
+)
+
+from PIL import Image
+
+
+class Ev3rstorm:
+    def __init__(
+            self,
+            left_foot_motor_port: str = OUTPUT_B,
+            right_foot_motor_port: str = OUTPUT_C,
+            shooting_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1,
+            color_sensor_port: str = INPUT_3,
+            ir_sensor_port: str = INPUT_4,
+            ir_beacon_channel: int = 4):
+        self.left_foot_motor = LargeMotor(address=left_foot_motor_port)
+        self.right_foot_motor = LargeMotor(address=right_foot_motor_port)
+        
+        self.shooting_motor = MediumMotor(address=shooting_motor_port)
+
+        self.touch_sensor = TouchSensor(address=touch_sensor_port)
+        self.color_sensor = ColorSensor(address=color_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.remote_control = RemoteControl(sensor=self.ir_sensor,
+                                            channel=ir_beacon_channel)
+
+        self.screen = Screen()
+        self.speaker = Sound()
+
+
+    def drive_by_ir_beacon(
+            self,
+            speed: float = 1000   # degrees per second
+        ):
+        # forward
+        if self.remote_control.red_up and self.remote_control.blue_up:
+            self.left_foot_motor.run_forever(speed_sp=speed)
+            self.right_foot_motor.run_forever(speed_sp=speed)
+
+        # backward
+        elif self.remote_control.red_down and self.remote_control.blue_down:
+            self.left_foot_motor.run_forever(speed_sp=-speed)
+            self.right_foot_motor.run_forever(speed_sp=-speed)
+
+        # turn left on the spot
+        elif self.remote_control.red_up and self.remote_control.blue_down:
+            self.left_foot_motor.run_forever(speed_sp=-speed)
+            self.right_foot_motor.run_forever(speed_sp=speed)
+
+        # turn right on the spot
+        elif self.remote_control.red_down and self.remote_control.blue_up:
+            self.left_foot_motor.run_forever(speed_sp=speed)
+            self.right_foot_motor.run_forever(speed_sp=-speed)
+
+        # turn left forward
+        elif self.remote_control.red_up:
+            self.right_foot_motor.run_forever(speed_sp=speed)
+
+        # turn right forward
+        elif self.remote_control.blue_up:
+            self.left_foot_motor.run_forever(speed_sp=speed)
+
+        # turn left backward
+        elif self.remote_control.red_down:
+            self.right_foot_motor.run_forever(speed_sp=-speed)
+
+        # turn right backward
+        elif self.remote_control.blue_down:
+            self.left_foot_motor.run_forever(speed_sp=-speed)
+
+        # otherwise stop
+        else:
+            self.left_foot_motor.stop(stop_action=Motor.STOP_ACTION_COAST)
+            self.right_foot_motor.stop(stop_action=Motor.STOP_ACTION_COAST)
+
+
+    def shoot_when_touched(self):
+        if self.touch_sensor.is_pressed:
+            if self.color_sensor.ambient_light_intensity < 5:   # 15 not dark enough
+                self.speaker.play(wav_file='/home/robot/sound/Up.wav').wait()
+
+                self.shooting_motor.run_to_rel_pos(
+                    speed_sp=1000,   # degrees per second
+                    position_sp=-3 * 360,   # degrees
+                    stop_action=Motor.STOP_ACTION_HOLD)
+
+            else:
+                self.speaker.play(wav_file='/home/robot/sound/Down.wav').wait()
+
+                self.shooting_motor.run_to_rel_pos(
+                    speed_sp=1000,   # degrees per second
+                    position_sp=3 * 360,   # degrees
+                    stop_action=Motor.STOP_ACTION_HOLD)
+
+            while not self.touch_sensor.is_pressed:
+                pass
+
+
+    def main(self):
+        self.screen.image.paste(im=Image.open('/home/robot/image/Target.bmp'))
+        self.screen.update()
+    
+        while True:
+            self.drive_by_ir_beacon()
+            
+            self.shoot_when_touched()
+
+
+if __name__ == '__main__':
+    EV3RSTORM = Ev3rstorm()
+
+    EV3RSTORM.main()
