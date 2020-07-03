d14d073e90ed737a5aa97826555f39f119df7b05
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-03 10:18:52 -0700

    add Ev3rstorm-Super.PyBricks.RunParallel.FIXME program

---
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-Super.PyBricks.RunParallel.FIXME.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env pybricks-micropython
+
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor
+from pybricks.media.ev3dev import ImageFile, SoundFile
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Button, Color, Direction, Port, Stop
+
+from pybricks.experimental import run_parallel
+
+from random import randint
+from threading import Thread
+
+
+class Ev3rstorm(EV3Brick):
+    WHEEL_DIAMETER = 26   # milimeters
+    AXLE_TRACK = 102      # milimeters
+
+
+    def __init__(
+            self,
+            left_foot_motor_port: Port = Port.B, right_foot_motor_port: Port = Port.C,
+            bazooka_blast_motor_port: Port = Port.A,
+            touch_sensor_port: Port = Port.S1, color_sensor_port: Port = Port.S3,
+            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
+        self.drive_base = DriveBase(left_motor=Motor(port=left_foot_motor_port,
+                                                     positive_direction=Direction.CLOCKWISE),
+                                    right_motor=Motor(port=right_foot_motor_port,
+                                                      positive_direction=Direction.CLOCKWISE),
+                                    wheel_diameter=self.WHEEL_DIAMETER,
+                                    axle_track=self.AXLE_TRACK)
+        
+        self.bazooka_blast_motor = Motor(port=bazooka_blast_motor_port,
+                                         positive_direction=Direction.CLOCKWISE)
+
+        self.touch_sensor = TouchSensor(port=touch_sensor_port)
+        self.color_sensor = ColorSensor(port=color_sensor_port)
+
+        self.ir_sensor = InfraredSensor(port=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+    
+
+    def drive_once_by_ir_beacon(
+            self,
+            speed: float = 1000,    # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+        ):
+        ir_beacon_button_pressed = set(self.ir_sensor.buttons(channel=self.ir_beacon_channel))
+
+        # forward
+        if ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=0)
+
+        # backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN, Button.RIGHT_DOWN}:
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=0)
+
+        # turn left on the spot
+        elif ir_beacon_button_pressed == {Button.LEFT_UP, Button.RIGHT_DOWN}:
+            self.drive_base.drive(
+                speed=0,
+                turn_rate=-turn_rate)
+
+        # turn right on the spot
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP, Button.LEFT_DOWN}:
+            self.drive_base.drive(
+                speed=0,
+                turn_rate=turn_rate)
+
+        # turn left forward
+        elif ir_beacon_button_pressed == {Button.LEFT_UP}:
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=-turn_rate)
+
+        # turn right forward
+        elif ir_beacon_button_pressed == {Button.RIGHT_UP}:
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=turn_rate)
+
+        # turn left backward
+        elif ir_beacon_button_pressed == {Button.LEFT_DOWN}:
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=turn_rate)
+
+        # turn right backward
+        elif ir_beacon_button_pressed == {Button.RIGHT_DOWN}:
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=-turn_rate)
+
+        # otherwise stop
+        else:
+            self.drive_base.stop()
+    
+    def keep_driving_by_ir_beacon(
+            self,
+            speed: float = 1000,    # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+        ):
+        while True:
+            self.drive_once_by_ir_beacon(
+                speed=speed,
+                turn_rate=turn_rate)
+
+    
+    def dance_whenever_ir_beacon_pressed(self):
+        while True:
+            while Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+                self.drive_base.turn(angle=randint(-360, 360))
+
+    
+    def keep_detecting_objects_by_ir_sensor(self):
+        while True:
+            if self.ir_sensor.distance() < 25:
+                self.light.on(color=Color.RED)
+                self.speaker.play_file(file=SoundFile.OBJECT)
+                self.speaker.play_file(file=SoundFile.DETECTED)
+                self.speaker.play_file(file=SoundFile.ERROR_ALARM)
+                
+            else:
+                self.light.off()
+
+
+    def blast_bazooka_whenever_touched(self):
+        MEDIUM_MOTOR_N_ROTATIONS_PER_BLAST = 3
+        MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST = MEDIUM_MOTOR_N_ROTATIONS_PER_BLAST * 360
+
+        while True:
+            if self.touch_sensor.pressed():
+                if self.color_sensor.ambient() < 5:   # 15 not dark enough
+                    self.speaker.play_file(file=SoundFile.UP)
+
+                    self.bazooka_blast_motor.run_angle(
+                        speed=2 * MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,   # shoot quickly in half a second
+                        rotation_angle=-MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,
+                        then=Stop.HOLD,
+                        wait=True)
+
+                    self.speaker.play_file(file=SoundFile.LAUGHING_1)
+
+                else:
+                    self.speaker.play_file(file=SoundFile.DOWN)
+
+                    self.bazooka_blast_motor.run_angle(
+                        speed=2 * MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,   # shoot quickly in half a second
+                        rotation_angle=MEDIUM_MOTOR_ROTATIONAL_DEGREES_PER_BLAST,
+                        then=Stop.HOLD,
+                        wait=True)
+
+                    self.speaker.play_file(file=SoundFile.LAUGHING_2)
+
+
+    def main(self,
+             driving_speed: float = 1000   # mm/s
+            ):
+        self.screen.load_image(ImageFile.TARGET)
+
+        run_parallel(
+            self.keep_driving_by_ir_beacon,
+            
+            # FIXME: following thread seems to fail to run
+            self.dance_whenever_ir_beacon_pressed,
+
+            # DON'T use IR Sensor in 2 different modes in the same program / loop
+            # - https://github.com/pybricks/support/issues/62
+            # - https://github.com/ev3dev/ev3dev/issues/1401
+            # self.keep_detecting_objects_by_ir_sensor,
+
+            self.blast_bazooka_whenever_touched)
+            
+
+if __name__ == '__main__':
+    EV3RSTORM = Ev3rstorm()
+    
+    EV3RSTORM.main()
