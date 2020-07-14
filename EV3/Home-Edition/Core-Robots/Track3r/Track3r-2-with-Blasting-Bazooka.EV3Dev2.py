6f04b6d0ff29d6512bd12df0b2871f2b0b9a82a7
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-14 11:58:48 -0700

    add tTrack3rWithBlastingBazooka program

--- EV3/Home-Edition/Core-Robots/Track3r/Track3r-2-with-Blasting-Bazooka.EV3Dev2.py
+++ EV3/Home-Edition/Core-Robots/Track3r/Track3r-2-with-Blasting-Bazooka.EV3Dev2.py
@@ -8,114 +8,48 @@ from ev3dev2.sensor.lego import InfraredSensor
 from ev3dev2.display import Display
 from ev3dev2.sound import Sound
 
-import os
-import sys
-sys.path.append(os.path.expanduser('~'))
-from util.drive_util_ev3dev2 import IRBeaconRemoteControlledTank
+from track3r_ev3dev2 import Track3r
 
 
-MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
-TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B,
-                       right_motor_port=OUTPUT_C,
-                       motor_class=LargeMotor)
-IR_BEACON_DRIVER = \
-    IRBeaconRemoteControlledTank(
-        left_motor_port=OUTPUT_B,
-        right_motor_port=OUTPUT_C,
-        motor_class=LargeMotor,
-        ir_sensor_port=INPUT_4,
-        ir_beacon_channel=1)
+class Track3rWithBlastingBazooka(Track3r):
+    def blast_bazooka_by_ir_beacon(self, channel: int = 1, speed: float = 100):
+        if self.ir_sensor.beacon(channel=channel):
+            self.screen.image_filename(
+                filename='/home/robot/image/Pinch middle.bmp',
+                clear_screen=True)
+            self.screen.update()
 
-IR_SENSOR = InfraredSensor(address=INPUT_4)
+            self.medium_motor.on_for_rotations(
+                speed=speed,
+                rotations=3,   # about 3 rotations for 1 shot
+                block=True,
+                brake=True)
 
-SCREEN = Display()
-SPEAKER = Sound()
+            self.speaker.play_file(
+                wav_file='/home/robot/sound/Laughing 1.wav',
+                volume=100,
+                play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
 
+            while self.ir_sensor.beacon(channel=channel):
+                pass
 
-def drive_once_by_ir_beacon(channel: int = 1, speed: float = 100):
-    if IR_SENSOR.top_left(channel) and IR_SENSOR.top_right(channel):
-        # go forward
-        TANK_DRIVER.on(
-            left_speed=speed,
-            right_speed=speed)
-
-    elif IR_SENSOR.bottom_left(channel) and IR_SENSOR.bottom_right(channel):
-        # go backward
-        TANK_DRIVER.on(
-            left_speed=-speed,
-            right_speed=-speed)
-
-    elif IR_SENSOR.top_left(channel) and IR_SENSOR.bottom_right(channel):
-        # turn around left
-        TANK_DRIVER.on(
-            left_speed=-speed,
-            right_speed=speed)
-
-    elif IR_SENSOR.top_right(channel) and IR_SENSOR.bottom_left(channel):
-        # turn around right
-        TANK_DRIVER.on(
-            left_speed=speed,
-            right_speed=-speed)
-
-    elif IR_SENSOR.top_left(channel):
-        # turn left
-        TANK_DRIVER.on(
-            left_speed=0,
-            right_speed=speed)
-
-    elif IR_SENSOR.top_right(channel):
-        # turn right
-        TANK_DRIVER.on(
-            left_speed=speed,
-            right_speed=0)
-
-    elif IR_SENSOR.bottom_left(channel):
-        # left backward
-        TANK_DRIVER.on(
-            left_speed=0,
-            right_speed=-speed)
-
-    elif IR_SENSOR.bottom_right(channel):
-        # right backward
-        TANK_DRIVER.on(
-            left_speed=-speed,
-            right_speed=0)
-
-    else:
-        TANK_DRIVER.off(brake=False)
+        else:
+            self.medium_motor.off(brake=True)
 
 
-def shoot_objects_by_ir_beacon(channel: int = 1, speed: float = 1):
-    if IR_SENSOR.beacon(channel=channel):
-        SCREEN.image_filename(
-            filename='/home/robot/image/Pinch middle.bmp',
-            clear_screen=True)
-        SCREEN.update()
+    def main(self):
+        while True:
+            self.drive_once_by_ir_beacon(
+                channel=1,
+                speed=100)
 
-        MEDIUM_MOTOR.on_for_degrees(
-            speed=speed,
-            degrees=1000,   # about 3 rotations for 1 shot
-            block=True,
-            brake=True)
-
-        SPEAKER.play_file(
-            wav_file='/home/robot/sound/Laughing 1.wav',
-            volume=100,
-            play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
-
-        while IR_SENSOR.beacon(channel=channel):
-            pass
-
-    else:
-        MEDIUM_MOTOR.off(brake=True)
+            self.blast_bazooka_by_ir_beacon(
+                channel=1,
+                speed=100)
 
 
-while True:
-    IR_BEACON_DRIVER.drive(speed=100)
-    # drive_once_by_ir_beacon(
-    #     channel=1,
-    #     speed=100)
+if __name__ == "__main__":
+    TRACKER_WITH_BLASTING_BAZOOKA = Track3rWithBlastingBazooka()
 
-    shoot_objects_by_ir_beacon(
-        channel=1,
-        speed=100)
+    TRACKER_WITH_BLASTING_BAZOOKA.main()
+    
