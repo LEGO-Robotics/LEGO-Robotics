845e3ca9c7cdbcc87e963722d388633197939dcb
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-13 22:20:14 -0700

    add Track3r_RC_Tank_EV3Dev2 module

--- EV3/Home-Edition/Core-Robots/Track3r/track3r_rc_tank_ev3dev2.py
+++ EV3/Home-Edition/Core-Robots/Track3r/track3r_rc_tank_ev3dev2.py
@@ -1,3 +1,28 @@
-#!/usr/bin/env micropython
+#!/usr/bin/env python3
 
 
+__all__ = 'Track3r',
+
+
+from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.control.rc_tank import RemoteControlledTank
+
+
+class Track3r(RemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
+            medium_motor_port: str = OUTPUT_A):
+        super().__init__(
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            polarity='normal',
+            speed=1000,
+            channel=1)
+
+        self.medium_motor = MediumMotor(address=medium_motor_port)
+
+
+if __name__ == '__main__':
+    TRACK3R = Track3r()
+
+    TRACK3R.main()
