b52abef54d945cbd98958530473bc385c47695fd
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-14 11:23:17 -0700

    update track3r programs

--- EV3/Home-Edition/Core-Robots/Track3r/track3r_ev3dev2.py
+++ EV3/Home-Edition/Core-Robots/Track3r/track3r_ev3dev2.py
@@ -4,7 +4,7 @@
 __all__ = 'Track3r',
 
 
-from ev3dev2.motor import OUTPUT_B, OUTPUT_C
+from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from ev3dev2.sensor import INPUT_4
 
 import os
@@ -17,10 +17,13 @@ class Track3r(IRBeaconRemoteControlledTank):
     def __init__(
             self,
             left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
+            medium_motor_port: str = OUTPUT_A,
             ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
         super().__init__(
             left_motor_port=left_motor_port, right_motor_port=right_motor_port,
             ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+
+        self.medium_motor = MediumMotor(address=medium_motor_port)
 
 
 if __name__ == '__main__':
--- EV3/Home-Edition/Core-Robots/Track3r/track3r_pybricks.py
+++ EV3/Home-Edition/Core-Robots/Track3r/track3r_pybricks.py
@@ -4,6 +4,7 @@
 __all__ = 'Track3r',
 
 
+from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 
 import os
@@ -19,11 +20,14 @@ class Track3r(IRBeaconRemoteControlledTank):
     def __init__(
             self,
             left_motor_port: Port = Port.B, right_motor_port: Port = Port.C,
+            medium_motor_port: Port = Port.A,
             ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
         super().__init__(
             wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
             left_motor_port=left_motor_port, right_motor_port=right_motor_port,
             ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+
+        self.medium_motor = Motor(port=medium_motor_port)
 
 
 if __name__ == '__main__':
