ea8eaa1eb054775729cc717ab84ddc213f640456
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-14 12:57:30 -0700

    add Track3r-RCTank EV3Dev2 programs

--- EV3/Home-Edition/Core-Robots/Track3r/Track3r-5-RCTank-with-Spinner.EV3Dev2.py
+++ EV3/Home-Edition/Core-Robots/Track3r/Track3r-5-RCTank-with-Spinner.EV3Dev2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-from track3r_rc_tank_ev3dev1 import Track3r
+from track3r_rc_tank_ev3dev2 import Track3r
 
 
 class Track3rWithSpinner(Track3r):
@@ -13,7 +13,7 @@ class Track3rWithSpinner(Track3r):
             left_motor_port=left_motor_port, right_motor_port=right_motor_port,
             medium_motor_port=medium_motor_port)
         
-        self.remote.on_beacon = self.spin
+        self.remote.on_channel1_beacon = self.spin
 
 
     def spin(self, state):
