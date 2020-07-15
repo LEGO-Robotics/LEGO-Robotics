0e096a24a3dade03ff5cd05c4e9913ff63a9c2c0
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-14 21:06:27 -0700

    add Tracker-2-with-Blasting-Bazooka.EV3Dev2.Python3.MultiProcessing/Threading programs

--- EV3/Home-Edition/Core-Robots/Track3r/Track3r-2-with-Blasting-Bazooka.EV3Dev2.Python3.Threading.py
+++ EV3/Home-Edition/Core-Robots/Track3r/Track3r-2-with-Blasting-Bazooka.EV3Dev2.Python3.Threading.py
@@ -2,3 +2,48 @@
 # (MicroPython does not yet support Display as of 2020)
 
 
+from ev3dev2.sound import Sound
+
+from threading import Thread
+
+from track3r_ev3dev2 import Track3r
+
+
+class Track3rWithBlastingBazooka(Track3r):
+    def blast_bazooka_by_ir_beacon(self, channel: int = 1, speed: float = 100):
+        while True:
+            if self.ir_sensor.beacon(channel=channel):
+                self.medium_motor.on_for_rotations(
+                    speed=speed,
+                    rotations=3,   # about 3 rotations for 1 shot
+                    block=True,
+                    brake=True)
+
+                self.speaker.play_file(
+                    wav_file='/home/robot/sound/Laughing 1.wav',
+                    volume=100,
+                    play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+
+                while self.ir_sensor.beacon(channel=channel):
+                    pass
+
+            else:
+                self.medium_motor.off(brake=True)
+
+
+    def main(self, speed: float = 100):
+        self.screen.image_filename(
+            filename='/home/robot/image/Pinch middle.bmp',
+            clear_screen=True)
+        self.screen.update()
+
+        Thread(target=self.blast_bazooka_by_ir_beacon,
+               daemon=True).start()
+
+        self.keep_driving_by_ir_beacon(speed=speed)
+
+
+if __name__ == "__main__":
+    TRACKER_WITH_BLASTING_BAZOOKA = Track3rWithBlastingBazooka()
+
+    TRACKER_WITH_BLASTING_BAZOOKA.main()
