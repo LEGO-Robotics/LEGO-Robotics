7c4fa28c7bd7c95a1ab214033c203db214fe3b6f
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-15 20:27:29 -0700

    add track3r program ;p, :B, :)

--- EV3/Home-Edition/Core-Robots/Track3r/Track3r-3-with-Gripping-Claw.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Track3r/Track3r-3-with-Gripping-Claw.PyBricks.py
@@ -1,1 +1,46 @@
 #!/usr/bin/env pybricks-micropython
+
+
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Button
+
+from track3r_pybricks import Track3r
+
+
+class Track3rWithGrippingClaw(Track3r):
+    is_grabbing = False
+    
+    def grip_or_release_claw_by_ir_beacon(
+            self,
+            speed: float = 1000   # deg/s
+        ):
+        if Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+            if self.is_grabbing:
+                self.medium_motor.run(speed=-speed)
+
+                self.speaker.play_file(file=SoundFile.AIR_RELEASE)
+
+                self.is_grabbing = False
+
+            else:
+                self.medium_motor.run(speed=speed)
+
+                self.speaker.play_file(file=SoundFile.AIRBRAKE)
+
+                self.is_grabbing = True
+
+            while Button.BEACON in self.ir_sensor.buttons(channel=self.ir_beacon_channel):
+                pass
+
+
+    def main(self, speed: float = 1000):
+        while True:
+            self.drive_once_by_ir_beacon(speed=speed)
+
+            self.grip_or_release_claw_by_ir_beacon(speed=speed)
+
+
+if __name__ == '__main__':
+    TRACKER_WITH_GRIPPING_CLAW = Track3rWithGrippingClaw()
+
+    TRACKER_WITH_GRIPPING_CLAW.main()
