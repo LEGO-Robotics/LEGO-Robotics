c8492f06d5659569fef038ce0a5d27171cb3c362
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-18 16:40:14 -0700

    add Track3r-4-with-Heavy-Hammer.EV3Dev2 program

--- EV3/Home-Edition/Core-Robots/Track3r/Track3r-4-with-Heavy-Hammer.EV3Dev2.py
+++ EV3/Home-Edition/Core-Robots/Track3r/Track3r-4-with-Heavy-Hammer.EV3Dev2.py
@@ -1,92 +1,77 @@
 #!/usr/bin/env python3
-# (MicroPython does not yet support Display as of May 2020)
 
 
-from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
-from ev3dev2.sensor import INPUT_4
-from ev3dev2.sensor.lego import InfraredSensor
-from ev3dev2.display import Display
-from ev3dev2.sound import Sound
+from time import sleep
 
-import os
-import sys
-sys.path.append(os.path.expanduser('~'))
-from util.drive_util_ev3dev2 import IRBeaconRemoteControlledTank
+from track3r_ev3dev2 import Track3r
 
 
-MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
-TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B,
-                       right_motor_port=OUTPUT_C,
-                       motor_class=LargeMotor)
-IR_BEACON_DRIVER = \
-    IRBeaconRemoteControlledTank(
-        left_motor_port=OUTPUT_B,
-        right_motor_port=OUTPUT_C,
-        motor_class=LargeMotor,
-        ir_sensor_port=INPUT_4,
-        ir_beacon_channel=1)
+class Track3rWithHeavyHammer(Track3r):
+    def hammer_by_ir_beacon(self):
+        if self.ir_sensor.beacon(channel=self.ir_beacon_channel):
+            self.screen.image_filename(
+                filename='/home/robot/image/Angry.bmp',
+                clear_screen=True)
+            self.screen.update()
+        
+            self.medium_motor.on_for_seconds(
+                seconds=0.3,
+                speed=100,
+                block=True,
+                brake=True)
 
-IR_SENSOR = InfraredSensor(address=INPUT_4)
+            self.speaker.play_file(
+                wav_file='/home/robot/sound/Laughing 2.wav',
+                volume=100,
+                play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
 
-SCREEN = Display()
-SPEAKER = Sound()
+            self.medium_motor.on_for_seconds(
+                speed=-20,
+                seconds=1, 
+                block=True,
+                brake=True)
 
+        else:
+            self.screen.image_filename(
+                filename='/home/robot/image/Crazy 1.bmp',
+                clear_screen=True)
+            self.screen.update()
+            
+            self.medium_motor.on_for_seconds(
+                speed=75,
+                seconds=0.1,
+                block=True,
+                brake=True)
 
-def drive_once_by_ir_beacon(channel: int = 1, speed: float = 100):
-    if IR_SENSOR.top_left(channel) and IR_SENSOR.top_right(channel):
-        # go forward
-        TANK_DRIVER.on(
-            left_speed=speed,
-            right_speed=speed)
+            sleep(0.1)
 
-    elif IR_SENSOR.bottom_left(channel) and IR_SENSOR.bottom_right(channel):
-        # go backward
-        TANK_DRIVER.on(
-            left_speed=-speed,
-            right_speed=-speed)
+            self.screen.image_filename(
+                filename='/home/robot/image/Crazy 2.bmp',
+                clear_screen=True)
+            self.screen.update()
 
-    elif IR_SENSOR.top_left(channel) and IR_SENSOR.bottom_right(channel):
-        # turn around left
-        TANK_DRIVER.on(
-            left_speed=-speed,
-            right_speed=speed)
-
-    elif IR_SENSOR.top_right(channel) and IR_SENSOR.bottom_left(channel):
-        # turn around right
-        TANK_DRIVER.on(
-            left_speed=speed,
-            right_speed=-speed)
-
-    elif IR_SENSOR.top_left(channel):
-        # turn left
-        TANK_DRIVER.on(
-            left_speed=0,
-            right_speed=speed)
-
-    elif IR_SENSOR.top_right(channel):
-        # turn right
-        TANK_DRIVER.on(
-            left_speed=speed,
-            right_speed=0)
-
-    elif IR_SENSOR.bottom_left(channel):
-        # left backward
-        TANK_DRIVER.on(
-            left_speed=0,
-            right_speed=-speed)
-
-    elif IR_SENSOR.bottom_right(channel):
-        # right backward
-        TANK_DRIVER.on(
-            left_speed=-speed,
-            right_speed=0)
-
-    else:
-        TANK_DRIVER.off(brake=False)
+            self.medium_motor.on_for_seconds(
+                speed=-30,   # -10 too soft
+                seconds=0.2,
+                brake=False,
+                block=True)
 
 
-while True:
-    IR_BEACON_DRIVER.drive(speed=100)
-    # drive_once_by_ir_beacon(
-    #     channel=1,
-    #     speed=100)
+    def main(self, speed: float = 100):
+        self.medium_motor.on_for_seconds(
+            speed=-20,  
+            seconds=1,   
+            block=True,
+            brake=True)
+
+        # FIXME: need MultiProcessing/Threading for smoother run
+        while True:
+            self.drive_once_by_ir_beacon(speed=speed)
+
+            self.hammer_by_ir_beacon()
+
+
+if __name__ == '__main__':
+    TRACKER_WITH_HEAVY_HAMMER = Track3rWithHeavyHammer()
+
+    TRACKER_WITH_HEAVY_HAMMER.main()
