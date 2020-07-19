521ca6fbeeddccec055cdb2d575dab7d4ab85803
Vinh Luong <MBALearnsToCode@UChicago.edu>
2020-07-19 12:14:40 -0700

    add Track3r programs

--- EV3/Home-Edition/Core-Robots/Track3r/Track3r-5-with-BiBlade-Spinner.EV3Dev1.MultiProcessing.py
+++ EV3/Home-Edition/Core-Robots/Track3r/Track3r-5-with-BiBlade-Spinner.EV3Dev1.MultiProcessing.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import Motor
+
+from track3r_ev3dev1 import Track3r
+
+from multiprocessing import Process
+
+class Track3rWithBiBladeSpinner(Track3r):
+    def spin_blade_by_ir_beacon(
+            self,
+            speed: float = 1000   # deg/s
+        ):
+        while True:
+            if self.remote_control.beacon:
+                self.medium_motor.run_forever(speed_sp=speed)
+
+            else:
+                self.medium_motor.stop(stop_action=Motor.STOP_ACTION_COAST)
+
+    def main(self,
+             speed: float = 1000   # deg/s
+            ):
+        Process(target=self.spin_blade_by_ir_beacon).start()
+            
+        self.keep_driving_by_ir_beacon(speed=speed)
+
+           
+if __name__ == '__main__':
+    TRACKER_WITH_BIBLADE_SPINNER = Track3rWithBiBladeSpinner()
+
+    TRACKER_WITH_BIBLADE_SPINNER.main()
