34af6e40b4c1bd9bd67bc3d75eb3703f51754752
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-23 20:41:50 -0700

    update sweep3r program

--- EV3/Antoni-Bots/Sweep3r/Sweep3r.EV3Dev2.py
+++ EV3/Antoni-Bots/Sweep3r/Sweep3r.EV3Dev2.py
@@ -1,9 +1,11 @@
-from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
+#!/usr/bin/env python3
+
+
+from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from ev3dev2.sensor import INPUT_1, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor
 from ev3dev2.display import Display
 from ev3dev2.sound import Sound
-
 
 import os
 import sys
@@ -18,11 +20,11 @@ class Sweep3r(IRBeaconRemoteControlledTank):
             medium_motor_port: str = OUTPUT_A,
             touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
             ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
-        self.tank_driver = MoveTank(left_motor_port=left_foot_motor_port,
-                                    right_motor_port=right_foot_motor_port,
-                                    motor_class=LargeMotor)
+        super().__init__(
+            left_motor_port=left_foot_motor_port, right_motor_port=right_foot_motor_port,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
 
-        self.medium_motor = MediumMotor(address=bazooka_blast_motor_port)
+        self.medium_motor = MediumMotor(address=medium_motor_port)
 
         self.touch_sensor = TouchSensor(address=touch_sensor_port)
         self.color_sensor = ColorSensor(address=color_sensor_port)
@@ -34,7 +36,7 @@ class Sweep3r(IRBeaconRemoteControlledTank):
         self.speaker = Sound()
 
 
-    def drill(self, speed: int = 100):
+    def drill(self, speed: float = 100):
         if self.ir_sensor.beacon(channel=self.ir_beacon_channel):
             self.medium_motor.on_for_rotations(
                 speed=speed,
@@ -43,37 +45,40 @@ class Sweep3r(IRBeaconRemoteControlledTank):
                 brake=True)
 
 
-    def move_when_touched(self, speed_sp: str = 1000):    
+    def move_when_touched(self):    
         if self.touch_sensor.is_pressed:
-            if self.color_sensor.reflected_light_intensity < 3:
-                self.tank_driver.on_for_seconds(
-                    left_speed=100,
-                    right_speed=-100,
-                    brake=True,
-                    block=True)
+            self.tank_driver.on_for_seconds(
+                left_speed=100,
+                right_speed=-100,
+                seconds=2,
+                brake=True,
+                block=True)
 
 
-    def spin_when_see_smothing(self):
-        if self.color_sensor.reflected_light_intensity < 3:
+    def move_when_see_smothing(self):
+        if self.color_sensor.reflected_light_intensity > 30:
             self.tank_driver.on_for_seconds(
                 left_speed=-100,
                 right_speed=100,
+                seconds=2,
                 brake=True,
                 block=True)
 
         
-    def main(self,
-             speed: float = 100
-            ):
-        self.screen.image.paste(im=Image.open('/home/robot/image/Pinch middle.bmp'))
+    def main(self, speed: float = 100):
+        self.screen.image_filename(
+            filename='/home/robot/image/Neutral.bmp',
+            clear_screen=True)
         self.screen.update()
     
         while True:
             self.drive_once_by_ir_beacon(speed=speed)
 
-            self.move_when_touched(speed=speed)
+            self.move_when_touched()
 
-            self.spin_when_see_smothing(speed=speed)
+            self.move_when_see_smothing()
+
+            self.drill(speed=speed)
 
 
 if __name__ == '__main__':
