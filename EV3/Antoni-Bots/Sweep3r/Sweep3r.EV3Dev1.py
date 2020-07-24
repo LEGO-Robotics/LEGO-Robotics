f2a1a42e88a422fe4ffd2ec07817b5221c484626
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-23 20:26:14 -0700

    update Sweep3r.EV3Dev1 program

--- EV3/Antoni-Bots/Sweep3r/Sweep3r.EV3Dev1.py
+++ EV3/Antoni-Bots/Sweep3r/Sweep3r.EV3Dev1.py
@@ -2,7 +2,7 @@
 
 
 from ev3dev.ev3 import (
-    Motor, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    Motor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
     TouchSensor, ColorSensor, InfraredSensor, RemoteControl, INPUT_1, INPUT_3, INPUT_4,
     Screen, Sound
 )
