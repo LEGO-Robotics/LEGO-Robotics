94c7e5f6203eab69c619f4067688ae0420c53fa7
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-29 12:54:08 -0700

    add Spik3r Program :P :B

--- EV3/Home-Edition/Core-Robots/Spik3r/Spik3r-1.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Spik3r/Spik3r-1.PyBricks.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Stop, Direction, Port
+from pybricks.media.ev3dev import SoundFile
+
+
+BRICK = EV3Brick()
+
+LARGE_MOTOR = Motor(port=Port.D,
+                    positive_direction=Direction.CLOCKWISE)
+                    
+
+LARGE_MOTOR.run_time(
+    time=1000,
+    speed=400,
+    then=Stop.HOLD,
+    wait=True)
+
+LARGE_MOTOR.run_angle(
+    speed=-750,
+    rotation_angle=220,
+    then=Stop.HOLD,
+    wait=True)
+
+BRICK.speaker.play_file(SoundFile.ERROR_ALARM)
+
+LARGE_MOTOR.run_time(
+    speed=-1000,
+    time=1000,
+    then=Stop.HOLD,
+    wait=True)
+
+LARGE_MOTOR.run_time(
+    speed=1000,
+    time=1000,
+    then=Stop.HOLD,
+    wait=True)
