7e361352200d18ea37c47e74538f31a6ae2a157e
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-30 10:32:13 -0700

    add Spik3r program

--- EV3/Home-Edition/Core-Robots/Spik3r/Spik3r-4.EV3Dev1.py
+++ EV3/Home-Edition/Core-Robots/Spik3r/Spik3r-4.EV3Dev1.py
@@ -1,3 +1,97 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, MediumMotor, LargeMotor, OUTPUT_B, OUTPUT_A, OUTPUT_D,
+    INPUT_4, InfraredSensor, Sound
+)   
+
+
+MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
+STING_MOTOR = LargeMotor(address=OUTPUT_D)
+GO_MOTOR = LargeMotor(address=OUTPUT_B)
+
+INFRARED_SENSOR = InfraredSensor(address=INPUT_4)
+
+SPEAKER = Sound()
+
+
+MEDIUM_MOTOR.run_timed(
+    speed_sp=500,
+    time_sp=1000,
+    stop_action=Motor.STOP_ACTION_HOLD)
+MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+MEDIUM_MOTOR.run_timed(
+    speed_sp=500,
+    time_sp=0.3 * 1000,
+    stop_action=Motor.STOP_ACTION_HOLD)
+MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+STING_MOTOR.run_timed(
+    speed_sp=400,
+    time_sp=0.3 * 1000,
+    stop_action=Motor.STOP_ACTION_HOLD)
+STING_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+for i in range(3):
+    GO_MOTOR.run_forever(speed_sp=1000)
+
+    SPEAKER.play(wav_file='/home/robot/sound/Blip 2.wav')
+
+    while INFRARED_SENSOR.proximity >= 40:
+        pass
+
+    GO_MOTOR.run_forever(speed_sp=250)
+
+    SPEAKER.play(wav_file='/home/robot/sound/Blip 4.wav')
+
+    STING_MOTOR.run_to_rel_pos(
+        speed_sp=-750,
+        position_sp=220,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    STING_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    SPEAKER.play(wav_file='/home/robot/sound/Blip 4.wav')
+    
+    STING_MOTOR.run_timed(
+        speed_sp=-1000,
+        time_sp=1000,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    STING_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    STING_MOTOR.run_timed(
+        speed_sp=400,
+        time_sp=0.3 * 1000,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    STING_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    MEDIUM_MOTOR.run_timed(
+        speed_sp=750,
+        time_sp=0.2 * 1000,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    MEDIUM_MOTOR.run_timed(
+        speed_sp=500,
+        time_sp=0.3 * 1000,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    GO_MOTOR.run_to_rel_pos(
+        speed_sp=-1000,
+        position_sp=2,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    STING_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    STING_MOTOR.run_timed(
+        speed_sp=1000,
+        time_sp=1000,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    STING_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    GO_MOTOR.run_to_rel_pos(
+        speed_sp=1000,
+        position_sp=2,
+        stop_action=Motor.STOP_ACTION_HOLD)
+    STING_MOTOR.wait_while(Motor.STATE_RUNNING)
