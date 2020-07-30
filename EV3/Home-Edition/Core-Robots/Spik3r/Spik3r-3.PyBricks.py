ed6a01b547d63c64f89cc9d418677cb9eb16fa4b
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-07-29 21:12:49 -0700

    add spik3r program

--- EV3/Home-Edition/Core-Robots/Spik3r/Spik3r-3.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Spik3r/Spik3r-3.PyBricks.py
@@ -1,3 +1,66 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Stop, Direction, Port
+from pybricks.media.ev3dev import SoundFile
+
+
+BRICK = EV3Brick()
+
+STING_MOTOR = Motor(port=Port.D,
+                    positive_direction=Direction.CLOCKWISE)
+                
+GO_MOTOR = Motor(port=Port.B,   
+                 positive_direction=Direction.CLOCKWISE)
+
+MEDIUM_MOTOR = Motor(port=Port.A,
+                     positive_direction=Direction.CLOCKWISE)
+
+
+MEDIUM_MOTOR.run_time(
+    speed=400,
+    time=1000,
+    then=Stop.HOLD,
+    wait=True)
+
+MEDIUM_MOTOR.run_time(
+    speed=-400,
+    time=0.3000,
+    then=Stop.HOLD,
+    wait=True)
+
+for i in range(2):
+    for i in range(3):
+        MEDIUM_MOTOR.run_time(
+            speed=750,
+            time=0.2000,
+            then=Stop.HOLD,
+            wait=True)
+
+        MEDIUM_MOTOR.run_time(
+            speed=-750,
+            time=0.2000,
+            then=Stop.HOLD,
+            wait=True)
+
+    GO_MOTOR.run_angle(
+        speed=1000,
+        rotation_angle=3 * 360,
+        then=Stop.HOLD,
+        wait=True)
+
+    BRICK.speaker.play_file(file=SoundFile.ERROR_ALARM)
+
+    GO_MOTOR.run_angle(
+        speed=1000,
+        rotation_angle=-3 * 360,
+        then=Stop.HOLD,
+        wait=True)
+
+    BRICK.speaker.play_file(file=SoundFile.ERROR_ALARM)
+
+    GO_MOTOR.run(speed=250)
+    
+    BRICK.speaker.play_file(file=SoundFile.ERROR_ALARM)
