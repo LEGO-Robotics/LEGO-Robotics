3f64419dc1100ae4f5e19bb0f1ce38310b1ecb41
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-14 10:23:11 -0700

    add programs

--- EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-2.EV3Dev1.py
+++ EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-2.EV3Dev1.py
@@ -1,3 +1,61 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import(
+    Motor, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_D,
+    INPUT_4, InfraredSensor,
+    Sound
+) 
+   
+
+from time import sleep
+
+
+LARGE_MOTOR = LargeMotor(address=OUTPUT_D)
+MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
+
+IR_SENSOR = InfraredSensor(address=INPUT_4)
+
+SPEAKER = Sound()
+
+
+def rattle():
+    MEDIUM_MOTOR.run_timed(
+        speed_sp=100,
+        time_sp=1000,
+        stop_action=Motor.STOP_ACTION_COAST)
+    MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    SPEAKER.play(wav_file='/home/robot/sound/Snake rattle.wav').wait()
+
+    MEDIUM_MOTOR.run_timed(
+        speed_sp=-100,
+        time_sp=1000,
+        stop_action=Motor.STOP_ACTION_COAST)
+    MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    sleep(1)
+
+
+def scare_people():
+    if IR_SENSOR.proximity < 30:
+        SPEAKER.play(wav_file='/home/robot/sound/Snake hiss.wav').wait()
+
+        LARGE_MOTOR.run_timed(
+            speed_sp=1000,
+            time_sp=1000,
+            stop_action=Motor.STOP_ACTION_COAST)
+        LARGE_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+    else:
+        LARGE_MOTOR.run_timed(
+            speed_sp=-300,
+            time_sp=1000,
+            stop_action=Motor.STOP_ACTION_COAST)
+        LARGE_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+
+while True:
+    scare_people()
+
+    rattle()
--- EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-2.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-2.PyBricks.py
@@ -1,3 +1,61 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, InfraredSensor    
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Direction, Port, Stop    
+
+from time import sleep
+
+
+BRICK = EV3Brick()
+
+LARGE_MOTOR = Motor(port=Port.D,
+                    positive_direction=Direction.CLOCKWISE)
+MEDIUM_MOTOR = Motor(port=Port.A,
+                     positive_direction=Direction.CLOCKWISE)
+
+IR_SENSOR = InfraredSensor(port=Port.S4)
+
+
+def rattle():
+    MEDIUM_MOTOR.run_time(
+        speed=100,
+        time=1000,
+        then=Stop.COAST,
+        wait=True)
+        
+    BRICK.speaker.play_file(file=SoundFile.SNAKE_RATTLE)
+
+    MEDIUM_MOTOR.run_time(
+        speed=-100,
+        time=1000,
+        then=Stop.COAST,
+        wait=True)
+
+    sleep(1)
+
+
+def scare_people():
+    if IR_SENSOR.distance() < 30:
+        BRICK.speaker.play_file(file=SoundFile.SNAKE_HISS)
+
+        LARGE_MOTOR.run_time(
+            speed=1000,
+            time=1000,
+            then=Stop.COAST,
+            wait=True)
+
+    else:
+        LARGE_MOTOR.run_time(
+            speed=-300,
+            time=1000,
+            then=Stop.COAST,
+            wait=True)
+
+
+while True:
+    scare_people()
+
+    rattle()
