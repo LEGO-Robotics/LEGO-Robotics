8997fe04d31f8641ae0bdbaf1433b7038f384bd3
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-13 19:39:49 -0700

    add program

--- EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-1.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-1.PyBricks.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port, Direction, Stop 
+
+
+LARGE_MOTOR = Motor(port=Port.B,
+                    positive_direction=Direction.CLOCKWISE)
+
+MEDIUM_MOTOR = Motor(port=Port.A,
+                     positive_direction=Direction.CLOCKWISE)
+
+
+LARGE_MOTOR.run(speed=1000)
+
+for i in range(3):
+    MEDIUM_MOTOR.run_time(
+        speed=100,
+        time=1000,
+        then=Stop.COAST,
+        wait=True)
+
+    MEDIUM_MOTOR.run_time(
+        speed=-100,
+        time=1000,
+        then=Stop.COAST,
+        wait=True)
