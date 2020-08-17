6e44b81983d94c45b73caa4ab035c1286b667c5b
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-16 22:10:01 -0700

    add program

--- EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-Super.EV3Dev2.py
+++ EV3/Home-Edition/Core-Robots/R3ptar/R3ptar-Super.EV3Dev2.py
@@ -1,159 +1,175 @@
 #!/usr/bin/env micropython
 
 
-from ev3dev2.motor import MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D
-from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D
+from ev3dev2.sensor import INPUT_1, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor
 from ev3dev2.sound import Sound
 
 
-CHEST_MOTOR = LargeMotor(address=OUTPUT_D)
-TAIL_MOTOR = LargeMotor(address=OUTPUT_B)
-MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
+class R3ptar:
+    def __init__(
+            self,
+            turn_motor_port: str = OUTPUT_A,
+            move_motor_port: str = OUTPUT_B,
+            scare_motor_port: str = OUTPUT_D,
+            touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
+        self.turn_motor = MediumMotor(address=turn_motor_port)
+        self.move_motor = LargeMotor(address=move_motor_port)
+        self.scare_motor = LargeMotor(address=scare_motor_port)
 
-TOUCH_SENSOR = TouchSensor(address=INPUT_1)
-COLOR_SENSOR = ColorSensor(address=INPUT_2)
-IR_SENSOR = InfraredSensor(address=INPUT_4)
+        self.touch_sensor = TouchSensor(address=touch_sensor_port)
+        self.color_sensor = ColorSensor(address=color_sensor_port)
 
-SPEAKER = Sound()
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
 
-
-def drive_once_by_ir_beacon(channel: int = 1, speed: float = 100):
-    if IR_SENSOR.top_left(channel) and IR_SENSOR.top_right(channel):
-        TAIL_MOTOR.on(
-            speed=speed,
-            brake=False,
-            block=False)
-
-    elif IR_SENSOR.bottom_left(channel) and IR_SENSOR.bottom_right(channel):
-        TAIL_MOTOR.on(
-            speed=-speed,
-            brake=False,
-            block=False)
-
-    elif IR_SENSOR.top_left(channel):
-        MEDIUM_MOTOR.on(
-            speed=-10,
-            brake=False,
-            block=False)
-
-        TAIL_MOTOR.on(
-            speed=speed,
-            brake=False,
-            block=False)
-
-    elif IR_SENSOR.top_right(channel):
-        MEDIUM_MOTOR.on(
-            speed=10,
-            brake=False,
-            block=False)
-
-        TAIL_MOTOR.on(
-            speed=speed,
-            brake=False,
-            block=False)
-
-    elif IR_SENSOR.bottom_left(channel):
-        MEDIUM_MOTOR.on(
-            speed=-10,
-            brake=False,
-            block=False)
-
-        TAIL_MOTOR.on(
-            speed=-speed,
-            brake=False,
-            block=False)
-
-    elif IR_SENSOR.bottom_right(channel):
-        MEDIUM_MOTOR.on(
-            speed=10,
-            brake=False,
-            block=False)
-
-        TAIL_MOTOR.on(
-            speed=-speed,
-            brake=False,
-            block=False)
-
-    else:
-        MEDIUM_MOTOR.off(brake=True)
-        TAIL_MOTOR.off(brake=False)
+        self.noise = Sound()
 
 
-def bite_by_ir_beacon(channel: int = 1, speed: float = 100):
-    if IR_SENSOR.beacon(channel):
-        CHEST_MOTOR.on_for_seconds(
-            speed=speed,
-            seconds=1,
-            brake=True,
-            block=False)
-
-        SPEAKER.play_file(
-            wav_file='/home/robot/sound/Snake hiss.wav',
-            volume=100,
-            play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
-
-        CHEST_MOTOR.on_for_seconds(
-            speed=-speed,
-            seconds=1,
-            brake=True,
-            block=True)
-
-        while IR_SENSOR.beacon(channel=1):
-            pass
-
-
-def run_away_if_chased():
-    if COLOR_SENSOR.reflected_light_intensity > 30:
-        TAIL_MOTOR.on_for_seconds(
-            speed=50,
-            seconds=4,
-            brake=True,
-            block=False)
-
-        for i in range(2):
-            MEDIUM_MOTOR.on_for_seconds(
-                speed=10,
-                seconds=1,
+    def drive_once_by_ir_beacon(self, speed: float = 100):
+        if self.ir_sensor.top_left(channel=self.ir_beacon_channel) and \
+                self.ir_sensor.top_right(channel=self.ir_beacon_channel):
+            self.move_motor.on(
+                speed=speed,
                 brake=False,
+                block=False)
+
+        elif self.ir_sensor.bottom_left(channel=self.ir_beacon_channel) and \
+                self.ir_sensor.bottom_right(channel=self.ir_beacon_channel):
+            self.move_motor.on(
+                speed=-speed,
+                brake=False,
+                block=False)
+
+        elif self.ir_sensor.top_left(channel=self.ir_beacon_channel):
+            self.turn_motor.on(
+                speed=-50,
+                brake=False,
+                block=False)
+
+            self.move_motor.on(
+                speed=speed,
+                brake=False,
+                block=False)
+
+        elif self.ir_sensor.top_right(channel=self.ir_beacon_channel):
+            self.turn_motor.on(
+                speed=50,
+                brake=False,
+                block=False)
+
+            self.move_motor.on(
+                speed=speed,
+                brake=False,
+                block=False)
+
+        elif self.ir_sensor.bottom_left(channel=self.ir_beacon_channel):
+            self.turn_motor.on(
+                speed=-50,
+                brake=False,
+                block=False)
+
+            self.move_motor.on(
+                speed=-speed,
+                brake=False,
+                block=False)
+
+        elif self.ir_sensor.bottom_right(channel=self.ir_beacon_channel):
+            self.turn_motor.on(
+                speed=50,
+                brake=False,
+                block=False)
+
+            self.move_motor.on(
+                speed=-speed,
+                brake=False,
+                block=False)
+
+        else:
+            self.turn_motor.off(brake=True)
+
+            self.move_motor.off(brake=False)
+
+
+    def bite_by_ir_beacon(self, speed: float = 100):
+        if self.ir_sensor.beacon(channel=self.ir_beacon_channel):
+            self.scare_motor.on_for_seconds(
+                speed=speed,
+                seconds=1,
+                brake=True,
+                block=False)
+
+            self.noise.play_file(
+                wav_file='/home/robot/sound/Snake hiss.wav',
+                volume=100,
+                play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+
+            self.scare_motor.on_for_seconds(
+                speed=-speed,
+                seconds=1,
+                brake=True,
                 block=True)
 
-            MEDIUM_MOTOR.on_for_seconds(
+            while self.ir_sensor.beacon(channel=self.ir_beacon_channel):
+                pass
+
+
+    def run_away_if_chased(self):
+        if self.color_sensor.reflected_light_intensity > 30:
+            self.move_motor.on_for_seconds(
+                speed=50,
+                seconds=4,
+                brake=True,
+                block=False)
+
+            for i in range(2):
+                self.turn_motor.on_for_seconds(
+                    speed=50,
+                    seconds=1,
+                    brake=False,
+                    block=True)
+
+                self.turn_motor.on_for_seconds(
+                    speed=-50,
+                    seconds=1,
+                    brake=False,
+                    block=True)
+
+
+    def bite_if_touched(self):
+        if self.touch_sensor.is_pressed:
+            self.scare_motor.on_for_seconds(
+                speed=100,
+                seconds=1,
+                brake=True,
+                block=False)
+
+            self.noise.play_file(
+                wav_file='/home/robot/sound/Snake hiss.wav',
+                volume=100,
+                play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+
+            self.scare_motor.on_for_seconds(
                 speed=-10,
-                seconds=1,
-                brake=False,
+                seconds=10,
+                brake=True,
                 block=True)
 
 
-def bite_if_touched():
-    if TOUCH_SENSOR.is_pressed:
-        CHEST_MOTOR.on_for_seconds(
-            speed=100,
-            seconds=1,
-            brake=True,
-            block=False)
+    def main(self, speed: float = 100):
+        while True:
+            self.drive_once_by_ir_beacon(speed=speed)
 
-        SPEAKER.play_file(
-            wav_file='/home/robot/sound/Snake hiss.wav',
-            volume=100,
-            play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+            self.bite_by_ir_beacon(speed=speed)
 
-        CHEST_MOTOR.on_for_seconds(
-            speed=-10,
-            seconds=10,
-            brake=True,
-            block=True)
+            self.bite_if_touched()
+                
+            self.run_away_if_chased()
 
 
-while True:
-    drive_once_by_ir_beacon(
-        channel=1,
-        speed=100)
-
-    bite_by_ir_beacon(
-        channel=1,
-        speed=100)
-
-    bite_if_touched()
-    
-    run_away_if_chased()
+if __name__ == '__main__':
+    R3PTAR = R3ptar()
+        
+    R3PTAR.main(speed=100)
