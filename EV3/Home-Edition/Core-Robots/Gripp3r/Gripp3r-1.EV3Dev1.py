5ae466e761e883407d4d7afd2ebbcf2e791cab9e
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-19 16:40:31 -0700

    add program

--- EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-1.EV3Dev1.py
+++ EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-1.EV3Dev1.py
@@ -2,7 +2,7 @@
 
 
 from ev3dev.ev3 import (
-    MediumMotor, OUTPUT_A,
+    Motor, MediumMotor, OUTPUT_A,
     Sound
 )
 
@@ -13,3 +13,27 @@ MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
 SPEAKER = Sound()
 
 
+MEDIUM_MOTOR.run_timed(
+    speed_sp=-500,
+    time_sp=1000,
+    stop_action=Motor.STOP_ACTION_BRAKE)
+MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+
+SPEAKER.play(wav_file='/home/robot/sound/Airbrake.wav')
+
+MEDIUM_MOTOR.run_timed(
+    speed_sp=500,
+    time_sp=1000,
+    stop_action=Motor.STOP_ACTION_BRAKE)
+MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+sleep(1)
+
+SPEAKER.play(wav_file='/home/robot/sound/Air release.wav')
+
+MEDIUM_MOTOR.run_timed(
+    speed_sp=-500,
+    time_sp=1000,
+    stop_action=Motor.STOP_ACTION_BRAKE)
+MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
--- EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-1.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-1.PyBricks.py
@@ -4,12 +4,36 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
 from pybricks.media.ev3dev import SoundFile
-from pybricks.parameters import Port, Stop
+from pybricks.parameters import Port, Stop, Direction
 
 from time import sleep
 
 
 EV3_BRICK = EV3Brick()
-MEDIUM_MOTOR = Motor(port=Port.A)
+MEDIUM_MOTOR = Motor(port=Port.A,
+                     positive_direction=Direction.COUNTERCLOCKWISE)
 
 
+MEDIUM_MOTOR.run_time(
+    speed=-500,
+    time=1000,
+    then=Stop.BRAKE,
+    wait=True)
+
+EV3_BRICK.speaker.play_file(file=SoundFile.AIRBRAKE)
+
+MEDIUM_MOTOR.run_time(
+    speed=500,
+    time=1000,
+    then=Stop.BRAKE,
+    wait=True)
+
+sleep(1)
+
+EV3_BRICK.speaker.play_file(file=SoundFile.AIR_RELEASE)
+
+MEDIUM_MOTOR.run_time(
+    speed=500,
+    time=1000,
+    then=Stop.BRAKE,
+    wait=True)
