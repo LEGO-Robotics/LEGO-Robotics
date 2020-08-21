88b43b38ce2f945f5b8aae2c362558bd491b5e13
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-20 20:47:07 -0700

    update program

--- EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-4.EV3Dev2.py
+++ EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-4.EV3Dev2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env micropython
 
 
-from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
-from ev3dev2.sensor import INPUT_1, INPUT_4
+from ev3dev2.motor import LargeMotor, MediumMotor, MoveSteering, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor import INPUT_4
 from ev3dev2.sensor.lego import InfraredSensor, TouchSensor
 from ev3dev2.sound import Sound
 
@@ -11,15 +11,16 @@ MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
 TANK_DRIVER = MoveTank(left_motor_port=OUTPUT_B,
                        right_motor_port=OUTPUT_C,
                        motor_class=LargeMotor)
+STEER_DRIVER = MoveSteering(left_motor_port=OUTPUT_B,
+                            right_motor_port=OUTPUT_C,
+                            motor_class=LargeMotor)
 
-TOUCH_SENSOR = TouchSensor(address=INPUT_1)
 IR_SENSOR = InfraredSensor(address=INPUT_4)
 
 SPEAKER = Sound()
 
-
 def drive_once_by_ir_beacon(channel: int = 1, speed: float = 100):
-    if IR_SENSOR.top_left(channel=channel) and IR_SENSOR.top_right(channel=channel):
+    if IR_SENSOR.top_right(channel=channel) and IR_SENSOR.top_right(channel=channel):
         # go forward
         TANK_DRIVER.on(
             left_speed=speed,
