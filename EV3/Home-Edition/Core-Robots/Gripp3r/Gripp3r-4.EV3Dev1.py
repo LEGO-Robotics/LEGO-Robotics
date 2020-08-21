70bb10214159c058c0599ea380949c4caa7e0df8
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-20 22:11:52 -0700

    add programs

--- EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-4.EV3Dev1.py
+++ EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-4.EV3Dev1.py
@@ -1,3 +1,89 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, InfraredSensor, RemoteControl, INPUT_1, INPUT_4,
+    Sound
+)
+
+
+MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
+LEFT_MOTOR = LargeMotor(address=OUTPUT_B)
+RIGHT_MOTOR = LargeMotor(address=OUTPUT_C)
+
+TOUCH_SENSOR = TouchSensor(address=INPUT_1)
+IR_SENSOR = InfraredSensor(address=INPUT_4)
+BEACON_CONTROL = RemoteControl(sensor=IR_SENSOR,
+                               channel=1)
+
+SPEAKER = Sound() 
+
+
+def drive_once_by_ir_beacon(speed: float = 1000):
+    if BEACON_CONTROL.red_up and BEACON_CONTROL.blue_up:
+        # go forward
+        LEFT_MOTOR.run_forever(speed_sp=speed)
+        RIGHT_MOTOR.run_forever(speed_sp=speed)
+    
+    elif BEACON_CONTROL.red_down and BEACON_CONTROL.blue_down:
+        # go backward
+        LEFT_MOTOR.run_forever(speed_sp=-speed)
+        RIGHT_MOTOR.run_forever(speed_sp=-speed)
+
+    elif BEACON_CONTROL.red_up and BEACON_CONTROL.blue_down:
+        # turn around left
+        LEFT_MOTOR.run_forever(speed_sp=-speed)
+        RIGHT_MOTOR.run_forever(speed_sp=speed)
+
+    elif BEACON_CONTROL.red_down and BEACON_CONTROL.blue_up:
+        # turn around right
+        LEFT_MOTOR.run_forever(speed_sp=speed)
+        RIGHT_MOTOR.run_forever(speed_sp=-speed)
+
+    elif BEACON_CONTROL.red_up:
+        # turn left
+        LEFT_MOTOR.run_forever(speed_sp=0)
+        RIGHT_MOTOR.run_forever(speed_sp=speed)
+
+    elif BEACON_CONTROL.blue_up:
+        # turn right
+        LEFT_MOTOR.run_forever(speed_sp=speed)
+        RIGHT_MOTOR.run_forever(speed_sp=0)
+
+    elif BEACON_CONTROL.red_down:
+        # left backward
+        LEFT_MOTOR.run_forever(speed_sp=0)
+        RIGHT_MOTOR.run_forever(speed_sp=-speed)
+
+    elif BEACON_CONTROL.blue_down:
+        # right backward
+        LEFT_MOTOR.run_forever(speed_sp=-speed)
+        RIGHT_MOTOR.run_forever(speed_sp=0)
+
+    else:
+        LEFT_MOTOR.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+        RIGHT_MOTOR.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+
+
+while True:
+    drive_once_by_ir_beacon(speed=1000)
+
+    if BEACON_CONTROL.beacon:
+        if TOUCH_SENSOR.is_pressed:
+            MEDIUM_MOTOR.run_timed(
+                speed_sp=500,
+                time_sp=1000,
+                stop_action=Motor.STOP_ACTION_BRAKE)
+            MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+        else:
+            MEDIUM_MOTOR.run_forever(speed_sp=-500)
+
+            while not TOUCH_SENSOR.is_pressed:
+                pass
+
+            MEDIUM_MOTOR.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+
+        while BEACON_CONTROL.beacon:
+            pass
--- EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-4.PyBricks.py
+++ EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-4.PyBricks.py
@@ -1,3 +1,121 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, InfraredSensor
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Button, Direction, Port, Stop
+from pybricks.robotics import DriveBase
+
+
+EV3_BRICK = EV3Brick()
+
+MEDIUM_MOTOR = Motor(port=Port.A, 
+                     positive_direction=Direction.CLOCKWISE)                     
+
+LEFT_MOTOR = Motor(port=Port.B,
+                   positive_direction=Direction.CLOCKWISE)
+RIGHT_MOTOR = Motor(port=Port.C,
+                    positive_direction=Direction.CLOCKWISE)
+DRIVE_BASE = DriveBase(left_motor=LEFT_MOTOR,
+                       right_motor=RIGHT_MOTOR,
+                       wheel_diameter=26,
+                       axle_track=115)
+DRIVE_BASE.settings(
+    straight_speed=750,   # milimeters per second
+    straight_acceleration=750,
+    turn_rate=90,   # degrees per second
+    turn_acceleration=90)
+
+TOUCH_SENSOR = TouchSensor(port=Port.S1)
+IR_SENSOR = InfraredSensor(port=Port.S4)
+
+
+def drive_once_by_ir_beacon(
+        channel: int = 1,
+        speed: float = 1000   # milimeters per second
+    ):
+    ir_beacon_buttons_pressed = set(IR_SENSOR.buttons(channel=channel))
+
+    # forward
+    if ir_beacon_buttons_pressed == {Button.LEFT_UP, Button.RIGHT_UP}:
+        DRIVE_BASE.drive(
+            speed=speed,
+            turn_rate=0   # degrees per second
+        )
+
+    # backward
+    elif ir_beacon_buttons_pressed == {Button.LEFT_DOWN, Button.RIGHT_DOWN}:
+        DRIVE_BASE.drive(
+            speed=-speed,
+            turn_rate=0   # degrees per second
+        )
+
+    # turn left on the spot
+    elif ir_beacon_buttons_pressed == {Button.LEFT_UP, Button.RIGHT_DOWN}:
+        DRIVE_BASE.drive(
+            speed=0,
+            turn_rate=-90   # degrees per second
+        )
+
+    # turn right on the spot
+    elif ir_beacon_buttons_pressed == {Button.LEFT_DOWN, Button.RIGHT_UP}:
+        DRIVE_BASE.drive(
+            speed=0,
+            turn_rate=90   # degrees per second
+        )
+
+    # turn left forward
+    elif ir_beacon_buttons_pressed == {Button.LEFT_UP}:
+        DRIVE_BASE.drive(
+            speed=speed,
+            turn_rate=-90   # degrees per second
+        )
+
+    # turn right forward
+    elif ir_beacon_buttons_pressed == {Button.RIGHT_UP}:
+        DRIVE_BASE.drive(
+            speed=speed,
+            turn_rate=90   # degrees per second
+        )
+
+    # turn left backward
+    elif ir_beacon_buttons_pressed == {Button.LEFT_DOWN}:
+        DRIVE_BASE.drive(
+            speed=-speed,
+            turn_rate=90   # degrees per second
+        )
+
+    # turn right backward
+    elif ir_beacon_buttons_pressed == {Button.RIGHT_DOWN}:
+        DRIVE_BASE.drive(
+            speed=-speed,
+            turn_rate=-90   # degrees per second
+        )
+
+    # otherwise stop
+    else:
+        DRIVE_BASE.stop()
+
+
+while True:
+    drive_once_by_ir_beacon(speed=1000)
+
+    if Button.BEACON in IR_SENSOR.buttons(channel=1):
+        if TOUCH_SENSOR.pressed():
+            MEDIUM_MOTOR.run_time(
+                speed=500,
+                time=1000,
+                then=Stop.BRAKE,
+                wait=True)
+
+        else:
+            MEDIUM_MOTOR.run(speed=-500)
+
+            while not TOUCH_SENSOR.pressed():
+                pass
+
+            MEDIUM_MOTOR.stop()
+
+        while Button.BEACON in IR_SENSOR.buttons(channel=1):
+            pass
