1d697f9debbbeb2a530054d574ea56566417f61b
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-22 13:12:40 -0700

    add program

--- EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-Super.EV3Dev1.py
+++ EV3/Home-Edition/Core-Robots/Gripp3r/Gripp3r-Super.EV3Dev1.py
@@ -1,3 +1,119 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, InfraredSensor, RemoteControl, INPUT_1, INPUT_4,
+    Sound
+)
+
+
+class Gripp3r:
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
+            grip_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
+        self.left_motor = LargeMotor(address=left_motor_port)
+        self.right_motor = LargeMotor(address=right_motor_port)
+        self.grip_motor = MediumMotor(address=grip_motor_port)
+
+        self.touch_sensor = TouchSensor(address=touch_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+        self.beacon = RemoteControl(sensor=self.ir_sensor,
+                                    channel=ir_beacon_channel)
+
+        self.speaker = Sound()
+
+
+    def drive_once_by_ir_beacon(self, speed: float = 1000):
+        if self.beacon.red_up and self.beacon.blue_up:
+            # go forward
+            self.left_motor.run_forever(speed_sp=speed)
+            self.right_motor.run_forever(speed_sp=speed)
+        
+        elif self.beacon.red_down and self.beacon.blue_down:
+            # go backward
+            self.left_motor.run_forever(speed_sp=-speed)
+            self.right_motor.run_forever(speed_sp=-speed)
+
+        elif self.beacon.red_up and self.beacon.blue_down:
+            # turn around left
+            self.left_motor.run_forever(speed_sp=-speed)
+            self.right_motor.run_forever(speed_sp=speed)
+
+        elif self.beacon.red_down and self.beacon.blue_up:
+            # turn around right
+            self.left_motor.run_forever(speed_sp=speed)
+            self.right_motor.run_forever(speed_sp=-speed)
+
+        elif self.beacon.red_up:
+            # turn left
+            self.left_motor.run_forever(speed_sp=0)
+            self.right_motor.run_forever(speed_sp=speed)
+
+        elif self.beacon.blue_up:
+            # turn right
+            self.left_motor.run_forever(speed_sp=speed)
+            self.right_motor.run_forever(speed_sp=0)
+
+        elif self.beacon.red_down:
+            # left backward
+            self.left_motor.run_forever(speed_sp=0)
+            self.right_motor.run_forever(speed_sp=-speed)
+
+        elif self.beacon.blue_down:
+            # right backward
+            self.left_motor.run_forever(speed_sp=-speed)
+            self.right_motor.run_forever(speed_sp=0)
+
+        else:
+            self.left_motor.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+            self.right_motor.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+
+
+    def grip_or_release_by_ir_beacon(self, speed: float = 50):
+        if self.beacon.beacon:
+            if self.touch_sensor.is_pressed:
+                self.speaker.play(wav_file='/home/robot/sound/Air release.wav')
+
+                self.grip_motor.run_timed(
+                    speed_sp=500,
+                    time_sp=1000,
+                    stop_action=Motor.STOP_ACTION_BRAKE)
+                self.grip_motor.wait_while(Motor.STATE_RUNNING)
+
+            else:
+                self.speaker.play(wav_file='/home/robot/sound/Airbrake.wav')
+
+                self.grip_motor.run_forever(speed_sp=-500)
+
+                while not self.touch_sensor.is_pressed:
+                    pass
+        
+                self.grip_motor.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+
+            while self.beacon.beacon:
+                pass
+
+        
+    def main(self, speed: float = 1000):
+        self.grip_motor.run_timed(
+            speed_sp=-500,
+            time_sp=1000,
+            stop_action=Motor.STOP_ACTION_BRAKE)
+        self.grip_motor.wait_while(Motor.STATE_RUNNING)
+
+        while True:
+            self.drive_once_by_ir_beacon(speed=speed)
+
+            self.grip_or_release_by_ir_beacon()
+            
+
+if __name__ == '__main__':
+    GRIPP3R = Gripp3r()
+
+    GRIPP3R.main()
