d5281a017aacaca8d425773bfbd99420d4cb4ac8
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-30 19:34:08 -0700

    add Curiosity-Rov3r-RCTank.EV3Dev1 programs

---
+++ EV3/Antoni-Bots/Curiosity-Rov3r/Curiosity-Rov3r-RCTank.EV3Dev1.MultiProcessing.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+
+from ev3dev.ev3 import (
+    Motor, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, ColorSensor, InfraredSensor, RemoteControl, INPUT_1, INPUT_3, INPUT_4,
+    Sound, Screen
+)
+from ev3dev.helper import RemoteControlledTank
+
+from multiprocessing import Process
+from PIL import Image
+
+
+class CuriosityRov3r(RemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
+            medium_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
+        super().__init__(
+            left_motor=left_motor_port, right_motor=right_motor_port,
+            polarity='normal')
+            
+        self.medium_motor = MediumMotor(address=medium_motor_port)
+
+        self.touch_sensor = TouchSensor(address=touch_sensor_port)
+        self.color_sensor = ColorSensor(address=color_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+        self.beacon = RemoteControl(sensor=self.ir_sensor,
+                                    channel=ir_beacon_channel)
+
+        self.dis = Screen()
+        self.noise = Sound()
+
+    
+    def spin_fan(self, speed: float = 1000):
+        while True:
+            if self.color_sensor.reflected_light_intensity > 20:
+                self.medium_motor.run_forever(speed_sp=speed)
+
+            else:
+                self.medium_motor.stop(stop_action=Motor.STOP_ACTION_HOLD)
+
+            
+    def say_when_touched(self):
+        while True:
+            if self.touch_sensor.is_pressed:
+                self.dis.image.paste(im=Image.open('/home/robot/image/Angry.bmp'))
+                self.dis.update()
+
+                self.noise.play(wav_file='/home/robot/sound/No.wav').wait()
+
+                self.medium_motor.run_timed(
+                    speed_sp=-500,
+                    time_sp=3000,
+                    stop_action=Motor.STOP_ACTION_BRAKE)
+                self.medium_motor.wait_while(Motor.STATE_RUNNING)
+
+
+    def main(self, speed: float = 1000):
+        Process(target=self.say_when_touched,
+                daemon=True).start()
+
+        Process(target=self.spin_fan,
+                daemon=True).start()
+
+        super().main()   # RemoteControlledTank.main()
+            
+
+if __name__ == '__main__':
+    CURIOSITY_ROV3R = CuriosityRov3r()
+
+    CURIOSITY_ROV3R.main()
