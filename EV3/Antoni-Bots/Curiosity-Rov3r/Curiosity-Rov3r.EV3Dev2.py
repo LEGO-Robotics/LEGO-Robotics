e4d48d331c6d84035182434ba014759b0c2d1398
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-08-30 18:49:39 -0700

    add program

---
+++ EV3/Antoni-Bots/Curiosity-Rov3r/Curiosity-Rov3r.EV3Dev2.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor import INPUT_1, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor
+from ev3dev2.display import Display
+from ev3dev2.sound import Sound
+
+
+import os
+import sys
+sys.path.append(os.path.expanduser('~'))
+from util.drive_util_ev3dev2 import IRBeaconRemoteControlledTank
+
+
+class Curiosity_Rov3r(IRBeaconRemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
+            medium_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
+        super().__init__(
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+            
+        self.medium_motor = MediumMotor(address=medium_motor_port)
+
+        self.touch_sensor = TouchSensor(address=touch_sensor_port)
+        self.color_sensor = ColorSensor(address=color_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+
+        self.dis = Display()
+
+        self.noise = Sound()
+
+    
+    def spin_fan(self, speed: 1000):
+        if self.color_sensor.reflected_light_intensity > 20:
+            self.medium_motor.on(
+                speed=speed,
+                brake=True,
+                block=True)
+
+        elif self.color_sensor.reflected_light_intensity < 20:
+            self.medium_motor.off(brake=True)
+            
+    def say_when_touched(self):
+        if self.touch_sensor.is_pressed:
+            self.dis.image_filename(
+                filename='/home/robot/image/Angry.bmp',
+                clear_screen=True)
+            self.dis.update()
+
+            self.noise.play_file(
+                wav_file='/home/robot/sound/No.wav',
+                volume=100,
+                play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+            self.medium_motor.run_time(
+                speed_sp=-500,
+                time_sp=3000,
+                stop_action=Motor.STOP_ACTION_BRAKE)
+            self.medium_motor.wait_while(Motor.STATE_RUNNING)
+
+
+    def main(self, speed: float = 1000):
+        while True:
+            self.drive_once_by_ir_beacon(speed=speed)
+
+            self.say_when_touched()
+
+            self.spin_fan(speed=speed)
+            
+
+if __name__ == '__main__':
+    CURIOSITY_ROV3R = Curiosity_Rov3r()
+
+    CURIOSITY_ROV3R.main()
+
+
