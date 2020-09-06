93c41a55c61fd8b6e711e886a7b160e9097178d9
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-09-05 17:58:05 -0700

    add program

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev1.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev1.py
@@ -1,3 +1,54 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import Motor
+
+from dino3rex_ev3dev1 import Dinor3x
+
+
+DINOR3X = Dinor3x()
+
+
+DINOR3X.left_motor.run_forever(speed_sp=100)
+DINOR3X.right_motor.run_forever(speed_sp=200)
+
+while DINOR3X.touch_sensor.is_pressed:
+    pass
+
+DINOR3X.left_motor.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+DINOR3X.right_motor.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+
+DINOR3X.left_motor.run_forever(speed_sp=400)
+
+while not DINOR3X.touch_sensor.is_pressed:
+    pass
+
+DINOR3X.left_motor.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+
+DINOR3X.left_motor.run_to_rel_pos(
+    position_sp=-0.2 * 360,
+    speed_sp=500,
+    stop_action=Motor.STOP_ACTION_BRAKE)
+DINOR3X.left_motor.wait_while(Motor.STATE_RUNNING)
+
+DINOR3X.right_motor.run_forever(speed_sp=400)
+
+while not DINOR3X.touch_sensor.is_pressed:
+    pass
+
+DINOR3X.right_motor.stop(stop_action=Motor.STOP_ACTION_BRAKE)
+
+DINOR3X.right_motor.run_to_rel_pos(
+    position_sp=-0.2 * 360,
+    speed_sp=500,
+    stop_action=Motor.STOP_ACTION_BRAKE)
+DINOR3X.right_motor.wait_while(Motor.STATE_RUNNING)
+
+DINOR3X.left_motor.reset()
+DINOR3X.right_motor.reset()
+
+DINOR3X.left_motor.run_forever(speed_sp=-400)
+DINOR3X.right_motor.run_forever(speed_sp=-400)
+
+while True:
+    pass
--- EV3/Home-Edition/Fan-Robots/Dinor3x/dino3rex_ev3dev1.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/dino3rex_ev3dev1.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, InfraredSensor, RemoteControl, INPUT_1, INPUT_3, INPUT_4,
+    Sound
+)
+
+import os
+import sys
+sys.path.append(os.path.expanduser('~'))
+from util.drive_util_ev3dev1 import IRBeaconRemoteControlledTank
+
+
+class Dinor3x(IRBeaconRemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
+            jaw_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
+        super().__init__(
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+
+        self.jaw_motor = MediumMotor(address=jaw_motor_port)
+
+        self.touch_sensor = TouchSensor(address=touch_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.beacon = RemoteControl(sensor=self.ir_sensor,
+                                    channel=ir_beacon_channel)
+
+        self.speaker = Sound()
