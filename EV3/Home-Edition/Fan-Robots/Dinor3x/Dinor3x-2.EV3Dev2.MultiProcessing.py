c0d5081366b21664d61188e4c7d1d8d32ae59243
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-09-15 20:37:57 -0700

    add program

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-2.EV3Dev2.MultiProcessing.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-2.EV3Dev2.MultiProcessing.py
@@ -1,3 +1,44 @@
 #!/usr/bin/env micropython
 
 
+from multiprocessing import Process
+from time import sleep
+
+from dinor3x_ev3dev2 import Dinor3x
+
+
+DINOR3X = Dinor3x()
+
+
+DINOR3X.calibrate_legs()
+
+DINOR3X.jaw_motor.on(
+    speed=20,
+    block=False,
+    brake=False)
+
+sleep(1)
+
+DINOR3X.jaw_motor.off(brake=True)
+
+while True:
+    # recalibrate legs so that the legs don't get too tired
+    DINOR3X.calibrate_legs()
+
+    DINOR3X.steer_driver.on(
+        steering=0,
+        speed=-40)
+
+    while DINOR3X.ir_sensor.proximity >= 25:
+        pass
+
+    DINOR3X.steer_driver.off(brake=True)
+
+    Process(target=DINOR3X.roar).start()
+
+    DINOR3X.steer_driver.on_for_rotations(
+        speed=75,
+        steering=0,
+        rotations=3,
+        brake=True,
+        block=True)
