123a039b17cf15688134b6cd9bab8e1798ce6cb4
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-12 17:32:18 -0700

    add Mars-Rov3r-RCTank.EV3Dev1 programs

--- EV3/Antoni-Bots/Mars-Rov3r/Mars-Rov3r-RCTank.EV3Dev1.Threading.py
+++ EV3/Antoni-Bots/Mars-Rov3r/Mars-Rov3r-RCTank.EV3Dev1.Threading.py
@@ -1,3 +1,87 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, ColorSensor, INPUT_1, INPUT_3, INPUT_4,
+    Sound, RemoteControl, InfraredSensor
+)
+
+from ev3dev.helper import RemoteControlledTank
+
+from threading import Thread
+
+
+class MarsRov3r(RemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_B,
+            right_motor_port: str = OUTPUT_C,
+            grip_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
+        super().__init__(
+            left_motor=left_motor_port, right_motor=right_motor_port,
+            polarity=Motor.POLARITY_NORMAL)
+
+        self.grip_motor = MediumMotor(address=grip_motor_port)
+
+        self.touch_sensor = TouchSensor(address=touch_sensor_port)
+        self.color_sensor = ColorSensor(address=color_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+        self.beacon = RemoteControl(sensor=self.ir_sensor,
+                                    channel=ir_beacon_channel)
+
+        self.speaker = Sound()
+
+    is_gripping = False
+
+    def grip_or_release_claw_by_ir_beacon(self):
+        while True:
+            if self.beacon.beacon:
+                if self.is_gripping:
+                    self.grip_motor.run_timed(
+                        speed_sp=1000,
+                        time_sp=2000,
+                        stop_action=Motor.STOP_ACTION_BRAKE)
+                    self.grip_motor.wait_while(Motor.STATE_RUNNING)
+
+                    self.speaker.play(
+                        wav_file='/home/robot/sound/Air release.wav').wait()
+
+                    self.is_gripping = False
+
+                else:
+                    self.grip_motor.run_timed(
+                        speed_sp=-1000,
+                        time_sp=2000,
+                        stop_action=Motor.STOP_ACTION_BRAKE)
+                    self.grip_motor.wait_while(Motor.STATE_RUNNING)
+
+                    self.speaker.play(
+                        wav_file='/home/robot/sound/Airbrake.wav').wait()
+
+                    self.is_gripping = True
+
+                while self.beacon.beacon:
+                    pass
+
+    def main(self):
+        self.grip_motor.run_timed(
+            speed_sp=500,
+            time_sp=1000,
+            stop_action=Motor.STOP_ACTION_BRAKE)
+        self.grip_motor.wait_while(Motor.STATE_RUNNING)
+
+        Thread(target=self.grip_or_release_claw_by_ir_beacon,
+               daemon=True).start()
+
+        super().main()   # RemoteControlledTank.main()
+
+
+if __name__ == '__main__':
+    MARS_ROV3R = MarsRov3r()
+
+    MARS_ROV3R.main()
