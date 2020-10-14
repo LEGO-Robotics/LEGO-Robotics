f83d58f339f99281abb06bdd3da2a1880e5e9b37
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-13 18:25:31 -0700

    enhance drive_util_ev3dev with EV3Fast utils

--- util/drive_util_ev3dev1.py
+++ util/drive_util_ev3dev1.py
@@ -8,15 +8,23 @@ from ev3dev.ev3 import \
     Motor, LargeMotor, OUTPUT_B, OUTPUT_C, \
     InfraredSensor, RemoteControl, INPUT_4
 
+from .ev3dev_fast.ev3fast import LargeMotor as FastLargeMotor
+
 
 class IRBeaconRemoteControlledTank:
     def __init__(
             self,
             left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
             polarity: str = Motor.POLARITY_NORMAL,
-            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
-        self.left_motor = LargeMotor(address=left_motor_port)
-        self.right_motor = LargeMotor(address=right_motor_port)
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1,
+            fast=False):
+        if fast:
+            self.left_motor = FastLargeMotor(address=left_motor_port)
+            self.right_motor = FastLargeMotor(address=right_motor_port)
+        else:
+            self.left_motor = LargeMotor(address=left_motor_port)
+            self.right_motor = LargeMotor(address=right_motor_port)
+        
         self.left_motor.polarity = self.right_motor.polarity = polarity
 
         self.ir_sensor = InfraredSensor(address=ir_sensor_port)
