339ff0068ad1eb0a67276b9023a8dc51dbe2b8a0
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-13 18:26:20 -0700

    add Mars-Rov3r-1.EV3Dev.Fast programs

---
+++ EV3/Antoni-Bots/Mars-Rov3r/Mars-Rov3r-1.EV3Dev1.Fast.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+
+from ev3dev.ev3 import (
+    OUTPUT_A,
+    InfraredSensor, RemoteControl, INPUT_4,
+    Sound
+)
+
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))
+from util.ev3dev_fast.ev3fast import (   
+    Motor, LargeMotor, MediumMotor,
+    MotorSet, MoveTank, MoveSteering,
+    SpeedValue, SpeedPercent, SpeedNativeUnits,
+    SpeedRPS, SpeedRPM, SpeedDPS, SpeedDPM,
+    Sensor, TouchSensor, ColorSensor, UltrasonicSensor, GyroSensor
+)
+
+
+MEDIUM_MOTOR = MediumMotor(address=OUTPUT_A)
+
+IR_SENSOR = InfraredSensor(address=INPUT_4)
+BEACON_CONTROL = RemoteControl(sensor=IR_SENSOR,
+                               channel=1)
+
+SPEAKER = Sound()
+
+
+is_gripping = False
+
+MEDIUM_MOTOR.run_timed(
+    speed_sp=500,
+    time_sp=1000,
+    stop_action=Motor.STOP_ACTION_BRAKE)
+MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+
+while True:
+    if BEACON_CONTROL.beacon:
+        if is_gripping:
+            MEDIUM_MOTOR.run_timed(
+                speed_sp=1000,
+                time_sp=2000,
+                stop_action=Motor.STOP_ACTION_BRAKE)
+            MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+            SPEAKER.play(wav_file='/home/robot/sound/Air release.wav').wait()
+
+            is_gripping = False
+
+        else:
+            MEDIUM_MOTOR.run_timed(
+                speed_sp=-1000,
+                time_sp=2000,
+                stop_action=Motor.STOP_ACTION_BRAKE)
+            MEDIUM_MOTOR.wait_while(Motor.STATE_RUNNING)
+
+            SPEAKER.play(wav_file='/home/robot/sound/Airbrake.wav').wait()
+
+            is_gripping = True
+
+        while BEACON_CONTROL.beacon:
+            pass
