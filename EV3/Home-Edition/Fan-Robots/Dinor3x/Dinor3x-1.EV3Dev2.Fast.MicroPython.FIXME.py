736a0ac3f8d94b07ad22fd9f4c59d2656a181269
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-14 17:46:41 -0700

    add Dinor3x-1.EV3Dev2.Fast.MicroPython.FIXME program

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev2.Fast.MicroPython.FIXME.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev2.Fast.MicroPython.FIXME.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env micropython
 
 
+from dinor3x_ev3dev2 import Dinor3x
+
+
+DINOR3X = Dinor3x(fast=True)
+
+
+while not DINOR3X.button.any():
+    # recalibrate legs every 5 seconds so that the legs don't get too tired
+    DINOR3X.calibrate_legs()
+
+    DINOR3X.steer_driver.on_for_seconds(
+        steering=0,
+        speed=-40,
+        seconds=5,
+        brake=True,
+        block=True)
