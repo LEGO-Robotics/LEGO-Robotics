5c27f4972f448ba4dd33655d0827f5457e1f7da4
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-14 17:43:28 -0700

    add Dinor3x-1.EV3Dev1.Fast program

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev1.Fast.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev1.Fast.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import Motor
+
+from dinor3x_ev3dev1 import Dinor3x
+
+
+DINOR3X = Dinor3x(fast=True)
+
+
+while not DINOR3X.button.any():
+    # recalibrate legs every 5 seconds so that the legs don't get too tired
+    DINOR3X.calibrate_legs()
+
+    DINOR3X.left_motor.run_timed(
+        speed_sp=-400,
+        time_sp=5000,
+        stop_action=Motor.STOP_ACTION_BRAKE)
+    DINOR3X.right_motor.run_timed(
+        speed_sp=-400,
+        time_sp=5000,
+        stop_action=Motor.STOP_ACTION_BRAKE)
+    DINOR3X.left_motor.wait_while(Motor.STATE_RUNNING)
+    DINOR3X.right_motor.wait_while(Motor.STATE_RUNNING)
