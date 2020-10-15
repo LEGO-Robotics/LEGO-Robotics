34097dfdf8658b562c9ca0816cdb684d0f87af98
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-14 17:53:42 -0700

    add Dinor3x-2 EV3Dev Fast programs

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-2.EV3Dev2.Fast.Python3.MultiProcessing.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-2.EV3Dev2.Fast.Python3.MultiProcessing.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
 
+from multiprocessing import Process
+
+from dinor3x_ev3dev2 import Dinor3x
+
+
+DINOR3X = Dinor3x(fast=True)
+
+
+DINOR3X.calibrate_legs()
+
+DINOR3X.close_mouth()
+
+while True:
+    # recalibrate legs so that the legs don't get too tired
+    DINOR3X.calibrate_legs()
+
+    DINOR3X.walk_until_blocked()
+
+    Process(target=DINOR3X.roar).start()
+
+    DINOR3X.run_away()
