af1d63c645cadfe04e774144352a72e517a441c0
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-16 16:53:25 -0700

    simplify Dinor3x-1.PyBricks program

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.PyBricks.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.PyBricks.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env pybricks-micropython
 
 
-from pybricks.parameters import Button, Stop
+from pybricks.parameters import Button
 
 from dinor3x_pybricks import Dinor3x
 
 
 DINOR3X = Dinor3x()
 
+DINOR3X.calibrate_legs()
+
+DINOR3X.left_motor.run(speed=-400)
+DINOR3X.right_motor.run(speed=-400)
+
 while not {Button.LEFT, Button.RIGHT,
            Button.UP, Button.DOWN,
            Button.CENTER}.intersection(DINOR3X.buttons.pressed()):
-    DINOR3X.calibrate_legs()
-
-    DINOR3X.left_motor.run_time(
-        speed=-400,
-        time=5000,
-        then=Stop.BRAKE,
-        wait=False)
-    DINOR3X.right_motor.run_time(
-        speed=-400,
-        time=5000,
-        then=Stop.BRAKE,
-        wait=True)
+    pass
