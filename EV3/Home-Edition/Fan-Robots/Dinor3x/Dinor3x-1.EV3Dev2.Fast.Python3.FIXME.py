63f9ff32e869922ab585bdd421cd7a0e33be6de5
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-16 17:40:23 -0700

    Revert "simplify Dinor3x-1.EV3Dev2 programs"
    This reverts commit e658e7a1eb7c0b65fe7cf512a68f77cb4b5d6516.

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev2.Fast.Python3.FIXME.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.EV3Dev2.Fast.Python3.FIXME.py
@@ -6,11 +6,15 @@ from dinor3x_ev3dev2 import Dinor3x
 
 DINOR3X = Dinor3x(fast=True)
 
-DINOR3X.calibrate_legs()
-
-DINOR3X.steer_driver.on(
-    steering=0,
-    speed=-40)
 
 while not DINOR3X.button.any():
-    pass
+    # FIXME: FastTouchSensor doesn't seem to detect press/release
+    # recalibrate legs every 5 seconds so that the legs don't get too tired
+    DINOR3X.calibrate_legs()
+
+    DINOR3X.steer_driver.on_for_seconds(
+        steering=0,
+        speed=-40,
+        seconds=5,
+        brake=True,
+        block=True)
