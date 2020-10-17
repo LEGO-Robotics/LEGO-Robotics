4e02d107cf0b06dd15d54caab26b2457f2a33046
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-16 17:40:19 -0700

    Revert "simplify Dinor3x-1.PyBricks program"
    This reverts commit af1d63c645cadfe04e774144352a72e517a441c0.

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.PyBricks.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-1.PyBricks.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env pybricks-micropython
 
 
-from pybricks.parameters import Button
+from pybricks.parameters import Button, Stop
 
 from dinor3x_pybricks import Dinor3x
 
 
 DINOR3X = Dinor3x()
 
-DINOR3X.calibrate_legs()
-
-DINOR3X.left_motor.run(speed=-400)
-DINOR3X.right_motor.run(speed=-400)
-
 while not {Button.LEFT, Button.RIGHT,
            Button.UP, Button.DOWN,
            Button.CENTER}.intersection(DINOR3X.buttons.pressed()):
-    pass
+    DINOR3X.calibrate_legs()
+
+    DINOR3X.left_motor.run_time(
+        speed=-400,
+        time=5000,
+        then=Stop.BRAKE,
+        wait=False)
+    DINOR3X.right_motor.run_time(
+        speed=-400,
+        time=5000,
+        then=Stop.BRAKE,
+        wait=True)
