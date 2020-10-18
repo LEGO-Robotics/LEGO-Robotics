989a355732ea4b55154ad7182b69492da353402b
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-17 18:33:35 -0700

    add Dinor3x-Super.EV3Dev2.Fast.MicroPython programs

--- EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-Super.EV3Dev2.Fast.MicroPython.Threading.FIXME.py
+++ EV3/Home-Edition/Fan-Robots/Dinor3x/Dinor3x-Super.EV3Dev2.Fast.MicroPython.Threading.FIXME.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env micropython
 
 
+from dinor3x_ev3dev2 import Dinor3x
+
+from threading import Thread
+
+
+DINOR3X = Dinor3x(fast=True)
+
+# FIXME: AttributeError: 'module' object has no attribute 'lseek'
+
+Thread(target=DINOR3X.keep_changing_speed_by_color).start()
+
+Thread(target=DINOR3X.keep_roaring_by_ir_beacon).start()
+
+DINOR3X.keep_walking_by_ir_beacon()
