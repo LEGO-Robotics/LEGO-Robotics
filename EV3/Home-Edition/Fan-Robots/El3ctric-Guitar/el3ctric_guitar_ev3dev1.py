deda9085305ae439b0890b291775b06d8647ca27
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-10-20 19:56:40 -0700

    add programs

--- EV3/Home-Edition/Fan-Robots/El3ctric-Guitar/el3ctric_guitar_ev3dev1.py
+++ EV3/Home-Edition/Fan-Robots/El3ctric-Guitar/el3ctric_guitar_ev3dev1.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, MediumMotor, OUTPUT_A,
+    TouchSensor, InfraredSensor, INPUT_1, INPUT_4,
+    Sound
+)
+
+from time import sleep
+
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))
+from util.ev3dev_fast.ev3fast import (
+    MediumMotor as FastMediumMotor,
+    TouchSensor as FastTouchSensor
+)
+
+
+class El3ctricGuitar:
+    def __init__(
+            self,
+            lever_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1,
+            ir_sensor_port: str = INPUT_4,
+            fast=False):
+        if fast:
+            self.lever_motor = FastMediumMotor(address=lever_motor_port)
+
+            self.touch_sensor = FastTouchSensor(address=touch_sensor_port)
+
+        else:
+            self.lever_motor = MediumMotor(address=lever_motor_port)
+
+            self.touch_sensor = TouchSensor(address=touch_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+
+        self.speaker = Sound()
--- EV3/Home-Edition/Fan-Robots/El3ctric-Guitar/el3ctric_guitar_pybricks.py
+++ EV3/Home-Edition/Fan-Robots/El3ctric-Guitar/el3ctric_guitar_pybricks.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, InfraredSensor
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Direction, Port, Stop
+
+from time import sleep
+
+
+class El3ctricGuitar(EV3Brick):
+    def __init__(
+            self,
+            lever_motor_port: Port = Port.A,
+            touch_sensor_port: Port = Port.S1,
+            ir_sensor_port: Port = Port.S4):
+        self.lever_motor = Motor(port=lever_motor_port,
+                                 positive_direction=Direction.CLOCKWISE)
+
+        self.touch_sensor = TouchSensor(port=touch_sensor_port)
+
+        self.ir_sensor = InfraredSensor(port=ir_sensor_port)
