e257bb8d9b5c643ed10f4b3d8b5526a75e16fd36
Antoni Lương-Phạm <Antoni.LuongPham@GMail.com>
2020-10-28 21:14:48 -0700

    update mr_b3am_pybricks docstrings

--- EV3/Home-Edition/Fan-Robots/Mr-B3am/mr_b3am_pybricks.py
+++ EV3/Home-Edition/Fan-Robots/Mr-B3am/mr_b3am_pybricks.py
@@ -27,6 +27,12 @@ class MrB3am(EV3Brick):
             background_color=None)
 
     def insert_b3am(self):
+        """
+        This first sequence of blocks waits for a B3am to be inserted. 
+        A B3am is detected when the ambient light level is
+        above or equal to the value 3.
+        When a B3am is inserted the motor stops and the EV3 says "Thank you".
+        """
         self.hello_text()
 
         self.screen.draw_text(
@@ -36,6 +42,15 @@ class MrB3am(EV3Brick):
             background_color=None)
 
     def measure_b3am(self):
+        """
+        The next sequence of blocks measures the length of the B3am,
+        by first reseting the motor counter and then moves the B3am
+        until the other end is found. 
+        This is detected when the ambient light level is below the value 1.
+        Note that the length measured is the number of degrees
+        that the wheels has turned.
+        This value will later be converted to the actual B3am length.
+        """
         self.hello_text()
 
         self.screen.draw_text(
@@ -45,6 +60,16 @@ class MrB3am(EV3Brick):
             background_color=None)
 
     def detect_color(self):
+        """
+        After having found the length of the B3am, the length is saved
+        in a variable, named "Length".
+        Afterwards the B3am is moved half way thorugh the machine
+        so its color can be measured.
+        When the color is found it is saved in a variable,
+        named "Color" and the EV3 says "Detected".
+        Note the saved value is the color ID and this will later be converted
+        to the actual color name.
+        """
         self.hello_text()
 
         self.screen.draw_text(
@@ -54,6 +79,11 @@ class MrB3am(EV3Brick):
             background_color=None)
 
     def eject_b3am(self):
+        """
+        After the color is found, the EV3 calculates the number of degrees
+        required to move the wheels,
+        such that the B3am is ejected from the machine.
+        """
         self.hello_text()
 
         self.screen.draw_text(
