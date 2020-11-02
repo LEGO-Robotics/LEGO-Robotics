20d173a3863d2d618655a75ee023ad06cf9aa7f3
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-01 19:41:52 -0800

    add Mr-B3am.EV3Dev1 programs

--- EV3/Home-Edition/Fan-Robots/Mr-B3am/mr_b3am_ev3dev1.py
+++ EV3/Home-Edition/Fan-Robots/Mr-B3am/mr_b3am_ev3dev1.py
@@ -7,8 +7,6 @@ from ev3dev.ev3 import (
     Button, Screen, Sound
 )
 
-from time import sleep
-
 # import os
 # import sys
 # sys.path.append(os.path.expanduser('~'))
@@ -39,7 +37,7 @@ class MrB3am:
 
         self.screen.draw.text(
             xy=(2, 0),
-            text='Mr. B3am!',
+            text='MR. B3AM',
             fill=None,
             font=None,
             anchor=None,
@@ -53,7 +51,7 @@ class MrB3am:
 
     def insert_b3am(self):
         """
-        This first sequence of blocks waits for a B3am to be inserted.
+        This waits for a B3am to be inserted.
         A B3am is detected when the ambient light level is
         above or equal to the value 3.
         When a B3am is inserted the motor stops and the EV3 says "Thank you".
@@ -76,8 +74,6 @@ class MrB3am:
 
         self.screen.update()
 
-        # self.color_sensor.calibrate_white()
-
         self.gear_motor.run_forever(speed_sp=-150)
 
         while self.color_sensor.reflected_light_intensity < 3:
@@ -89,19 +85,21 @@ class MrB3am:
 
     def measure_b3am(self):
         """
-        The next sequence of blocks measures the length of the B3am,
-        by first reseting the motor counter and then moves the B3am
+        This measures the length of the B3am,
+        by first resetting the motor counter and then moves the B3am
         until the other end is found.
         This is detected when the ambient light level is below the value 1.
         Note that the length measured is the number of degrees
         that the wheels has turned.
         This value will later be converted to the actual B3am length.
+        After having found the length of the B3am, the length is saved
+        in a variable, named "current_b3am_length_in_degrees".
         """
         self.header_text()
 
         self.screen.draw.text(
             xy=(0, 6),
-            text='Measuring B3am!',
+            text='Measuring B3am...',
             fill=None,
             font=None,
             anchor=None,
@@ -128,12 +126,10 @@ class MrB3am:
 
     def detect_color(self):
         """
-        After having found the length of the B3am, the length is saved
-        in a variable, named "Length".
         Afterwards the B3am is moved half way thorugh the machine
         so its color can be measured.
         When the color is found it is saved in a variable,
-        named "Color" and the EV3 says "Detected".
+        named "current_b3am_color_code" and the EV3 says "Detected".
         Note the saved value is the color ID and this will later be converted
         to the actual color name.
         """
@@ -141,7 +137,7 @@ class MrB3am:
 
         self.screen.draw.text(
             xy=(0, 6),
-            text='Detecting Color!',
+            text='Detecting Color...',
             fill=None,
             font=None,
             anchor=None,
@@ -175,7 +171,7 @@ class MrB3am:
 
         self.screen.draw.text(
             xy=(0, 6),
-            text='Ejecting B3am!',
+            text='Ejecting B3am...',
             fill=None,
             font=None,
             anchor=None,
@@ -204,11 +200,11 @@ class MrB3am:
 
         self.eject_b3am()
 
-    def report_result(self):
+    def report_result(self, debug=False):
         self.header_text()
 
         if self.current_b3am_color_code == ColorSensor.COLOR_BLACK:
-            self.current_b3am_color = 'BLACK'
+            self.current_b3am_color = 'black'
 
             if 400 <= self.current_b3am_length_in_degrees <= 600:
                 self.current_b3am_length = 5
@@ -228,8 +224,11 @@ class MrB3am:
             elif 1501 <= self.current_b3am_length_in_degrees <= 1700:
                 self.current_b3am_length = 15
 
+            else:
+                self.current_b3am_length = 'UNKNOWN'
+
         elif self.current_b3am_color_code == ColorSensor.COLOR_RED:
-            self.current_b3am_color = 'RED'
+            self.current_b3am_color = 'red'
 
             if 400 <= self.current_b3am_length_in_degrees <= 800:
                 self.current_b3am_length = 5
@@ -249,13 +248,16 @@ class MrB3am:
             elif 1701 <= self.current_b3am_length_in_degrees <= 1900:
                 self.current_b3am_length = 15
 
+            else:
+                self.current_b3am_length = 'UNKNOWN'
+
         else:
             self.current_b3am_color = 'UNKNOWN'
             self.current_b3am_length = 'UNKNOWN'
 
         self.screen.draw.text(
             xy=(0, 18),
-            text='Color: {}'.format(self.current_b3am_color),
+            text='Color: {}'.format(self.current_b3am_color.upper()),
             fill=None,
             font=None,
             anchor=None,
@@ -281,22 +283,49 @@ class MrB3am:
             stroke_width=0,
             stroke_fill=None)
 
+        if debug:
+            self.screen.draw.text(
+                xy=(0, 30),
+                text='Color Code: {}'.format(self.current_b3am_color_code),
+                fill=None,
+                font=None,
+                anchor=None,
+                spacing=4,
+                align='left',
+                direction=None,
+                features=None,
+                language=None,
+                stroke_width=0,
+                stroke_fill=None)
+
+            self.screen.draw.text(
+                xy=(0, 36),
+                text='Degrees: {:,}'.format(
+                        self.current_b3am_length_in_degrees),
+                fill=None,
+                font=None,
+                anchor=None,
+                spacing=4,
+                align='left',
+                direction=None,
+                features=None,
+                language=None,
+                stroke_width=0,
+                stroke_fill=None)
+
         self.speaker.speak(
             text='{} {}'.format(
                 self.current_b3am_color,
                 self.current_b3am_length)).wait()
 
-    def debug(self):
-        ...
-
-    def main(self):
+    def main(self, debug=False):
         while True:
             self.process_b3am()
 
-            self.report_result()
+            self.report_result(debug=debug)
 
             self.screen.draw.text(
-                xy=(0, 11),
+                xy=(0, 42),
                 text='Press Enter...',
                 fill=None,
                 font=None,
@@ -318,4 +347,4 @@ class MrB3am:
 if __name__ == '__main__':
     MR_B3AM = MrB3am()
 
-    MR_B3AM.main()
+    MR_B3AM.main(debug=True)
