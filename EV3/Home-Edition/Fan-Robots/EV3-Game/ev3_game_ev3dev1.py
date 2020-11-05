5edcdac90e6835efa6c3a103b607b3b9acaf053a
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-04 21:12:19 -0800

    update program

--- EV3/Home-Edition/Fan-Robots/EV3-Game/ev3_game_ev3dev1.py
+++ EV3/Home-Edition/Fan-Robots/EV3-Game/ev3_game_ev3dev1.py
@@ -11,12 +11,12 @@ from ev3dev.ev3 import (
 class EV3Game:
     def __init__(
             self,
-            left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
+            b_motor_port: str = OUTPUT_B, c_motor_port: str = OUTPUT_C,
             grip_motor_port: str = OUTPUT_A,
             touch_sensor_port: str = INPUT_1,
             ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
-        self.left_motor = LargeMotor(address=left_motor_port)
-        self.right_motor = LargeMotor(address=right_motor_port)
+        self.b_motor = LargeMotor(address=b_motor_port)
+        self.c_motor = LargeMotor(address=c_motor_port)
 
         self.grip_motor = MediumMotor(address=grip_motor_port)
 
@@ -27,5 +27,88 @@ class EV3Game:
         self.beacon = RemoteControl(sensor=self.ir_sensor,
                                     channel=ir_beacon_channel)
 
-        self.dis = Screen()
+        self.screen = Screen()
         self.speaker = Sound()
+
+        self.offset_holdcup = 60
+        self.current_b = self.current_c = 1
+
+    def move_1_rotate_b(self):
+        if self.current_b == 1:
+            self.rotate_b = self.offset_holdcup + 180
+
+        elif self.current_b == 2:
+            self.rotate_b = 2 * self.offset_holdcup + 180
+
+        elif self.current_b == 3:
+            self.rotate_b = 180
+
+    def move_1_rotate_c(self):
+        if self.current_c == 1:
+            self.rotate_c = 0
+
+        elif self.current_c == 2:
+            self.rotate_c = -self.offset_holdcup
+
+        elif self.current_c == 3:
+            self.rotate_c = self.offset_holdcup
+
+    def move_2_rotate_b(self):
+        if self.current_b == 1:
+            self.rotate_b = self.offset_holdcup + 180
+
+        elif self.current_b == 2:
+            self.rotate_b = -180
+
+        elif self.current_b == 3:
+            self.rotate_b = 2 * self.offset_holdcup + 180
+
+    def move_2_rotate_c(self):
+        if self.current_c == 1:
+            self.rotate_c = 0
+
+        elif self.current_c == 2:
+            self.rotate_c = -self.offset_holdcup
+
+        elif self.current_c == 3:
+            self.rotate_c = self.offset_holdcup
+
+    def move_3_rotate_b(self):
+        if self.current_b == 1:
+            self.rotate_b = 0
+
+        elif self.current_b == 2:
+            self.rotate_b = self.offset_holdcup
+
+        elif self.current_b == 3:
+            self.rotate_b = -self.offset_holdcup
+
+    def move_3_rotate_c(self):
+        if self.current_c == 1:
+            self.rotate_c = self.offset_holdcup + 180
+
+        elif self.current_c == 2:
+            self.rotate_c = 180
+
+        elif self.current_c == 3:
+            self.rotate_c = 2 * self.offset_holdcup + 180
+
+    def move_4_rotate_b(self):
+        if self.current_b == 1:
+            self.rotate_b = 0
+
+        elif self.current_b == 2:
+            self.rotate_b = self.offset_holdcup
+
+        elif self.current_b == 3:
+            self.rotate_b = -self.offset_holdcup
+
+    def move_4_rotate_c(self):
+        if self.current_c == 1:
+            self.rotate_c = self.offset_holdcup + 180
+
+        elif self.current_c == 2:
+            self.rotate_c = 2 * self.offset_holdcup + 180
+
+        elif self.current_c == 3:
+            self.rotate_c = -180
