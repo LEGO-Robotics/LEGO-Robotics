aa952ce11700c96528bf495f16fc9105453cd973
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-07 21:33:27 -0800

    add main program

--- EV3/Home-Edition/Fan-Robots/EV3-Game/ev3_game_pybricks.py
+++ EV3/Home-Edition/Fan-Robots/EV3-Game/ev3_game_pybricks.py
@@ -4,8 +4,9 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, InfraredSensor
 from pybricks.media.ev3dev import SoundFile
-from pybricks.parameters import Button, Direction, Port, Stop
+from pybricks.parameters import Button, Color, Direction, Port, Stop
 
+from random import randint
 from time import sleep
 
 
@@ -29,6 +30,21 @@ class EV3Game(EV3Brick):
         self.ir_sensor = InfraredSensor(port=ir_sensor_port)
         self.ir_beacon_channel = ir_beacon_channel
 
+    def start_up(self):
+        self.light.on(color=Color.RED)
+
+        self.calibrate_grip()
+
+        self.screen.clear()
+
+        self.level = 1
+
+        self.display_level()
+
+        self.choice = 2
+
+        self.display_cup_number()
+
         self.offset_holdcup = 60
         self.current_b = self.current_c = 1
 
@@ -47,6 +63,15 @@ class EV3Game(EV3Brick):
             rotation_angle=30,
             then=Stop.HOLD,
             wait=True)
+
+    def display_level(self):
+        ...
+
+    def display_cup_number(self):
+        ...
+
+    def select_level(self):
+        ...
 
     def move_1_rotate_b(self):
         if self.current_b == 1:
@@ -127,3 +152,105 @@ class EV3Game(EV3Brick):
 
         elif self.current_c == 3:
             self.rotate_c = -180
+
+    def execute_move(self):
+        ...
+
+    def update_ballcup(self):
+        ...
+
+    def select_choice(self):
+        ...
+
+    def cup_to_center(self):
+        ...
+
+    def main(self):
+        self.start_up()
+
+        while True:
+            self.cup_with_ball = 2
+
+            self.select_level()
+
+            self.light.on(color=Color.GREEN)
+
+            for i in range(15):
+                move = randint(1, 4)
+
+                if move == 1:
+                    self.move_1_rotate_b()
+                    self.move_1_rotate_c()
+
+                    self.current_b = 3
+                    self.current_c = 1
+
+                elif move == 2:
+                    self.move_2_rotate_b()
+                    self.move_2_rotate_c()
+
+                    self.current_b = 2
+                    self.current_c = 1
+
+                elif move == 3:
+                    self.move_3_rotate_b()
+                    self.move_3_rotate_c()
+
+                    self.current_b = 1
+                    self.current_c = 2
+
+                elif move == 4:
+                    self.move_4_rotate_b()
+                    self.move_4_rotate_c()
+
+                    self.current_b = 1
+                    self.current_c = 3
+
+                self.execute_move()
+
+                self.update_ballcup()
+
+            self.move_3_rotate_b()
+
+            self.move_1_rotate_c()
+
+            self.current_b = self.current_c = 1
+
+            self.execute_move()
+
+            self.light.off()
+
+            correct_choice = False
+
+            while not correct_choice:
+                self.select_choice()
+
+                self.cup_to_center()
+
+                self.grip_motor.run_angle(
+                    speed=100,
+                    rotation_angle=220,
+                    then=Stop.HOLD,
+                    wait=True)
+
+                correct_choice = (self.cup_with_ball == 2)
+
+                if correct_choice:
+                    self.light.on(color=Color.GREEN)
+
+                    self.speaker.play_file(file=SoundFile.CHEERING)
+
+                else:
+                    self.light.on(color=Color.RED)
+
+                    self.speaker.play_file(file=SoundFile.BOO)
+
+            sleep(2)
+
+            self.calibrate_grip()
+
+
+if __name__ == '__main__':
+    EV3_GAME = EV3Game()
+
+    EV3_GAME.main()
