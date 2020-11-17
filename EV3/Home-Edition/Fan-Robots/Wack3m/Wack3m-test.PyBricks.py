ea500b0fb8b76bd88653ee434fe0f56d739f6b3a
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-16 18:34:57 -0800

    add program

--- EV3/Home-Edition/Fan-Robots/Wack3m/Wack3m-test.PyBricks.py
+++ EV3/Home-Edition/Fan-Robots/Wack3m/Wack3m-test.PyBricks.py
@@ -1,3 +1,40 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from wack3m_pybricks import Wack3m
+
+from pybricks.media.ev3dev import ImageFile
+from pybricks.parameters import Stop
+
+from random import uniform
+from time import sleep
+
+
+WACK3M = Wack3m()
+
+WACK3M.screen.load_image(ImageFile.EV3_ICON)
+
+WACK3M.left_motor.run_time(
+    speed=-300,
+    time=1000,
+    then=Stop.HOLD,
+    wait=True)
+
+WACK3M.left_motor.reset_angle(angle=0)
+
+while True:
+    sleep(uniform(0.1, 3))
+
+    WACK3M.left_motor.run_angle(
+        speed=1000,
+        rotation_angle=60,
+        then=Stop.COAST,
+        wait=True)
+
+    WACK3M.left_motor.run_time(
+        speed=-400,
+        time=1000 * 0.5,
+        then=Stop.HOLD,
+        wait=True)
+
+    sleep(1)
