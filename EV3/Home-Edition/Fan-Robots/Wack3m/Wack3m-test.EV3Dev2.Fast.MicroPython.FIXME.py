7a2be9380f0936c25c463c9ef040abd8f50629b1
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-17 16:28:51 -0800

    add Wack3m programs

--- EV3/Home-Edition/Fan-Robots/Wack3m/Wack3m-test.EV3Dev2.Fast.MicroPython.FIXME.py
+++ EV3/Home-Edition/Fan-Robots/Wack3m/Wack3m-test.EV3Dev2.Fast.MicroPython.FIXME.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env micropython
 
 
+from wack3m_ev3dev2 import Wack3m
+
+from random import randint
+from time import sleep
+
+
+WACK3M = Wack3m(fast=True)
+
+WACK3M.left_motor.on_for_seconds(
+    speed=-30,
+    seconds=1,
+    brake=True,
+    block=True)
+
+WACK3M.left_motor.reset()
+
+while True:
+    sleep(0.1 + (3 - 0.1) * randint(1, 10) / 10)
+
+    WACK3M.left_motor.on_for_degrees(
+        speed=100,
+        degrees=60,
+        brake=False,
+        block=True)
+
+    WACK3M.left_motor.on_for_seconds(
+        speed=-40,
+        seconds=0.5,
+        brake=True,
+        block=True)
+
+    sleep(1)
