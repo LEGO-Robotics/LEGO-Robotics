ffe9dd85bf83093f251c7c0ed66f8e944e816f43
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-17 17:44:05 -0800

    add program

--- EV3/Home-Edition/Fan-Robots/Wack3m/wack3m_ev3dev2.py
+++ EV3/Home-Edition/Fan-Robots/Wack3m/wack3m_ev3dev2.py
@@ -22,6 +22,8 @@ from time import sleep, time
 
 
 class Wack3m:
+    N_WACK_TIMES = 10
+
     def __init__(
             self,
             left_motor_port: str = OUTPUT_B, right_motor_port: str = OUTPUT_C,
@@ -53,7 +55,7 @@ class Wack3m:
             color='RED',
             groups=('LEFT', 'RIGHT'),
             sleeptime=0.5,
-            duration=3,
+            duration=1,
             block=True)
 
         self.console.text_at(
@@ -91,12 +93,154 @@ class Wack3m:
         self.start_up()
 
         while True:
+            self.speaker.play_file(
+                wav_file='/home/robot/sound/Start.wav',
+                volume=100,
+                play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
             self.leds.animate_flash(
                 color='ORANGE',
                 groups=('LEFT', 'RIGHT'),
                 sleeptime=0.5,
-                duration=3,
+                duration=1,
                 block=True)
+
+            self.touch_sensor.wait_for_pressed()
+
+            self.speaker.play_file(
+                wav_file='/home/robot/sound/Go.wav',
+                volume=100,
+                play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+            self.leds.animate_flash(
+                color='GREEN',
+                groups=('LEFT', 'RIGHT'),
+                sleeptime=0.5,
+                duration=1,
+                block=True)
+
+            total_response_time = 0
+
+            sleep(1)
+
+            for _ in range(self.N_WACK_TIMES):
+                self.leds.animate_flash(
+                    color='GREEN',
+                    groups=('LEFT', 'RIGHT'),
+                    sleeptime=0.5,
+                    duration=1,
+                    block=True)
+
+                sleep(0.1 + (3 - 0.1) * randint(1, 10) / 10)
+
+                which_motor = randint(1, 3)
+
+                if which_motor == 1:
+                    self.left_motor.on_for_degrees(
+                        speed=100,
+                        degrees=60,
+                        brake=False,
+                        block=True)
+
+                    self.left_motor.on_for_seconds(
+                        speed=-40,
+                        seconds=0.5,
+                        brake=True,
+                        block=True)
+
+                    proximity = self.ir_sensor.proximity
+                    start_time = time()
+                    while abs(self.ir_sensor.proximity - proximity) <= 2:
+                        pass
+
+                elif which_motor == 2:
+                    self.middle_motor.on_for_degrees(
+                        speed=50,
+                        degrees=170,
+                        brake=False,
+                        block=True)
+
+                    self.middle_motor.on_for_seconds(
+                        speed=-40,
+                        seconds=0.4,
+                        brake=True,
+                        block=True)
+
+                    proximity = self.ir_sensor.proximity
+                    start_time = time()
+                    while abs(self.ir_sensor.proximity - proximity) <= 3:
+                        pass
+
+                else:
+                    self.left_motor.on_for_degrees(
+                        speed=100,
+                        degrees=60,
+                        brake=False,
+                        block=True)
+
+                    self.left_motor.on_for_seconds(
+                        speed=-40,
+                        seconds=0.5,
+                        brake=True,
+                        block=True)
+
+                    proximity = self.ir_sensor.proximity
+                    start_time = time()
+                    while abs(self.ir_sensor.proximity - proximity) <= 3:
+                        pass
+
+                response_time = time() - start_time
+
+                self.console.text_at(
+                    column=1, row=1,
+                    text='Time: {:.1f}s'.format(response_time),
+                    reset_console=True,
+                    inverse=False,
+                    alignment='L')
+
+                self.leds.animate_flash(
+                    color='RED',
+                    groups=('LEFT', 'RIGHT'),
+                    sleeptime=0.5,
+                    duration=1,
+                    block=True)
+
+                self.speaker.play_file(
+                    wav_file='/home/robot/sound/Boing.wav',
+                    volume=100,
+                    play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+                total_response_time += response_time
+
+            average_response_time = total_response_time / self.N_WACK_TIMES
+
+            self.console.text_at(
+                column=1, row=1,
+                text='Avg Time: {:.1f}s'.format(average_response_time),
+                reset_console=True,
+                inverse=False,
+                alignment='L')
+
+            self.speaker.play_file(
+                wav_file='/home/robot/sound/Fantastic.wav'
+                         if average_response_time <= 1
+                         else '/home/robot/sound/Good job.wav',
+                volume=100,
+                play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+            self.speaker.play_file(
+                wav_file='/home/robot/sound/Game over.wav',
+                volume=100,
+                play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+            self.leds.animate_flash(
+                color='RED',
+                groups=('LEFT', 'RIGHT'),
+                sleeptime=0.5,
+                duration=1,
+                block=True)
+
+            sleep(4)
 
 
 if __name__ == '__main__':
