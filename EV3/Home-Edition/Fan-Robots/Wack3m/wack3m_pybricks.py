1617f8db45b84ae103c169e0433aefdbddc2ae61
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-17 16:28:35 -0800

    add program

--- EV3/Home-Edition/Fan-Robots/Wack3m/wack3m_pybricks.py
+++ EV3/Home-Edition/Fan-Robots/Wack3m/wack3m_pybricks.py
@@ -7,10 +7,12 @@ from pybricks.media.ev3dev import ImageFile, SoundFile
 from pybricks.parameters import Direction, Port, Stop, Color
 
 from time import sleep, time
-from random import randint
+from random import randint, uniform
 
 
 class Wack3m(EV3Brick):
+    N_WACK_TIMES = 10
+
     def __init__(
             self,
             left_motor_port: str = Port.B, right_motor_port: str = Port.C,
@@ -65,7 +67,120 @@ class Wack3m(EV3Brick):
         self.start_up()
 
         while True:
+            self.speaker.play_file(file=SoundFile.START)
+
+            self.screen.load_image(ImageFile.TARGET)
+
             self.light.on(color=Color.ORANGE)
+
+            while not self.touch_sensor.pressed():
+                pass
+
+            self.speaker.play_file(file=SoundFile.GO)
+
+            self.light.on(color=Color.GREEN)
+
+            total_response_time = 0
+
+            sleep(1)
+
+            for _ in range(self.N_WACK_TIMES):
+                self.light.on(color=Color.GREEN)
+
+                self.screen.load_image(ImageFile.EV3_ICON)
+
+                sleep(uniform(0.1, 3))
+
+                which_motor = randint(1, 3)
+
+                if which_motor == 1:
+                    self.left_motor.run_angle(
+                        speed=1000,
+                        rotation_angle=60,
+                        then=Stop.COAST,
+                        wait=True)
+
+                    self.screen.load_image(ImageFile.MIDDLE_LEFT)
+
+                    self.left_motor.run_time(
+                        speed=-400,
+                        time=1000 * 0.5,
+                        then=Stop.HOLD,
+                        wait=True)
+
+                    proximity = self.ir_sensor.distance()
+                    start_time = time()
+                    while abs(self.ir_sensor.distance() - proximity) <= 2:
+                        pass
+
+                elif which_motor == 2:
+                    self.middle_motor.run_angle(
+                        speed=500,
+                        rotation_angle=170,
+                        then=Stop.COAST,
+                        wait=True)
+
+                    self.screen.load_image(ImageFile.NEUTRAL)
+
+                    self.middle_motor.run_time(
+                        speed=-400,
+                        time=1000 * 0.4,
+                        then=Stop.HOLD,
+                        wait=True)
+
+                    proximity = self.ir_sensor.distance()
+                    start_time = time()
+                    while abs(self.ir_sensor.distance() - proximity) <= 3:
+                        pass
+
+                else:
+                    self.right_motor.run_angle(
+                        speed=1000,
+                        rotation_angle=60,
+                        then=Stop.COAST,
+                        wait=True)
+
+                    self.screen.load_image(ImageFile.MIDDLE_RIGHT)
+
+                    self.right_motor.run_time(
+                        speed=-400,
+                        time=1000 * 0.5,
+                        then=Stop.HOLD,
+                        wait=True)
+
+                    proximity = self.ir_sensor.distance()
+                    start_time = time()
+                    while abs(self.ir_sensor.distance() - proximity) <= 3:
+                        pass
+
+                response_time = time() - start_time
+
+                self.screen.load_image(ImageFile.DIZZY)
+
+                self.screen.print(response_time)
+
+                self.light.on(color=Color.RED)
+
+                self.speaker.play_file(file=SoundFile.BOING)
+
+                total_response_time += response_time
+
+            average_response_time = total_response_time / self.N_WACK_TIMES
+
+            self.screen.clear()
+            self.screen.print(
+                'Avg. Time: {:.1f}s'.format(average_response_time))
+
+            self.speaker.play_file(
+                file=SoundFile.FANTASTIC
+                     if average_response_time <= 1
+                     else SoundFile.GOOD_JOB)
+
+            self.speaker.play_file(file=SoundFile.GAME_OVER)
+
+            self.light.on(color=Color.RED)
+
+            sleep(4)
 
 
 if __name__ == '__main__':
