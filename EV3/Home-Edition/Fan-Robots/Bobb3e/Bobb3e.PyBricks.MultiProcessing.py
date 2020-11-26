7097a4da695a5e6827d825a6c9e8fc4f14b72fac
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-25 23:55:28 -0800

    add Bobb3e.PyBricks programs

--- EV3/Home-Edition/Fan-Robots/Bobb3e/Bobb3e.PyBricks.MultiProcessing.py
+++ EV3/Home-Edition/Fan-Robots/Bobb3e/Bobb3e.PyBricks.MultiProcessing.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from bobb3e_pybricks import Bobb3e
+
+from multiprocessing import Process
+
+
+BOBB3E = Bobb3e()
+
+BOBB3E.screen.print('BOBB3E')
+
+Process(target=BOBB3E.sound_alarm_whenever_reversing).start()
+
+BOBB3E.keep_driving_or_operating_lift_by_ir_beacon(speed=1000)
