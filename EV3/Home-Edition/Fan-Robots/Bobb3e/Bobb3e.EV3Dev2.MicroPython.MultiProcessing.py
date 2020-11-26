44212f8a67399c42fae40ba4a8f4569c3c64c650
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-25 23:53:55 -0800

    add Bobb3e.EV3Dev2 programs

--- EV3/Home-Edition/Fan-Robots/Bobb3e/Bobb3e.EV3Dev2.MicroPython.MultiProcessing.py
+++ EV3/Home-Edition/Fan-Robots/Bobb3e/Bobb3e.EV3Dev2.MicroPython.MultiProcessing.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env micropython
 
 
+from bobb3e_ev3dev2 import Bobb3e
+
+from multiprocessing import Process
+
+
+BOBB3E = Bobb3e()
+
+BOBB3E.console.text_at(
+    text='BOBB3E',
+    column=3,
+    row=2,
+    reset_console=False,
+    inverse=False,
+    alignment='L')
+
+Process(target=BOBB3E.sound_alarm_whenever_reversing).start()
+
+BOBB3E.keep_driving_or_operating_lift_by_ir_beacon(speed=100)
