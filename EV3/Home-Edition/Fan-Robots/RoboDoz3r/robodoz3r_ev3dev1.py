87f03a8cb42ec34159f3c4f5a55e688088909dcb
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-28 21:02:53 -0800

    add program

--- EV3/Home-Edition/Fan-Robots/RoboDoz3r/robodoz3r_ev3dev1.py
+++ EV3/Home-Edition/Fan-Robots/RoboDoz3r/robodoz3r_ev3dev1.py
@@ -1,3 +1,68 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, MediumMotor, LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, RemoteControl, INPUT_1, INPUT_4,
+    Screen, Sound
+)
+
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))
+from util.drive_util_ev3dev1 import IRBeaconRemoteControlledTank
+from util.ev3dev_fast.ev3fast import (
+    MediumMotor as FastMediumMotor,
+    TouchSensor as FastTouchSensor
+)
+
+from time import sleep
+
+
+class RoboDoz3r(IRBeaconRemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_C, right_motor_port: str = OUTPUT_B,
+            shovel_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1,
+            ir_sensor_port: str = INPUT_4,
+            tank_drive_ir_beacon_channel: int = 1,
+            shovel_control_ir_beacon_channel: int = 4,
+            fast=False):
+        super().__init__(
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            polarity=Motor.POLARITY_INVERSED,
+            ir_sensor_port=ir_sensor_port,
+            ir_beacon_channel=tank_drive_ir_beacon_channel,
+            fast=fast)
+
+        if fast:
+            self.shovel_motor = FastMediumMotor(address=shovel_motor_port)
+
+            self.touch_sensor = FastTouchSensor(address=touch_sensor_port)
+
+        else:
+            self.shovel_motor = MediumMotor(address=shovel_motor_port)
+
+            self.touch_sensor = TouchSensor(address=touch_sensor_port)
+
+        self.shovel_control_remote_control = \
+            RemoteControl(
+                sensor=self.ir_sensor,
+                channel=shovel_control_ir_beacon_channel)
+
+        self.screen = Screen()
+        self.speaker = Sound()
+
+    def main(self,
+             driving_speed: float = 1000   # deg/s
+             ):
+
+        while True:
+            self.drive_once_by_ir_beacon(speed=driving_speed)
+
+
+if __name__ == '__main__':
+    ROBODOZ3R = RoboDoz3r()
+
+    ROBODOZ3R.main()
--- EV3/Home-Edition/Fan-Robots/RoboDoz3r/robodoz3r_pybricks.py
+++ EV3/Home-Edition/Fan-Robots/RoboDoz3r/robodoz3r_pybricks.py
@@ -1,3 +1,55 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Button, Direction, Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))
+from util.drive_util_pybricks import IRBeaconRemoteControlledTank
+
+
+class RoboDoz3r(IRBeaconRemoteControlledTank, EV3Brick):
+    WHEEL_DIAMETER = 24   # milimeters
+    AXLE_TRACK = 100      # milimeters
+
+    def __init__(
+            self,
+            left_motor_port: Port = Port.C, right_motor_port: Port = Port.B,
+            shovel_motor_port: Port = Port.A,
+            touch_sensor_port: Port = Port.S1,
+            ir_sensor_port: Port = Port.S4,
+            tank_drive_ir_beacon_channel: int = 1,
+            shovel_control_ir_beacon_channel: int = 4):
+        super().__init__(
+            wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            polarity='inversed',
+            ir_sensor_port=ir_sensor_port,
+            ir_beacon_channel=tank_drive_ir_beacon_channel)
+
+        self.shovel_motor = Motor(port=shovel_motor_port,
+                                  positive_direction=Direction.CLOCKWISE)
+
+        self.touch_sensor = TouchSensor(port=touch_sensor_port)
+
+        self.shovel_control_ir_beacon_channel = \
+            shovel_control_ir_beacon_channel
+
+    def main(self,
+             driving_speed: float = 1000   # mm/s
+             ):
+
+        while True:
+            self.drive_once_by_ir_beacon(speed=driving_speed)
+
+
+if __name__ == '__main__':
+    ROBODOZ3R = RoboDoz3r()
+
+    ROBODOZ3R.main()
