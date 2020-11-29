27cb8e9770f71bd5e6f8eda58d4a0ce4b82f8478
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-11-28 22:14:05 -0800

    add RoboDoz3r.EV3Dev.Fast programs

--- EV3/Home-Edition/Fan-Robots/RoboDoz3r/Robodoz3r.EV3Dev2.Fast.MicroPython.FIXME.py
+++ EV3/Home-Edition/Fan-Robots/RoboDoz3r/Robodoz3r.EV3Dev2.Fast.MicroPython.FIXME.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env micropython
 
 
+from robodoz3r_ev3dev2 import RoboDoz3r
+
+
+ROBODOZ3R = RoboDoz3r(fast=True)
+
+ROBODOZ3R.main()
--- EV3/Home-Edition/Fan-Robots/RoboDoz3r/Robodoz3r.EV3Dev2.Fast.Python3.py
+++ EV3/Home-Edition/Fan-Robots/RoboDoz3r/Robodoz3r.EV3Dev2.Fast.Python3.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
 
+from robodoz3r_ev3dev2 import RoboDoz3r
+
+
+ROBODOZ3R = RoboDoz3r(fast=True)
+
+ROBODOZ3R.main()
