0dbb91de5932f1de7548795db546900a80ee644b
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-07 20:18:44 -0800

    update program

--- EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Autonomous.EV3Dev1.py
+++ EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Autonomous.EV3Dev1.py
@@ -23,37 +23,36 @@ while True:
     while RAC3_TRUCK.ir_sensor.proximity >= 30:
         pass
 
-    RAC3_TRUCK.right_motor.stop(stop_action=Motor.STOP_ACTION_HOLD)
-    RAC3_TRUCK.left_motor.stop(stop_action=Motor.STOP_ACTION_HOLD)
+    RAC3_TRUCK.left_motor.stop(stop_action=Motor.STOP_ACTION_COAST)
+    RAC3_TRUCK.right_motor.stop(stop_action=Motor.STOP_ACTION_COAST)
 
     RAC3_TRUCK.steer_left()
 
     RAC3_TRUCK.speaker.play(wav_file='/home/robot/sound/Object.wav').wait()
-
     RAC3_TRUCK.speaker.play(wav_file='/home/robot/sound/Detected.wav').wait()
 
     RAC3_TRUCK.left_motor.run_timed(
         speed_sp=-600,
         time_sp=2000,
-        stop_action=Motor.STOP_ACTION_HOLD)
+        stop_action=Motor.STOP_ACTION_COAST)
     RAC3_TRUCK.right_motor.run_timed(
         speed_sp=-1000,
         time_sp=2000,
-        stop_action=Motor.STOP_ACTION_HOLD)
-    RAC3_TRUCK.right_motor.wait_while(Motor.STATE_RUNNING)
+        stop_action=Motor.STOP_ACTION_COAST)
     RAC3_TRUCK.left_motor.wait_while(Motor.STATE_RUNNING)
+    RAC3_TRUCK.right_motor.wait_while(Motor.STATE_RUNNING)
 
     RAC3_TRUCK.steer_right()
 
     RAC3_TRUCK.left_motor.run_timed(
         speed_sp=1000,
         time_sp=2000,
-        stop_action=Motor.STOP_ACTION_HOLD)
+        stop_action=Motor.STOP_ACTION_COAST)
     RAC3_TRUCK.right_motor.run_timed(
         speed_sp=600,
         time_sp=2000,
-        stop_action=Motor.STOP_ACTION_HOLD)
-    RAC3_TRUCK.right_motor.wait_while(Motor.STATE_RUNNING)
+        stop_action=Motor.STOP_ACTION_COAST)
     RAC3_TRUCK.left_motor.wait_while(Motor.STATE_RUNNING)
+    RAC3_TRUCK.right_motor.wait_while(Motor.STATE_RUNNING)
 
     RAC3_TRUCK.steer_center()
