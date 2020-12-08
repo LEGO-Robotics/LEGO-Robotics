2d1507a53ab392a9763176d315aa633a43bd4c86
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-07 17:37:53 -0800

    add program

--- EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-5-Advanced.EV3Dev2.Python3.py
+++ EV3/Home-Edition/Core-Robots/Ev3rstorm/Ev3rstorm-5-Advanced.EV3Dev2.Python3.py
@@ -2,9 +2,11 @@
 # (Display not yet working in MicroPython as of 2020)
 
 
-from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.motor import (
+    LargeMotor, MediumMotor, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C
+)
 from ev3dev2.sensor import INPUT_1, INPUT_3, INPUT_4
-from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor
 from ev3dev2.display import Display
 from ev3dev2.sound import Sound
 
@@ -17,15 +19,18 @@ from util.drive_util_ev3dev2 import IRBeaconRemoteControlledTank
 class Ev3rstorm(IRBeaconRemoteControlledTank):
     def __init__(
             self,
-            left_foot_motor_port: str = OUTPUT_B, right_foot_motor_port: str = OUTPUT_C,
+            left_foot_motor_port: str = OUTPUT_B,
+            right_foot_motor_port: str = OUTPUT_C,
             bazooka_blast_motor_port: str = OUTPUT_A,
             touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
             ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1):
         super().__init__(
-            left_motor_port=left_foot_motor_port, right_motor_port=right_foot_motor_port, motor_class=LargeMotor,
+            left_motor_port=left_foot_motor_port,
+            right_motor_port=right_foot_motor_port, motor_class=LargeMotor,
             ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
 
-        self.bazooka_blast_motor = MediumMotor(address=bazooka_blast_motor_port)
+        self.bazooka_blast_motor = \
+            MediumMotor(address=bazooka_blast_motor_port)
 
         self.touch_sensor = TouchSensor(address=touch_sensor_port)
         self.color_sensor = ColorSensor(address=color_sensor_port)
@@ -33,10 +38,10 @@ class Ev3rstorm(IRBeaconRemoteControlledTank):
         self.screen = Display()
         self.speaker = Sound()
 
-
     def blast_bazooka_if_touched(self):
         if self.touch_sensor.is_pressed:
-            if self.color_sensor.ambient_light_intensity < 5:   # 15 not dark enough
+            # 15 not dark enough
+            if self.color_sensor.ambient_light_intensity < 5:
                 self.speaker.play_file(
                     wav_file='/home/robot/sound/Up.wav',
                     volume=100,
@@ -61,17 +66,16 @@ class Ev3rstorm(IRBeaconRemoteControlledTank):
                     block=True)
 
             self.touch_sensor.wait_for_released()
- 
-    
+
     def main(self, driving_speed: float = 100):
         self.screen.image_filename(
             filename='/home/robot/image/Target.bmp',
             clear_screen=True)
         self.screen.update()
-    
+
         while True:
             self.drive_once_by_ir_beacon(speed=driving_speed)
-            
+
             self.blast_bazooka_if_touched()
 
 
