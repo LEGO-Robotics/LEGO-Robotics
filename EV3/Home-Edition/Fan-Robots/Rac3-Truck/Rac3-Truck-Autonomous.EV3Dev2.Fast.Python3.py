6294fdde7fad39c7c843e220e931d8ab80ac7cfb
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-07 20:27:22 -0800

    add Rac3-Truck-Autonomous.EV3Dev.Fast programs

--- EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Autonomous.EV3Dev2.Fast.Python3.py
+++ EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Autonomous.EV3Dev2.Fast.Python3.py
@@ -7,3 +7,51 @@ Run this program if you want the Rac3 Truck to drive around autonomously
 (by itself)
 """
 
+from ev3dev2.sound import Sound
+
+from rac3_truck_ev3dev2 import Rac3Truck
+
+
+RAC3_TRUCK = Rac3Truck(fast=True)
+
+
+RAC3_TRUCK.reset()
+
+while True:
+    RAC3_TRUCK.tank_driver.on(
+        left_speed=80,
+        right_speed=80)
+
+    while RAC3_TRUCK.ir_sensor.proximity >= 30:
+        pass
+
+    RAC3_TRUCK.tank_driver.off(brake=False)
+
+    RAC3_TRUCK.steer_left()
+
+    RAC3_TRUCK.speaker.play_file(
+        wav_file='/home/robot/sound/Object.wav',
+        volume=100,
+        play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+    RAC3_TRUCK.speaker.play_file(
+        wav_file='/home/robot/sound/Detected.wav',
+        volume=100,
+        play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+
+    RAC3_TRUCK.tank_driver.on_for_seconds(
+        left_speed=-60,
+        right_speed=-100,
+        seconds=2,
+        brake=False,
+        block=True)
+
+    RAC3_TRUCK.steer_right()
+
+    RAC3_TRUCK.tank_driver.on_for_seconds(
+        left_speed=100,
+        right_speed=60,
+        seconds=2,
+        brake=False,
+        block=True)
+
+    RAC3_TRUCK.steer_center()
