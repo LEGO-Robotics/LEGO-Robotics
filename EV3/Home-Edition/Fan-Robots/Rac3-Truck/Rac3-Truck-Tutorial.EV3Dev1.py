acc8aa2d0f6db33cb6eacd9e248f3858ef1063ed
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-07 21:26:06 -0800

    add programs

--- EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Tutorial.EV3Dev1.py
+++ EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Tutorial.EV3Dev1.py
@@ -1,3 +1,54 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import Motor
+
+from time import sleep
+
+from rac3_truck_ev3dev1 import Rac3Truck
+
+
+RAC3_TRUCK = Rac3Truck()
+
+RAC3_TRUCK.reset()
+
+RAC3_TRUCK.left_motor.run_to_rel_pos(
+    speed_sp=800,
+    position_sp=5 * 360,
+    stop_action=Motor.STOP_ACTION_HOLD)
+RAC3_TRUCK.right_motor.run_to_rel_pos(
+    speed_sp=800,
+    position_sp=5 * 360,
+    stop_action=Motor.STOP_ACTION_HOLD)
+RAC3_TRUCK.left_motor.wait_while(Motor.STATE_RUNNING)
+RAC3_TRUCK.right_motor.wait_while(Motor.STATE_RUNNING)
+
+sleep(1)
+
+RAC3_TRUCK.steer_right()
+
+RAC3_TRUCK.left_motor.run_timed(
+    speed_sp=1000,
+    time_sp=1000,
+    stop_action=Motor.STOP_ACTION_HOLD)
+RAC3_TRUCK.right_motor.run_timed(
+    speed_sp=600,
+    time_sp=1000,
+    stop_action=Motor.STOP_ACTION_HOLD)
+RAC3_TRUCK.left_motor.wait_while(Motor.STATE_RUNNING)
+RAC3_TRUCK.right_motor.wait_while(Motor.STATE_RUNNING)
+
+sleep(1)
+
+RAC3_TRUCK.steer_center()
+
+RAC3_TRUCK.left_motor.run_to_rel_pos(
+    speed_sp=800,
+    position_sp=5 * 360,
+    stop_action=Motor.STOP_ACTION_HOLD)
+RAC3_TRUCK.right_motor.run_to_rel_pos(
+    speed_sp=800,
+    position_sp=5 * 360,
+    stop_action=Motor.STOP_ACTION_HOLD)
+RAC3_TRUCK.left_motor.wait_while(Motor.STATE_RUNNING)
+RAC3_TRUCK.right_motor.wait_while(Motor.STATE_RUNNING)
--- EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Tutorial.EV3Dev2.py
+++ EV3/Home-Edition/Fan-Robots/Rac3-Truck/Rac3-Truck-Tutorial.EV3Dev2.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env micropython
+
+
+from time import sleep
+
+from rac3_truck_ev3dev2 import Rac3Truck
+
+
+RAC3_TRUCK = Rac3Truck()
+
+RAC3_TRUCK.reset()
+
+RAC3_TRUCK.steer_driver.on_for_rotations(
+    steering=0,
+    speed=80,
+    rotations=5,
+    brake=True,
+    block=True)
+
+sleep(1)
+
+RAC3_TRUCK.steer_right()
+
+RAC3_TRUCK.tank_driver.on_for_seconds(
+    left_speed=100,
+    right_speed=60,
+    seconds=1,
+    brake=True,
+    block=True)
+
+sleep(1)
+
+RAC3_TRUCK.steer_center()
+
+RAC3_TRUCK.steer_driver.on_for_rotations(
+    steering=0,
+    speed=80,
+    rotations=5,
+    brake=True,
+    block=True)
