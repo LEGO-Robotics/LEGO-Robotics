4da9ad057ef34b0c6b04f3875193d12ecb05c943
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-10 21:19:07 -0800

    add program

--- EV3/Home-Edition/Fan-Robots/Kraz3/kraz3_ev3dev2.py
+++ EV3/Home-Edition/Fan-Robots/Kraz3/kraz3_ev3dev2.py
@@ -1,3 +1,66 @@
 #!/usr/bin/env micropython
 
 
+from ev3dev2.motor import MediumMotor, Motor, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor import INPUT_1, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, InfraredSensor
+from ev3dev2.button import Button
+from ev3dev2.console import Console
+from ev3dev2.sound import Sound
+
+from time import sleep
+
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))
+from util.ev3dev_fast.ev3fast import (
+    MediumMotor as FastMediumMotor,
+    TouchSensor as FastTouchSensor,
+    ColorSensor as FastColorSensor
+)
+from util.drive_util_ev3dev2 import IRBeaconRemoteControlledTank
+
+
+class Kraz3(IRBeaconRemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_C, right_motor_port: str = OUTPUT_B,
+            wiggle_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1,
+            fast=False):
+        super().__init__(
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel,
+            polarity=Motor.POLARITY_INVERSED,
+            fast=fast)
+
+        if fast:
+            self.wiggle_motor = FastMediumMotor(address=wiggle_motor_port)
+
+            self.touch_sensor = FastTouchSensor(address=touch_sensor_port)
+
+            self.color_sensor = FastColorSensor(address=color_sensor_port)
+
+        else:
+            self.wiggle_motor = MediumMotor(address=wiggle_motor_port)
+
+            self.touch_sensor = TouchSensor(address=touch_sensor_port)
+
+            self.color_sensor = ColorSensor(address=color_sensor_port)
+
+        self.ir_sensor = InfraredSensor(address=ir_sensor_port)
+        self.ir_beacon_channel = ir_beacon_channel
+
+        self.console = Console()
+        self.speaker = Sound()
+
+    def main(self):
+        while True:
+            self.drive_once_by_ir_beacon()
+
+
+if __name__ == '__main__':
+    KRAZ3 = Kraz3()
+
+    KRAZ3.main()
