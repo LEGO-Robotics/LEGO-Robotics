17ba97f23863b2e906fec295a34fea487db3caa8
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-12 16:33:55 -0800

    add programs

--- EV3/Home-Edition/Fan-Robots/Kraz3/Kraz3-1.EV3Dev1.Fast.py
+++ EV3/Home-Edition/Fan-Robots/Kraz3/Kraz3-1.EV3Dev1.Fast.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+Control the left and right tracks using the Remote Control
+"""
 
+from kraz3_ev3dev1 import Kraz3
+
+
+KRAZ3 = Kraz3(fast=True)
+
+KRAZ3.keep_driving_by_ir_beacon()
--- EV3/Home-Edition/Fan-Robots/Kraz3/Kraz3-1.PyBricks.py
+++ EV3/Home-Edition/Fan-Robots/Kraz3/Kraz3-1.PyBricks.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env pybricks-micropython
 
+"""
+Control the left and right tracks using the Remote Control
+"""
 
+from kraz3_pybricks import Kraz3
+
+
+KRAZ3 = Kraz3()
+
+KRAZ3.keep_driving_by_ir_beacon()
