a5a416a3ff98eb4e528056bbdda7bd19426dd3a3
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-13 13:31:32 -0800

    refactor EV3 util.ir_beacon_util_ev3dev2.ir_beacon_measurements_reliable(...)

--- EV3/util/ir_beacon_util_ev3dev2.py
+++ EV3/util/ir_beacon_util_ev3dev2.py
@@ -1,5 +1,5 @@
 def ir_beacon_measurements_reliable(
         heading_angle: float,
         distance: float) -> bool:
-    return (heading_angle is not None) and (-25 < heading_angle < 25) \
+    return (-25 < heading_angle < 25) \
        and (distance is not None) and (0 < distance < 100)
