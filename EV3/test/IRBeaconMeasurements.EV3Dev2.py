76408280348fd7b2c9e6dc2150c79e3a0a8ef0c2
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-13 13:14:45 -0800

    add EV3 util.ir_beacon_util_ev3dev2.ir_beacon_measurements_reliable(...)

---
+++ EV3/test/IRBeaconMeasurements.EV3Dev2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env micropython
+
+
+from ev3dev2.sensor import INPUT_4
+from ev3dev2.sensor.lego import InfraredSensor
+from ev3dev2.console import Console
+
+from time import sleep
+
+from util.ir_beacon_util_ev3dev2 import ir_beacon_measurements_reliable
+
+
+IR_SENSOR = InfraredSensor(address=INPUT_4)
+CONSOLE = Console()
+
+while True:
+    heading, distance = IR_SENSOR.heading_and_distance(channel=1)
+    reliable = ir_beacon_measurements_reliable(heading_angle=heading,
+                                               distance=distance)
+
+    CONSOLE.text_at(
+        text='HA={}, D={}'.format(heading, distance)
+             if reliable
+             else 'x HA={}, D={}'.format(heading, distance),
+        column=1, row=1,
+        reset_console=True,
+        inverse=False,
+        alignment='L')
+
+    sleep(0.3)
