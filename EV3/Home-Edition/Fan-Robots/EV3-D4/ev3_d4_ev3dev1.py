a65ed26705ba5e3611f3c304bca0d85ae9e5a3a7
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-15 20:54:27 -0800

    add programs

--- EV3/Home-Edition/Fan-Robots/EV3-D4/ev3_d4_ev3dev1.py
+++ EV3/Home-Edition/Fan-Robots/EV3-D4/ev3_d4_ev3dev1.py
@@ -1,3 +1,66 @@
 #!/usr/bin/env python3
 
 
+from ev3dev.ev3 import (
+    Motor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
+    TouchSensor, ColorSensor, RemoteControl, INPUT_1, INPUT_3, INPUT_4,
+    Leds, Sound
+)
+
+from random import randint
+from time import sleep
+
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))
+from util.ev3dev_fast.ev3fast import (
+    MediumMotor as FastMediumMotor,
+    TouchSensor as FastTouchSensor,
+    ColorSensor as FastColorSensor
+)
+from util.drive_util_ev3dev1 import IRBeaconRemoteControlledTank
+
+
+class Ev3d4(IRBeaconRemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_C, right_motor_port: str = OUTPUT_B,
+            turn_head_motor_port: str = OUTPUT_A,
+            touch_sensor_port: str = INPUT_1, color_sensor_port: str = INPUT_3,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1,
+            fast=False):
+        super().__init__(
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel,
+            polarity=Motor.POLARITY_INVERSED,
+            fast=fast)
+
+        if fast:
+            self.turn_head_motor = FastMediumMotor(address=turn_head_motor_port)
+
+            self.touch_sensor = FastTouchSensor(address=touch_sensor_port)
+
+            self.color_sensor = FastColorSensor(address=color_sensor_port)
+
+        else:
+            self.turn_head_motor = MediumMotor(address=turn_head_motor_port)
+
+            self.touch_sensor = TouchSensor(address=touch_sensor_port)
+
+            self.color_sensor = ColorSensor(address=color_sensor_port)
+
+        self.remote_control = RemoteControl(sensor=self.ir_sensor,
+                                            channel=ir_beacon_channel)
+
+        self.leds = Leds()
+        self.speaker = Sound()
+
+    def main(self):
+        while True:
+            self.drive_once_by_ir_beacon()
+
+
+if __name__ == '__main__':
+    EV3D4 = Ev3d4()
+
+    EV3D4.main()
--- EV3/Home-Edition/Fan-Robots/EV3-D4/ev3_d4_pybricks.py
+++ EV3/Home-Edition/Fan-Robots/EV3-D4/ev3_d4_pybricks.py
@@ -1,3 +1,52 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
+from pybricks.media.ev3dev import SoundFile
+from pybricks.parameters import Button, Color, Direction, Port, Stop
+from pybricks.tools import wait
+
+from random import randint
+
+# import sys
+# sys.path.append('/home/robot')
+from util.drive_util_pybricks import IRBeaconRemoteControlledTank
+
+
+class Ev3d4(IRBeaconRemoteControlledTank, EV3Brick):
+    WHEEL_DIAMETER = 20
+    AXLE_TRACK = 110
+
+    def __init__(
+            self,
+            left_motor_port: Port = Port.C, right_motor_port: Port = Port.B,
+            wiggle_motor_port: Port = Port.A,
+            polarity: str = 'inversed',
+            touch_sensor_port: Port = Port.S1,
+            color_sensor_port: Port = Port.S3,
+            ir_sensor_port: Port = Port.S4, ir_beacon_channel: int = 1):
+        super().__init__(
+            wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            polarity=polarity,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel)
+
+        self.turn_head_motor = Motor(port=wiggle_motor_port,
+                                     positive_direction=Direction.CLOCKWISE)
+
+        self.touch_sensor = TouchSensor(port=touch_sensor_port)
+
+        self.color_sensor = ColorSensor(port=color_sensor_port)
+
+        self.ir_beacon_channel = ir_beacon_channel
+
+    def main(self):
+        while True:
+            self.drive_once_by_ir_beacon()
+
+
+if __name__ == '__main__':
+    EV3D4 = Ev3d4()
+
+    EV3D4.main()
