34094569742cb40511f4a1676de02139b9de2d20
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-17 20:36:23 -0800

    add comments to EV3-D4-2-Follow-Beacon programs

--- EV3/Home-Edition/Fan-Robots/EV3-D4/EV3-D4-2-Follow-Beacon.EV3Dev1.Threading.py
+++ EV3/Home-Edition/Fan-Robots/EV3-D4/EV3-D4-2-Follow-Beacon.EV3Dev1.Threading.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
+"""
+This is a simple follow-me loop.
+Turn on the Beacon Button of your Remote Control and EV3 D4 will follow you.
+"""
 
--- EV3/Home-Edition/Fan-Robots/EV3-D4/EV3-D4-2-Follow-Beacon.PyBricks.Threading.py
+++ EV3/Home-Edition/Fan-Robots/EV3-D4/EV3-D4-2-Follow-Beacon.PyBricks.Threading.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 
+"""
+This is a simple follow-me loop.
+Turn on the Beacon Button of your Remote Control and EV3 D4 will follow you.
+"""
 
