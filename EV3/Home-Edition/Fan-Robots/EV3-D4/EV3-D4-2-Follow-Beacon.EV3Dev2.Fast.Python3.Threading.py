b12ca50b8dd79d00083250de567d0140ec275107
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-17 21:31:11 -0800

    minor edit to EV3-D4-2-Follow-Beacon programs

--- EV3/Home-Edition/Fan-Robots/EV3-D4/EV3-D4-2-Follow-Beacon.EV3Dev2.Fast.Python3.Threading.py
+++ EV3/Home-Edition/Fan-Robots/EV3-D4/EV3-D4-2-Follow-Beacon.EV3Dev2.Fast.Python3.Threading.py
@@ -2,6 +2,6 @@
 
 """
 This is a simple follow-me loop.
-Turn on the Beacon Button of your Remote Control and EV3 D4 will follow you.
+Turn on the Beacon Button of your Remote Control and EV3-D4 will follow you.
 """
 
