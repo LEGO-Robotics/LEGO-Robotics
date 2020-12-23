70bd0cc310d319789792fad98843232e7b14907d
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-22 22:36:33 -0800

    update program

--- EV3/Home-Edition/Fan-Robots/EV3-D4/ev3_d4_ev3dev1.py
+++ EV3/Home-Edition/Fan-Robots/EV3-D4/ev3_d4_ev3dev1.py
@@ -4,10 +4,12 @@
 from ev3dev.ev3 import (
     Motor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
     TouchSensor, ColorSensor, RemoteControl, INPUT_1, INPUT_3, INPUT_4,
-    Leds, Sound
+    Leds, Screen, Sound
 )
 
-from random import randint
+from PIL import Image
+from random import choice, randint
+from threading import Thread
 from time import sleep
 
 # import os
@@ -53,9 +55,191 @@ class EV3D4(IRBeaconRemoteControlledTank):
                                             channel=ir_beacon_channel)
 
         self.leds = Leds()
+        self.screen = Screen()
         self.speaker = Sound()
 
         self.state = 0
+
+    def action_1(self):
+        self.state = 1
+
+        for _ in range(2):
+            self.screen.image.paste(
+                im=Image.open('/home/robot/image/Dial 0.bmp'))
+            self.screen.update()
+
+            self.speaker.play(
+                wav_file='/home/robot/sound/Blip 2.wav')
+
+            self.leds.set_color(
+                group=Leds.LEFT,
+                color=Leds.ORANGE,
+                pct=1)
+            self.leds.set_color(
+                group=Leds.RIGHT,
+                color=Leds.ORANGE,
+                pct=1)
+
+            sleep(0.1)
+
+            self.leds.set_color(
+                group=Leds.LEFT,
+                color=Leds.GREEN,
+                pct=1)
+            self.leds.set_color(
+                group=Leds.RIGHT,
+                color=Leds.GREEN,
+                pct=1)
+
+            sleep(0.1)
+
+            self.leds.set_color(
+                group=Leds.LEFT,
+                color=Leds.RED,
+                pct=1)
+            self.leds.set_color(
+                group=Leds.RIGHT,
+                color=Leds.RED,
+                pct=1)
+
+            sleep(0.1)
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Blip 3.wav').wait()
+
+    def action_2(self):
+        self.screen.image.paste(
+            im=Image.open('/home/robot/image/Dial 1.bmp'))
+        self.screen.update()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Confirm.wav').wait()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Walk.wav').wait()
+
+        self.shake_head(left_first=True)
+
+        self.leds.set_color(
+            group='LEFT',
+            color='RED',
+            pct=1)
+        self.leds.set_color(
+            group='RIGHT',
+            color='RED',
+            pct=1)
+
+    def action_3(self):
+        self.screen.image.paste(
+            im=Image.open('/home/robot/image/Dial 3.bmp'))
+        self.screen.update()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Overpower.wav').wait()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Arm 1.wav').wait()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Servo 2.wav').wait()
+
+        self.shake_head(
+            left_first=False,
+            n_times=3)
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Blip 3.wav').wait()
+
+    def action_4(self):
+        self.screen.image.paste(
+            im=Image.open('/home/robot/image/Dial 4.bmp'))
+        self.screen.update()
+
+        self.shake_head(
+            left_first=True,
+            n_times=2)
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Power down.wav').wait()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Ready.wav').wait()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Blip 2.wav').wait()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Blip 1.wav').wait()
+
+        self.speaker.play(
+            wav_file='/home/robot/sound/Blip 3.wav').wait()
+
+    def action_5(self):
+        for _ in range(3):
+            self.screen.image.paste(
+                im=Image.open('/home/robot/image/EV3.bmp'))
+            self.screen.update()
+
+            self.leds.set_color(
+                group='LEFT',
+                color='ORANGE',
+                pct=1)
+            self.leds.set_color(
+                group='RIGHT',
+                color='ORANGE',
+                pct=1)
+
+            self.speaker.play(
+                wav_file='/home/robot/sound/Blip 1.wav').wait()
+
+            self.speaker.play(
+                wav_file='/home/robot/sound/Blip 2.wav').wait()
+
+            self.leds.set_color(
+                group='LEFT',
+                color='RED',
+                pct=1)
+            self.leds.set_color(
+                group='RIGHT',
+                color='RED',
+                pct=1)
+
+            self.speaker.play(
+                wav_file='/home/robot/sound/Blip 4.wav').wait()
+
+            self.leds.set_color(
+                group='LEFT',
+                color='GREEN',
+                pct=1)
+            self.leds.set_color(
+                group='RIGHT',
+                color='GREEN',
+                pct=1)
+
+            self.speaker.play(
+                wav_file='/home/robot/sound/Blip 3.wav').wait()
+
+    def shake_head(self, left_first: bool = True, n_times: int = 1):
+        position_sign = 1 if left_first else -1
+
+        for _ in range(n_times):
+            self.head_motor.run_to_rel_pos(
+                speed_sp=500,
+                position_sp=position_sign * 100,
+                stop_action=Motor.STOP_ACTION_HOLD)
+            self.head_motor.wait_while(Motor.STATE_RUNNING)
+
+            self.head_motor.run_to_rel_pos(
+                speed_sp=500,
+                position_sp=-position_sign * 200,
+                stop_action=Motor.STOP_ACTION_HOLD)
+            self.head_motor.wait_while(Motor.STATE_RUNNING)
+
+            self.head_motor.run_to_rel_pos(
+                speed_sp=500,
+                position_sp=position_sign * 100,
+                stop_action=Motor.STOP_ACTION_HOLD)
+            self.head_motor.wait_while(Motor.STATE_RUNNING)
 
     def main_switch_loop(self, driving_speed: float = 750):
         """
@@ -70,15 +254,11 @@ class EV3D4(IRBeaconRemoteControlledTank):
         B+C motors.
         """
         while True:
-            if self.remote_control.beacon:
+            if not self.remote_control.any():
                 self.left_motor.stop(stop_action=Motor.STOP_ACTION_HOLD)
                 self.right_motor.stop(stop_action=Motor.STOP_ACTION_HOLD)
 
-                if self.state == 0:
-                    self.left_motor.stop(stop_action=Motor.STOP_ACTION_HOLD)
-                    self.right_motor.stop(stop_action=Motor.STOP_ACTION_HOLD)
-
-                elif self.state == 1:
+                if self.state == 1:
                     self.left_motor.run_to_rel_pos(
                         speed_sp=driving_speed,
                         position_sp=-1.5 * 360,
@@ -149,6 +329,9 @@ class EV3D4(IRBeaconRemoteControlledTank):
 
                 self.state = 0
 
+            elif self.remote_control.beacon:
+                self.shake_head(left_first=choice((False, True)))
+
             else:
                 self.drive_once_by_ir_beacon(speed=driving_speed)
 
@@ -157,16 +340,59 @@ class EV3D4(IRBeaconRemoteControlledTank):
         This is the Color Sensor Loop that supports 4 different behaviors that
         are triggered RANDOMLY!!!
         """
-        ...
+        while True:
+            if self.color_sensor.color == ColorSensor.COLOR_RED:
+                random_number = randint(1, 4)
+
+                if random_number == 1:
+                    self.action_1()
+
+                elif random_number == 2:
+                    self.action_2()
+
+                elif random_number == 3:
+                    self.action_3()
+
+                elif random_number == 4:
+                    self.action_4()
 
     def touch_sensor_loop(self):
         """
-        This is the Touch Sensor Loop that supports 6 different behaviors that
+        This is the Touch Sensor Loop that supports 5 different behaviors that
         are triggered RANDOMLY!!!
         """
-        ...
+        while True:
+            if self.touch_sensor.is_pressed:
+                random_number = randint(1, 5)
+
+                if random_number == 1:
+                    self.action_1()
+
+                elif random_number == 2:
+                    self.state = 2
+                    self.action_2()
+
+                elif random_number == 3:
+                    self.state = 3
+                    self.action_3()
+
+                elif random_number == 4:
+                    self.state = 2
+                    self.action_4()
+
+                elif random_number == 5:
+                    self.state = 3
+                    self.action_5()
 
     def main(self, driving_speed: float = 750):
+        Thread(
+            target=self.color_sensor_loop,
+            daemon=True).start()
+
+        Thread(
+            target=self.touch_sensor_loop,
+            daemon=True).start()
+
         self.main_switch_loop(driving_speed=driving_speed)
 
 
