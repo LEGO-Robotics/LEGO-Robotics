85780ca34c99ec5bf968314ee6ced40fe2e56d61
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-28 19:29:06 -0800

    update SPIKE Robot-Inventor Tricky tricky_ms module

--- SPIKE/Robot-Inventor/Tricky/tricky_ms.py
+++ SPIKE/Robot-Inventor/Tricky/tricky_ms.py
@@ -19,3 +19,32 @@ class Tricky:
         self.catapult_motor.run_for_seconds(
             seconds=1,
             speed=25)
+
+
+# EOF
+
+
+import os
+import sys
+
+
+TARGET_MODULE_NAME = 'tricky_ms'
+
+# set target file path in projects/ directory
+target_file_path = 'projects/{}.py'.format(TARGET_MODULE_NAME)
+
+# save current file to target file
+print('SAVING {} AS {}...'.format(__file__, target_file_path))
+os.rename(__file__, target_file_path)
+
+# remove file content after "# EOF"
+with open(target_file_path, 'r') as f:
+    file_content = f.read()
+file_content = file_content.split('# EOF')[0]
+with open(target_file_path, 'w') as f:
+    f.write(file_content)
+
+# list projects/ directory and print file content before exiting
+print(os.listdir('projects'))
+print('{} SAVED!'.format(target_file_path))
+sys.exit()
