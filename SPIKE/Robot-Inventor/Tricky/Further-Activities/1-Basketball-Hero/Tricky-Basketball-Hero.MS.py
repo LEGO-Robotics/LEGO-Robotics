8d4ac6f949acaa9ae5cd26d83981f6a8bf28dee8
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-28 19:41:15 -0800

    update Tricky program

--- SPIKE/Robot-Inventor/Tricky/Further-Activities/1-Basketball-Hero/Tricky-Basketball-Hero.MS.py
+++ SPIKE/Robot-Inventor/Tricky/Further-Activities/1-Basketball-Hero/Tricky-Basketball-Hero.MS.py
@@ -1,29 +1,11 @@
-from mindstorms import MSHub, Motor, MotorPair, DistanceSensor
 from mindstorms.control import wait_for_seconds, wait_until
 from mindstorms.operator import less_than
 
-
-class Tricky:
-    def __init__(
-            self,
-            left_motor_port: str = 'A', right_motor_port: str = 'B',
-            catapult_motor_port: str = 'C',
-            distance_sensor_port: str = 'D'):
-        self.hub = MSHub()
-
-        self.motor_pair = MotorPair(left_motor_port, right_motor_port)
-
-        self.catapult_motor = Motor(catapult_motor_port)
-
-        self.distance_sensor = DistanceSensor(distance_sensor_port)
-
-    def lower_catapult(self):
-        self.catapult_motor.run_for_seconds(
-            seconds=1,
-            speed=25)
+from tricky_ms import Tricky
 
 
 TRICKY = Tricky()
+
 
 TRICKY.hub.light_matrix.show_image(
     image='TARGET',
