c795bc5bebe7e8857e498aeb6615b4563e127111
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-28 19:37:16 -0800

    update SPIKE Robot-Inventor Tricky tricky_ms module

--- SPIKE/Robot-Inventor/Tricky/Tricky-1-Introducing-Tricky.MS.py
+++ SPIKE/Robot-Inventor/Tricky/Tricky-1-Introducing-Tricky.MS.py
@@ -1,17 +1,8 @@
-from mindstorms import MotorPair, DistanceSensor
-
-
-class Tricky:
-    def __init__(
-            self,
-            left_motor_port: str = 'A', right_motor_port: str = 'B',
-            distance_sensor_port: str = 'D'):
-        self.motor_pair = MotorPair(left_motor_port, right_motor_port)
-
-        self.distance_sensor = DistanceSensor(distance_sensor_port)
+from tricky_ms import Tricky
 
 
 TRICKY = Tricky()
+
 
 TRICKY.distance_sensor.light_up_all(brightness=100)
 
