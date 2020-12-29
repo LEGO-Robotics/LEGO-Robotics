57fa4e697141a6f88c4c6a00f354eb9959d0028e
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-12-28 19:21:34 -0800

    update SPIKE.util.save_module

--- SPIKE/util/save_module.py
+++ SPIKE/util/save_module.py
@@ -1,23 +1,18 @@
+# EOF
+
+
 import os
 import sys
 
 
 TARGET_MODULE_NAME = '___'
 
-
+# set target file path in projects/ directory
 target_file_path = 'projects/{}.py'.format(TARGET_MODULE_NAME)
+
+# save current file to target file
 print('SAVING {} AS {}...'.format(__file__, target_file_path))
-
-# remove target file if it already exists
-try:
-    os.remove(target_file_path)
-except:
-    print('CREATING NEW {}...'.format(target_file_path))
-
-# rename current file to target file
-os.rename(
-    __file__,
-    target_file_path)
+os.rename(__file__, target_file_path)
 
 # remove file content after "# EOF"
 with open(target_file_path, 'r') as f:
@@ -26,7 +21,7 @@ file_content = file_content.split('# EOF')[0]
 with open(target_file_path, 'w') as f:
     f.write(file_content)
 
-# list projects/ directory before exiting
+# list projects/ directory and print file content before exiting
 print(os.listdir('projects'))
 print('{} SAVED:\n```\n{}\n```'.format(target_file_path, file_content))
 sys.exit()
