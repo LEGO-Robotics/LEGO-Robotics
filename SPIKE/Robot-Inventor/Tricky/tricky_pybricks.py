dcdea03edff68be7610d3c068566304b6619f45c
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-01-01 22:05:01 -0800

    add SPIKE Robot-Inventor Tricky tricky_pybricks program

--- SPIKE/Robot-Inventor/Tricky/tricky_pybricks.py
+++ SPIKE/Robot-Inventor/Tricky/tricky_pybricks.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env pybricks-micropython
 
 
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Direction, Port
+from pybricks.robotics import DriveBase
+
+
+class Tricky:
+    WHEEL_DIAMETER = 44
+    AXLE_TRACK = 88
+
+    def __init__(
+            self,
+            left_wheel_motor_port: Port = Port.A,
+            right_wheel_motor_port: Port = Port.B,
+            sport_motor_port: Port = Port.C,
+            distance_sensor_port: Port = Port.D,
+            color_sensor_port: Port = Port.E):
+        self.hub = InventorHub()
+
+        self.driving_base = \
+            DriveBase(
+                left_motor=Motor(port=left_wheel_motor_port,
+                                 positive_direction=Direction.CLOCKWISE),
+                right_motor=Motor(port=right_wheel_motor_port,
+                                  positive_direction=Direction.CLOCKWISE),
+                wheel_diameter=self.WHEEL_DIAMETER,
+                axle_track=self.AXLE_TRACK)
+
+        self.sport_motor = Motor(port=sport_motor_port,
+                                 positive_direction=Direction.CLOCKWISE)
+
+        self.color_sensor = ColorSensor(port=color_sensor_port)
+
+        self.distance_sensor = UltrasonicSensor(port=distance_sensor_port)
