c568fe98217a4ef5ca9828f71956c765af6604aa
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-01-03 00:40:31 -0800

    update SPIKE Robot-Inventor Tricky Tricky-1-Introducing-Tricky.PyBricks program

--- SPIKE/Robot-Inventor/Tricky/Tricky-1-Introducing-Tricky.PyBricks.py
+++ SPIKE/Robot-Inventor/Tricky/Tricky-1-Introducing-Tricky.PyBricks.py
@@ -34,6 +34,8 @@ if __name__ == '__main__':
     # TRICKY.distance_sensor.lights.on(100)
 
     while True:
+        # Tricky will begin when
+        # the Distance Sensor detects something closer than 10 cm.
         if TRICKY.distance_sensor.distance() < 100:
             TRICKY.drive_base.turn(angle=360)
             TRICKY.drive_base.turn(angle=-360)
