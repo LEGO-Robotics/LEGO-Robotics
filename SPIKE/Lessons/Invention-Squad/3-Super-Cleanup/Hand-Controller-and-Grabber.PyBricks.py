898e0337139eb324ac73ce637c3eb800fbce34ef
Ariana Lương-Phạm <Ariana.LuongPham@gmail.com>
2021-03-16 21:02:41 -0700

    add SPIKE Lessons Invention-Squad Hand-Controller-and-Grabber PyBricks program

--- SPIKE/Lessons/Invention-Squad/3-Super-Cleanup/Hand-Controller-and-Grabber.PyBricks.py
+++ SPIKE/Lessons/Invention-Squad/3-Super-Cleanup/Hand-Controller-and-Grabber.PyBricks.py
@@ -1,1 +1,19 @@
-#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import ForceSensor, Motor
+from pybricks.parameters import Port, Stop
+
+
+hub = PrimeHub()
+force_sensor = ForceSensor(port=Port.E)
+motor = Motor(port=Port.A)
+
+
+while True:
+    if force_sensor.pressed(force=3):
+        motor.run(speed=-1000)
+
+    else:
+        motor.run_until_stalled(
+            speed=1000,
+            then=Stop.COAST,
+            duty_limit=None)
