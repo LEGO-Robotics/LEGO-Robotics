b689df810cc5676d72a71f1dcda0fb1abba2f0f9
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-07-22 00:58:27 -0700

    update EV3 Birthday-Bot program

--- EV3/Antoni-Bots/Birthday-Bot/Birthday-Bot.EV3Dev2.py
+++ EV3/Antoni-Bots/Birthday-Bot/Birthday-Bot.EV3Dev2.py
@@ -3,14 +3,33 @@
 
 import json
 
+from ev3dev2.motor import Motor, LargeMotor, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor import INPUT_4
+# from ev3dev2.display import Display
 from ev3dev2.sound import Sound
+
+# import os
+# import sys
+# sys.path.append(os.path.expanduser('~'))
+from util.drive_util_ev3dev2 import IRBeaconRemoteControlledTank
 
 
 HAPPY_BIRTHDAY_SONG = json.load(open('Happy-Birthday-Song.EV3Dev.json'))
 
 
-class BirthdayBot:
-    def __init__(self):
+class BirthdayBot(IRBeaconRemoteControlledTank):
+    def __init__(
+            self,
+            left_motor_port: str = OUTPUT_C, right_motor_port: str = OUTPUT_B,
+            ir_sensor_port: str = INPUT_4, ir_beacon_channel: int = 1,
+            fast: bool = False):
+        super().__init__(
+            left_motor_port=left_motor_port, right_motor_port=right_motor_port,
+            motor_class=LargeMotor, polarity=Motor.POLARITY_INVERSED,
+            ir_sensor_port=ir_sensor_port, ir_beacon_channel=ir_beacon_channel,
+            fast=fast)
+
+        # self.screen = Display()
         self.speaker = Sound()
 
     def say_happy_birthday(self):
@@ -26,9 +45,10 @@ class BirthdayBot:
             delay=0)
 
     def main(self):
-        self.say_happy_birthday()
+        # self.say_happy_birthday()
 
-        self.sing_happy_birthday()
+        # self.sing_happy_birthday()
+        self.keep_driving_by_ir_beacon()
 
 
 if __name__ == '__main__':
