85d0a5c6f6a85b82be1107473a6e4a740e4699c1
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-07-22 20:24:36 -0700

    update EV3 Birthday-Gift-Bear3r program

--- EV3/Antoni-Bots/Birthday-Gift-Bear3r/Birthday-Gift-Bear3r.PyBricks.RunParallel.py
+++ EV3/Antoni-Bots/Birthday-Gift-Bear3r/Birthday-Gift-Bear3r.PyBricks.RunParallel.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env pybricks-micropython
 
 
+import json
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor
-from pybricks.media.ev3dev import ImageFile, SoundFile
+from pybricks.media.ev3dev import ImageFile
 from pybricks.parameters import Button, Direction, Port, Stop
 
 from pybricks.experimental import run_parallel
@@ -11,6 +13,9 @@ from pybricks.experimental import run_parallel
 # import sys
 # sys.path.append('/home/robot')
 from util.drive_util_pybricks import IRBeaconRemoteControlledTank
+
+
+HAPPY_BIRTHDAY_SONG = json.load(open('Happy-Birthday-Song.PyBricks.json'))
 
 
 class GiftBear3rBirthdayBot(IRBeaconRemoteControlledTank, EV3Brick):
@@ -24,7 +29,7 @@ class GiftBear3rBirthdayBot(IRBeaconRemoteControlledTank, EV3Brick):
             touch_sensor_port: Port = Port.S1,
             ir_sensor_port: Port = Port.S4,
             driving_ir_beacon_channel: int = 1,
-            arm_control_ir_beacon_channel: int = 2):
+            non_driving_ir_beacon_channel: int = 2):
         super().__init__(
             wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
             left_motor_port=left_motor_port, right_motor_port=right_motor_port,
@@ -38,21 +43,29 @@ class GiftBear3rBirthdayBot(IRBeaconRemoteControlledTank, EV3Brick):
         self.touch_sensor = TouchSensor(port=touch_sensor_port)
 
         self.ir_sensor = InfraredSensor(port=ir_sensor_port)
-        self.arm_control_ir_beacon_channel = arm_control_ir_beacon_channel
+        self.non_driving_ir_beacon_channel = non_driving_ir_beacon_channel
 
     def start_up(self):
-        self.screen.load_image(source=ImageFile.WINKING)
+        self.screen.load_image(ImageFile.NEUTRAL)
 
-    def say_happy_birthday(self):
+    def sing_happy_birthday_whenever_ir_beacon_pressed(self):
         while True:
-            pass
-            
+            ir_beacon_button_pressed = \
+                set(self.ir_sensor.buttons(
+                        channel=self.non_driving_ir_beacon_channel))
+
+            if ir_beacon_button_pressed == {Button.BEACON}:
+                self.speaker.play_notes(
+                    notes=HAPPY_BIRTHDAY_SONG,
+                    tempo=120)
+
     def main(self):
         self.start_up()
-        self.keep_driving_by_ir_beacon
-        # run_parallel(
-        #     self.keep_driving_by_ir_beacon)
-            
+
+        run_parallel(
+            self.keep_driving_by_ir_beacon,
+            self.sing_happy_birthday_whenever_ir_beacon_pressed)
+
 
 if __name__ == '__main__':
     GIFT_BEAR3R_BIRTHDAY_BOT = GiftBear3rBirthdayBot()
