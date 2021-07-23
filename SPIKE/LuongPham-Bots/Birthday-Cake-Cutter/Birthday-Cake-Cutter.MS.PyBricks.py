970185e72cf21fe1ba75bcd2b07f2421d219a20c
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-07-23 15:52:01 -0700

    update Cake Cutter program

--- SPIKE/LuongPham-Bots/Birthday-Cake-Cutter/Birthday-Cake-Cutter.MS.PyBricks.py
+++ SPIKE/LuongPham-Bots/Birthday-Cake-Cutter/Birthday-Cake-Cutter.MS.PyBricks.py
@@ -2,7 +2,7 @@ from pybricks.hubs import InventorHub
 from pybricks.pupdevices import Motor, Remote
 from pybricks.robotics import DriveBase
 from pybricks.geometry import Axis
-from pybricks.parameters import Button, Direction, Icon, Port
+from pybricks.parameters import Button, Direction, Icon, Port, Stop
 
 # from pybricks.experimental import run_parallel
 
@@ -111,12 +111,13 @@ class RemoteControlledDriveBase:
 
 class BirthdayCakeCutter(RemoteControlledDriveBase):
     WHEEL_DIAMETER = 44   # milimeters
-    AXLE_TRACK = 100      # milimeters
+    AXLE_TRACK = 75       # milimeters
 
     def __init__(
             self,
-            left_motor_port: Port = Port.D, right_motor_port: Port = Port.C,
-            fan_motor_port: Port = Port.A):
+            left_motor_port: Port = Port.B, right_motor_port: Port = Port.A,
+            first_cutter_motor_port: Port = Port.C,
+            second_cutter_motor_port: Port = Port.D):
         super().__init__(
             wheel_diameter=self.WHEEL_DIAMETER,
             axle_track=self.AXLE_TRACK,
@@ -128,8 +129,28 @@ class BirthdayCakeCutter(RemoteControlledDriveBase):
         self.hub = InventorHub(top_side=Axis.X,
                                front_side=Axis.Z)
 
-        self.fan_motor = Motor(port=fan_motor_port,
-                               positive_direction=Direction.CLOCKWISE)
+        self.cake_cutting_mode = False
+
+        self.first_cutter_motor = \
+            Motor(port=first_cutter_motor_port,
+                  positive_direction=Direction.CLOCKWISE)
+
+        self.second_cutter_motor = \
+            Motor(port=second_cutter_motor_port,
+                  positive_direction=Direction.CLOCKWISE)
+
+    def switch_mode_by_remote(self):
+        remote_button_pressed = self.remote.buttons.pressed()
+
+        if remote_button_pressed == (Button.LEFT,):
+            if self.cake_cutting_mode:
+                self.cake_cutting_mode = False
+                print('Mode: Driving')
+
+        elif remote_button_pressed == (Button.RIGHT,):
+            if not self.cake_cutting_mode:
+                self.cake_cutting_mode = True
+                print('Mode: Cake-Cutting')
 
     def smile(self):
         self.hub.display.image(image=Icon.HAPPY)
@@ -140,12 +161,33 @@ class BirthdayCakeCutter(RemoteControlledDriveBase):
                 notes=HAPPY_BIRTHDAY_SONG,
                 tempo=120)
 
+    def cut_piece_if_right_red_button_pressed(self):
+        if self.remote.buttons.pressed() == (Button.RIGHT,):
+            self.first_cutter_motor.run_until_stalled(
+                speed=50,
+                then=Stop.HOLD
+            )
+            self.second_cutter_motor.run_until_stalled(
+                speed=50,
+                then=Stop.HOLD
+            )
+
     def main(self):
         self.smile()
 
         while True:
-            self.drive_once_by_remote()
-            self.sing_happy_birthday_by_remote_left_red_button()
+            self.switch_mode_by_remote()
+
+            if self.cake_cutting_mode:
+                ...
+
+            else:
+                self.drive_once_by_remote()
+            #    self.cut_piece_if_right_red_button_pressed()
+            #if self.cake_cutting_mode == False:
+            #    
+            #    self.sing_happy_birthday_by_remote_left_red_button()
+            #el
 
 
 if __name__ == '__main__':
