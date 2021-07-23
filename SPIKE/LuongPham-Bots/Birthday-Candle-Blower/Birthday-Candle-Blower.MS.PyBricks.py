45cb33b0d334f93800099a34d03e599c7c363749
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-07-23 01:42:46 -0700

    update Birthday-Candle-Blower.MS.PyBricks program

--- SPIKE/LuongPham-Bots/Birthday-Candle-Blower/Birthday-Candle-Blower.MS.PyBricks.py
+++ SPIKE/LuongPham-Bots/Birthday-Candle-Blower/Birthday-Candle-Blower.MS.PyBricks.py
@@ -1,4 +1,10 @@
 from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, Remote
+from pybricks.robotics import DriveBase
+from pybricks.geometry import Axis
+from pybricks.parameters import Button, Direction, Port
+
+# from pybricks.experimental import run_parallel
 
 
 HAPPY_BIRTHDAY_SONG = [
@@ -10,12 +16,122 @@ HAPPY_BIRTHDAY_SONG = [
 ]
 
 
-class BirthdayBot:
-    def __init__(self):
-        self.hub = InventorHub()
+class RemoteControlledDriveBase:
+    def __init__(
+            self,
+            wheel_diameter: float, axle_track: float,   # both in milimeters
+            left_motor_port: Port = Port.A,
+            left_motor_pos_dir: Direction = Direction.COUNTERCLOCKWISE,
+            right_motor_port: Port = Port.B,
+            right_motor_pos_dir: Direction = Direction.CLOCKWISE):
+        self.left_motor = Motor(port=left_motor_port,
+                                positive_direction=left_motor_pos_dir)
+        self.right_motor = Motor(port=right_motor_port,
+                                 positive_direction=right_motor_pos_dir)
 
-    def say_happy_birthday(self):
-        self.hub.speaker.say(text='Happy Birthday to Mommy!')
+        self.drive_base = DriveBase(left_motor=self.left_motor,
+                                    right_motor=self.right_motor,
+                                    wheel_diameter=wheel_diameter,
+                                    axle_track=axle_track)
+
+        self.remote = Remote()
+        print('Remote Connected!')
+
+    def drive_once_by_remote(
+            self,
+            speed: float = 1000,    # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+            ):
+        remote_button_pressed = self.remote.buttons.pressed()
+        print(remote_button_pressed)
+
+        # forward
+        if remote_button_pressed == {Button.LEFT_PLUS, Button.RIGHT_PLUS}:
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=0)
+
+        # backward
+        elif remote_button_pressed == {Button.LEFT_MINUS, Button.RIGHT_MINUS}:
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=0)
+
+        # turn left on the spot
+        elif remote_button_pressed == {Button.LEFT_PLUS, Button.RIGHT_MINUS}:
+            self.drive_base.drive(
+                speed=0,
+                turn_rate=-turn_rate)
+
+        # turn right on the spot
+        elif remote_button_pressed == {Button.RIGHT_PLUS, Button.LEFT_MINUS}:
+            self.drive_base.drive(
+                speed=0,
+                turn_rate=turn_rate)
+
+        # turn left forward
+        elif remote_button_pressed == {Button.LEFT_PLUS}:
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=-turn_rate)
+
+        # turn right forward
+        elif remote_button_pressed == {Button.RIGHT_PLUS}:
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=turn_rate)
+
+        # turn left backward
+        elif remote_button_pressed == {Button.LEFT_MINUS}:
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=turn_rate)
+
+        # turn right backward
+        elif remote_button_pressed == {Button.RIGHT_MINUS}:
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=-turn_rate)
+
+        # otherwise stop
+        else:
+            print(self)
+            self.drive_base.stop()
+
+    # this method must be used in a parallel process/thread
+    # in order not to block other operations
+    def keep_driving_by_remote(
+            self,
+            speed: float = 1000,    # mm/s
+            turn_rate: float = 90   # rotational speed deg/s
+            ):
+        while True:
+            self.drive_once_by_remote(
+                speed=speed,
+                turn_rate=turn_rate)
+
+
+class BirthdayCandleBlower(RemoteControlledDriveBase):
+    WHEEL_DIAMETER = 44   # milimeters
+    AXLE_TRACK = 99       # milimeters
+
+    def __init__(
+            self,
+            left_motor_port: Port = Port.D, right_motor_port: Port = Port.C,
+            fan_motor_port: Port = Port.A):
+        super().__init__(
+            wheel_diameter=self.WHEEL_DIAMETER,
+            axle_track=self.AXLE_TRACK,
+            left_motor_port=left_motor_port,
+            left_motor_pos_dir=Direction.COUNTERCLOCKWISE,
+            right_motor_port=right_motor_port,
+            right_motor_pos_dir=Direction.CLOCKWISE)
+
+        self.hub = InventorHub(top_side=Axis.X,
+                               front_side=Axis.Z)
+
+        self.fan_motor = Motor(port=fan_motor_port,
+                               positive_direction=Direction.COUNTERCLOCKWISE)
 
     def sing_happy_birthday(self):
         self.hub.speaker.play_notes(
@@ -23,12 +139,10 @@ class BirthdayBot:
             tempo=120)
 
     def main(self):
-        self.say_happy_birthday()
-
-        self.sing_happy_birthday()
+        self.keep_driving_by_remote()
 
 
 if __name__ == '__main__':
-    BIRTHDAY_BOT = BirthdayBot()
+    BIRTHDAY_CANDLE_BLOWER = BirthdayCandleBlower()
 
-    BIRTHDAY_BOT.main()
+    BIRTHDAY_CANDLE_BLOWER.main()
