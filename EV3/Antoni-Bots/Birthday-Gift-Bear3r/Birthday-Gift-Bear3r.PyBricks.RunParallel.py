0015d27e77dd5d58456f27c6296bd5ab334d1002
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-07-22 22:35:14 -0700

    update EV3 Birthday-Gift-Bear3r.PyBricks.RunParallel program

--- EV3/Antoni-Bots/Birthday-Gift-Bear3r/Birthday-Gift-Bear3r.PyBricks.RunParallel.py
+++ EV3/Antoni-Bots/Birthday-Gift-Bear3r/Birthday-Gift-Bear3r.PyBricks.RunParallel.py
@@ -4,7 +4,7 @@
 import json
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, TouchSensor, InfraredSensor
+from pybricks.ev3devices import Motor, TouchSensor
 from pybricks.media.ev3dev import ImageFile
 from pybricks.parameters import Button, Direction, Port
 
@@ -29,7 +29,7 @@ class BirthdayGiftBear3r(IRBeaconRemoteControlledTank, EV3Brick):
             touch_sensor_port: Port = Port.S1,
             ir_sensor_port: Port = Port.S4,
             driving_ir_beacon_channel: int = 1,
-            non_driving_ir_beacon_channel: int = 2):
+            non_driving_ir_beacon_channel: int = 4):
         super().__init__(
             wheel_diameter=self.WHEEL_DIAMETER, axle_track=self.AXLE_TRACK,
             left_motor_port=left_motor_port, right_motor_port=right_motor_port,
@@ -37,34 +37,63 @@ class BirthdayGiftBear3r(IRBeaconRemoteControlledTank, EV3Brick):
             ir_sensor_port=ir_sensor_port,
             ir_beacon_channel=driving_ir_beacon_channel)
 
-        self.arm_control_motor = Motor(port=medium_motor_port,
-                                       positive_direction=Direction.CLOCKWISE)
+        self.arm_control_motor = \
+            Motor(port=medium_motor_port,
+                  positive_direction=Direction.COUNTERCLOCKWISE)
 
         self.touch_sensor = TouchSensor(port=touch_sensor_port)
 
-        self.ir_sensor = InfraredSensor(port=ir_sensor_port)
         self.non_driving_ir_beacon_channel = non_driving_ir_beacon_channel
+
+        # self.non_driving_ir_beacon_channels = set(range(1, 5))
+        # self.non_driving_ir_beacon_channels.remove(driving_ir_beacon_channel)
 
     def start_up(self):
         self.screen.load_image(ImageFile.NEUTRAL)
 
-    def sing_happy_birthday_whenever_ir_beacon_pressed(self):
-        while True:
-            ir_beacon_button_pressed = \
-                set(self.ir_sensor.buttons(
-                        channel=self.non_driving_ir_beacon_channel))
+    def lower_or_raise_arm_by_ir_beacon(self):
+        non_driving_ir_beacon_button_pressed = \
+            self.ir_sensor.buttons(channel=self.non_driving_ir_beacon_channel)
+        #   *** Using sum(...) like below leads to                       ***
+        #   *** TypeError: unsupported type for __hash__: 'bound_method' ***
+        #   sum(self.ir_sensor.buttons(channel=channel)
+        #       for channel in self.non_driving_ir_beacon_channels)
 
-            if ir_beacon_button_pressed == {Button.BEACON}:
-                self.speaker.play_notes(
-                    notes=HAPPY_BIRTHDAY_SONG,
-                    tempo=120)
+        if {Button.LEFT_DOWN, Button.RIGHT_DOWN}.intersection(
+                non_driving_ir_beacon_button_pressed):
+            self.arm_control_motor.run(speed=-100)
+
+        elif {Button.LEFT_UP, Button.RIGHT_UP}.intersection(
+                non_driving_ir_beacon_button_pressed):
+            self.arm_control_motor.run(speed=100)
+
+        else:
+            self.arm_control_motor.hold()
+
+    def keep_controlling_arm_by_ir_beacon(self):
+        while True:
+            self.lower_or_raise_arm_by_ir_beacon()
+
+    def sing_happy_birthday_if_ir_beacon_button_pressed(self):
+        if Button.BEACON in (self.ir_sensor.buttons(channel=1) +
+                             self.ir_sensor.buttons(channel=2) +
+                             self.ir_sensor.buttons(channel=3) +
+                             self.ir_sensor.buttons(channel=4)):
+            self.speaker.play_notes(
+                notes=HAPPY_BIRTHDAY_SONG,
+                tempo=120)
+
+    def sing_happy_birthday_whenever_ir_beacon_button_pressed(self):
+        while True:
+            self.sing_happy_birthday_if_ir_beacon_button_pressed()
 
     def main(self):
         self.start_up()
 
         run_parallel(
             self.keep_driving_by_ir_beacon,
-            self.sing_happy_birthday_whenever_ir_beacon_pressed)
+            self.keep_controlling_arm_by_ir_beacon,
+            self.sing_happy_birthday_whenever_ir_beacon_button_pressed)
 
 
 if __name__ == '__main__':
