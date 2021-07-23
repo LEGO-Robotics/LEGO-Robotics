ad19ef549090c6e81f43185c2de75deba526542b
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-07-23 13:45:09 -0700

    update SPIKE Birthday-Candle-Blower program

--- SPIKE/LuongPham-Bots/Birthday-Candle-Blower/Birthday-Candle-Blower.SPIKE.PyBricks.py
+++ SPIKE/LuongPham-Bots/Birthday-Candle-Blower/Birthday-Candle-Blower.SPIKE.PyBricks.py
@@ -1,6 +1,19 @@
+from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, Remote
 from pybricks.robotics import DriveBase
-from pybricks.parameters import Button, Direction, Port
+from pybricks.geometry import Axis
+from pybricks.parameters import Button, Direction, Icon, Port
+
+# from pybricks.experimental import run_parallel
+
+
+HAPPY_BIRTHDAY_SONG = [
+    'G3/8', 'G3/8', 'A3/4', 'G3/4', 'C4/4', 'B3/2',
+    'G3/8', 'G3/8', 'A3/4', 'G3/4', 'D4/4', 'C4/2',
+    'G3/8', 'G3/8', 'G4/4', 'E4/4',
+    'C4/8', 'C4/8', 'B3/4', 'A3/4',
+    'F4/8', 'F4/8', 'E4/4', 'C4/4', 'D4/4', 'C4/2'
+]
 
 
 class RemoteControlledDriveBase:
@@ -24,7 +37,7 @@ class RemoteControlledDriveBase:
         self.remote = Remote()
         print('Remote Connected!')
 
-    def drive_by_remote(
+    def drive_once_by_remote(
             self,
             speed: float = 1000,    # mm/s
             turn_rate: float = 90   # rotational speed deg/s
@@ -33,97 +46,117 @@ class RemoteControlledDriveBase:
 
         # forward
         if remote_button_pressed == (Button.LEFT_PLUS, Button.RIGHT_PLUS):
-            self.left_motor.run(speed=speed)
-            self.right_motor.run(speed=speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=speed,
-            #     turn_rate=0)
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=0)
 
         # backward
         elif remote_button_pressed == (Button.LEFT_MINUS, Button.RIGHT_MINUS):
-            self.left_motor.run(speed=-speed)
-            self.right_motor.run(speed=-speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=-speed,
-            #     turn_rate=0)
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=0)
 
         # turn left on the spot
         elif remote_button_pressed == (Button.RIGHT_MINUS, Button.LEFT_PLUS):
-            self.left_motor.run(speed=-speed)
-            self.right_motor.run(speed=speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=0,
-            #     turn_rate=-turn_rate)
+            self.drive_base.drive(
+                speed=0,
+                turn_rate=-turn_rate)
 
         # turn right on the spot
         elif remote_button_pressed == (Button.LEFT_MINUS, Button.RIGHT_PLUS):
-            self.left_motor.run(speed=speed)
-            self.right_motor.run(speed=-speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=0,
-            #     turn_rate=turn_rate)
+            self.drive_base.drive(
+                speed=0,
+                turn_rate=turn_rate)
 
         # turn left forward
         elif remote_button_pressed == (Button.LEFT_PLUS,):
-            self.right_motor.run(speed=speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=speed,
-            #     turn_rate=-turn_rate)
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=-turn_rate)
 
         # turn right forward
         elif remote_button_pressed == (Button.RIGHT_PLUS,):
-            self.left_motor.run(speed=speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=speed,
-            #     turn_rate=turn_rate)
+            self.drive_base.drive(
+                speed=speed,
+                turn_rate=turn_rate)
 
         # turn left backward
         elif remote_button_pressed == (Button.LEFT_MINUS,):
-            self.right_motor.run(speed=-speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=-speed,
-            #     turn_rate=turn_rate)
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=turn_rate)
 
         # turn right backward
         elif remote_button_pressed == (Button.RIGHT_MINUS,):
-            self.left_motor.run(speed=-speed)
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.drive(
-            #     speed=-speed,
-            #     turn_rate=-turn_rate)
+            self.drive_base.drive(
+                speed=-speed,
+                turn_rate=-turn_rate)
 
         # otherwise stop
         else:
-            self.left_motor.hold()
-            self.right_motor.hold()
-            # *** BELOW NOT WORKING ***
-            # self.drive_base.stop()
+            self.drive_base.stop()
 
-    def main(
+    # this method must be used in a parallel process/thread
+    # in order not to block other operations
+    def keep_driving_by_remote(
             self,
             speed: float = 1000,    # mm/s
             turn_rate: float = 90   # rotational speed deg/s
             ):
         while True:
-            self.drive_by_remote(
+            self.drive_once_by_remote(
                 speed=speed,
                 turn_rate=turn_rate)
 
 
-if __name__ == '__main__':
-    DRIVE_BASE = \
-        RemoteControlledDriveBase(
-            wheel_diameter=44, axle_track=99,
-            left_motor_port=Port.D,
+class BirthdayCandleBlower(RemoteControlledDriveBase):
+    WHEEL_DIAMETER = 44   # milimeters
+    AXLE_TRACK = 100      # milimeters
+
+    def __init__(
+            self,
+            left_motor_port: Port = Port.D, right_motor_port: Port = Port.C,
+            fan_motor_port: Port = Port.A):
+        super().__init__(
+            wheel_diameter=self.WHEEL_DIAMETER,
+            axle_track=self.AXLE_TRACK,
+            left_motor_port=left_motor_port,
             left_motor_pos_dir=Direction.COUNTERCLOCKWISE,
-            right_motor_port=Port.C,
+            right_motor_port=right_motor_port,
             right_motor_pos_dir=Direction.CLOCKWISE)
 
-    DRIVE_BASE.main()
+        self.hub = PrimeHub(top_side=Axis.X,
+                            front_side=Axis.Z)
+
+        self.fan_motor = Motor(port=fan_motor_port,
+                               positive_direction=Direction.CLOCKWISE)
+
+    def smile(self):
+        self.hub.display.image(image=Icon.HAPPY)
+
+    def sing_happy_birthday_by_remote_left_red_button(self):
+        if self.remote.buttons.pressed() == (Button.LEFT,):
+            self.hub.speaker.play_notes(
+                notes=HAPPY_BIRTHDAY_SONG,
+                tempo=120)
+
+    def spin_fan_by_remote_right_red_button(self):
+        if self.remote.buttons.pressed() == (Button.RIGHT,):
+            self.fan_motor.run(speed=1000)
+
+        else:
+            self.fan_motor.stop()
+
+    def main(self):
+        self.smile()
+
+        while True:
+            self.drive_once_by_remote()
+            self.sing_happy_birthday_by_remote_left_red_button()
+            self.spin_fan_by_remote_right_red_button()
+
+
+if __name__ == '__main__':
+    BIRTHDAY_CANDLE_BLOWER = BirthdayCandleBlower()
+
+    BIRTHDAY_CANDLE_BLOWER.main()
