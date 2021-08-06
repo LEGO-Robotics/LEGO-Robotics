84d9bc22584800a2d618ef0c4a1f19c8126516bc
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-08-05 23:37:41 -0700

    update Bobb3e program

--- EV3/Home-Edition/Fan-Robots/Bobb3e/Bobb3e.PyBricks.MultiProcessing.FIXME.py
+++ EV3/Home-Edition/Fan-Robots/Bobb3e/Bobb3e.PyBricks.MultiProcessing.FIXME.py
@@ -14,7 +14,7 @@ Process(target=BOBB3E.sound_alarm_whenever_reversing).start()
 
 BOBB3E.keep_driving_or_operating_forks_by_ir_beacon(speed=1000)
 
-# FIXME: OSError: [Errno 5] EIO: 
+# FIXME: OSError: [Errno 5] EIO:
 #  Unexpected hardware input/output error with a motor or sensor:
 # --> Try unplugging the sensor or motor and plug it back in again.
 # --> To see which sensor or motor is causing the problem,
