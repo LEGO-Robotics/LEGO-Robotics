aa6f5a4dca803a32b91c79a451bc8c5a1c6f8b5b
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-08-13 12:15:34 -0700

    update SPIKE/util/Print-Hub-System-Reset-Reason

--- SPIKE/util/Print-Hub-System-Reset-Reason.py
+++ SPIKE/util/Print-Hub-System-Reset-Reason.py
@@ -3,4 +3,4 @@ from pybricks.hubs import InventorHub
 
 hub = InventorHub()
 
-print(f'Hub System Reset Reason: {hub.system.reset_reason()}')
+print('Hub System Reset Reason:', hub.system.reset_reason())
